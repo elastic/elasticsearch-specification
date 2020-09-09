38cd98b45cd0e2f072ab340bf9dd51d2e10acb1a
Martijn Laarman <Mpdreamz@gmail.com>
2020-09-09 16:27:19 +0200

    reference spec by name not path

--- typescript-generator/index.ts
+++ typescript-generator/index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import path from 'path'
-import { Specification } from '../specification/src/api-specification'
+import { Specification } from 'elasticsearch-client-specification'
 import Domain, {
   ArrayOf,
   Dictionary,
