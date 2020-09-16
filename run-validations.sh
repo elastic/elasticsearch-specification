e43e532e78ed6d15b5fd1e3586a6cfe1921ee365
Martijn Laarman <Mpdreamz@gmail.com>
2020-09-16 14:32:58 +0200

    add simple script to automate development steps

---
+++ run-validations.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+npm install --prefix specification
+npm install --prefix typescript-generator
+
+npm run compile:brain
+npm run compile:specs
+npm run compile:ts-validation
+
+
+
