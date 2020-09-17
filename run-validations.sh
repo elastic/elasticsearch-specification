eb0c1570a9f3b40f6fbc9a11254c9ab04f23c50b
Martijn Laarman <Mpdreamz@gmail.com>
2020-09-17 22:28:19 +0200

    Automate calling all validation tools for work on the type specification

--- run-validations.sh
+++ run-validations.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-npm install --prefix specification
-npm install --prefix typescript-generator
+[ -z "$FAST" ] && npm install --prefix specification
+[ -z "$FAST" ] && npm install --prefix typescript-generator
 
-npm run compile:brain
-npm run compile:specs
-npm run compile:ts-validation
+[ -z "$FAST" ] && npm run compile:brain
+[ -z "$FAST" ] && npm run compile:specs
+[ -z "$FAST" ] && npm run compile:ts-validation
+
+recorderFolder="../clients-flight-recorder"
+recorderScript="${recorderFolder}/run-validations.sh"
+if [[ ! -f "${recorderScript}" ]]; then
+  echo "Skipping running spec validation tests, not found: ${recorderScript}"
+  exit
+fi
 
 # assumes the flight recorder is checked out next to generator
-pushd ../clients-flight-recorder
+pushd "${recorderFolder}"
+function finish { popd; }
+trap finish EXIT
+./run-validations.sh $@
 
 
 
