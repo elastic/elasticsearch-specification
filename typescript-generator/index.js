377ad8b4a4237075cfdfe898701a813173db6c72
Martijn Laarman <Mpdreamz@gmail.com>
2021-01-19 19:05:30 +0100

    update index reader to traverse for trait information

--- typescript-generator/index.js
+++ typescript-generator/index.js
@@ -186,8 +186,8 @@ function isSpecialInterface (type) {
   if (/^Cat.*Response$/g.test(type.name.name)) {
     return true
   }
-  if (Array.isArray(type.implements)) {
-    return !!type.implements.find(i=>i.type.name === 'IDictionary');
+  if (Array.isArray(type.traits)) {
+    return type.traits.length > 0
   }
   switch (type.name.name) {
     case 'DictionaryResponseBase':
@@ -197,25 +197,40 @@ function isSpecialInterface (type) {
   }
 }
 
+function lookupTraitInterface(type, name) {
+  const dictionaryOf = type.implements.find(i => i.type.name === name);
+  if (dictionaryOf) return dictionaryOf;
+  if (Array.isArray(t.inherits)) return lookupTraitInterface(t.inherits[0], name)
+  return null;
+}
+
+function serializeDictionaryTraitType(type) {
+  const dictionaryOf = lookupTraitInterface(type, 'IDictionary');
+  if (!dictionaryOf) throw new Error(`Unknown implements ${type.name.name}`)
+  //export type SingleBucketAggregate = { doc_count: double } & Record<string, Aggregate>
+  let code = `export interface ${type.name.name}Keys${buildGenerics(type)}${buildInherits(type)} {\n`
+
+  function required(property) {
+    return property.required ? '' : '?'
+  }
+
+  for (const property of type.properties) {
+    code += `  ${cleanPropertyName(property.name)}${required(property)}: ${buildType(property.type)}\n`
+  }
+  code += '}\n'
+  code += `export type ${type.name.name}${buildGenerics(type)} = ${type.name.name}Keys${buildGenerics(type, true)} |\n`
+  code += `    { ${buildIndexer(dictionaryOf)} }\n`
+  return code
+}
+
 function serializeSpecialInterface (type) {
   if (/^Cat.*Response$/g.test(type.name.name)) {
     return buildCatResponse(type)
   }
-  if (Array.isArray(type.implements)) {
-    const dictionaryOf = type.implements.find(i=>i.type.name === 'IDictionary');
-    if (!dictionaryOf) throw new Error(`Unknown implements ${type.name.name}`)
-    //export type SingleBucketAggregate = { doc_count: double } & Record<string, Aggregate>
-    let code = `export interface ${type.name.name}Keys${buildGenerics(type)}${buildInherits(type)} {\n`
-    function required (property) {
-      return property.required ? '' : '?'
+  if (Array.isArray(type.traits)) {
+    if (type.traits.includes('IDictionary')) {
+      return serializeDictionaryTraitType(type);
     }
-    for (const property of type.properties) {
-      code += `  ${cleanPropertyName(property.name)}${required(property)}: ${buildType(property.type)}\n`
-    }
-    code += '}\n'
-    code += `export type ${type.name.name}${buildGenerics(type)} = ${type.name.name}Keys${buildGenerics(type, true)} |\n`
-    code += `    { ${buildIndexer(dictionaryOf)} }\n`
-    return code
   }
   switch (type.name.name) {
     case 'DictionaryResponseBase':
