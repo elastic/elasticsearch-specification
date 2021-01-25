0529a909d247a225e7cc5d72705d5a586f3d7d20
Steve Gordon <sgordon@hotmail.co.uk>
2021-01-25 19:55:38 +0000

    Fix snapshot create response

--- specification/specs/modules/snapshot_and_restore/snapshot/SnapshotInfo.ts
+++ specification/specs/modules/snapshot_and_restore/snapshot/SnapshotInfo.ts
@@ -1,13 +1,19 @@
 class SnapshotInfo {
-  duration_in_millis: long
-  end_time: Date
-  end_time_in_millis: long
-  failures: SnapshotShardFailure[]
-  indices: IndexName[]
-  metadata: Dictionary<string, UserDefinedValue>
-  snapshot: string
-  shards: ShardStatistics
-  start_time: Date
-  start_time_in_millis: long
-  state: string
+  data_streams: Array<string>;
+  duration_in_millis: long;
+  end_time?: Date;
+  end_time_in_millis?: long;
+  failures?: SnapshotShardFailure[];
+  include_global_state?: boolean;
+  indices: IndexName[];
+  metadata?: Dictionary<string, UserDefinedValue>;
+  reason?: string;
+  snapshot: string;
+  shards?: ShardStatistics;
+  start_time?: Date;
+  start_time_in_millis?: long;
+  state?: string;
+  uuid: string;
+  version?: string;
+  version_id?: integer;
 }
