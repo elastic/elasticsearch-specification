d6a408f8f04da32fedae83fd22d17dff3a83f6ed
Martijn Laarman <Mpdreamz@gmail.com>
2021-01-28 16:49:41 +0100

    Update contrib command to install deps too
    Changed the order to run the code cleanup and adding headers after we
    generate the output specification

--- .github/add-license-headers.sh
+++ .github/add-license-headers.sh
@@ -3,7 +3,7 @@ script_path=$(dirname $(realpath -s $0))/../
 
 function add_license () {
 
-    (find "$script_path" -name $1 | grep -v "/node_modules/" | grep -v "/output/")|while read fname; do
+    (find "$script_path" -name $1 | grep -v "/node_modules/" | grep -v "/output/" | grep -v "/specification/lib/")|while read fname; do
         line=$(sed -n '2p;3q' "$fname")
         if ! [[ "$line" == " * Licensed to Elasticsearch B.V. under one or more contributor" ]] ; then
            cat "${script_path}.github/license-header.txt" "$fname" > "${fname}.new"
