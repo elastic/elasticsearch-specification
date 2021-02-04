d20760de107d8d8480f75860b77c8f582efccd6b
elastic <philip@philipkrauss.at>
2021-02-04 16:29:53 +0100

    added metrics type

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -1982,7 +1982,7 @@ export interface ClusterRerouteRequest extends RequestBase {
   dry_run?: boolean
   explain?: boolean
   master_timeout?: Time
-  metric?: string | Array<string>
+  metric?: Metrics
   retry_failed?: boolean
   timeout?: Time
   body?: {
