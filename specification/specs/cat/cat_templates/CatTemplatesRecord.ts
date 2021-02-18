5074fd7fc07094015a7f1777752c68d42a734246
elastic <philip@philipkrauss.at>
2021-02-18 20:25:54 +0100

    Validate cat.templates Response

--- specification/specs/cat/cat_templates/CatTemplatesRecord.ts
+++ specification/specs/cat/cat_templates/CatTemplatesRecord.ts
@@ -21,8 +21,8 @@ class CatTemplatesRecord {
   index_patterns: string
   name: string
   /** @prop_serializer StringLongFormatter */
-  order: long
-  /** @prop_serializer NullableStringLongFormatter */
-  version: long
-  composed_of: string
+  order?: string
+  /** @prop_serializer StringLongFormatter */
+  version?: string
+  composed_of?: string
 }
