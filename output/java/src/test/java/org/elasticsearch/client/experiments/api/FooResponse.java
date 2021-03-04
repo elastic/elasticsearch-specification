eaf13933d317ebdfdff31b097026a6bcaf9a308c
Martijn Laarman <Mpdreamz@gmail.com>
2021-03-04 19:24:31 +0100

    Cleanup unused projects and output
    csharp and java generators are sunsetted!
    
    All welcome the new `elastic-client-generator-*` repositories that use
    the canonical json!

--- output/java/src/test/java/org/elasticsearch/client/experiments/api/FooResponse.java
+++
@@ -1,9 +0,0 @@
-package org.elasticsearch.client.experiments.api;
-
-import org.elasticsearch.client.experiments.base.FromXContent;
-import org.elasticsearch.common.xcontent.ContextParser;
-
-public class FooResponse {
-
-  public static ContextParser<FromXContent.Params, FooResponse> PARSER = null;
-}
