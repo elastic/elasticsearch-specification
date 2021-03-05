7540096063ca2958952d39226b3d012d3d436085
Martijn Laarman <Mpdreamz@gmail.com>
2021-03-05 11:09:48 +0100

    Cleanup unused projects and output (#171)
    csharp and java generators are sunsetted!
    
    All welcome the new `elastic-client-generator-*` repositories that use
    the canonical json!

--- output/java/src/test/java/org/elasticsearch/client/experiments/base/FromXContent.java
+++
@@ -1,9 +0,0 @@
-package org.elasticsearch.client.experiments.base;
-
-public interface FromXContent<T> {
-  public final static Params EMPTY_PARAMS = new Params();
-
-  public static class Params {
-    public boolean lenient;
-  }
-}
--- output/java/src/test/java/org/elasticsearch/client/experiments/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Experiments for the design of high-level client classes and usage illustration of the XContent-related classes.
- */
-package org.elasticsearch.client.experiments;
