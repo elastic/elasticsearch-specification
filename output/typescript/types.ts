7d7a19d2ea1dcae3b06826e92abef2232bd9250d
elastic <philip@philipkrauss.at>
2021-03-08 13:53:31 +0100

    fix cat.allocations response

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -952,16 +952,16 @@ export interface CatAliasesRequest extends CatRequestBase {
 export type CatAliasesResponse = CatAliasesRecord[]
 
 export interface CatAllocationRecord {
-  'disk.avail': string
-  'disk.indices': string
-  'disk.percent': string
-  disk_ratio: string
-  'disk.total': string
-  'disk.used': string
-  host: string
-  ip: string
+  'disk.avail'?: ByteSize
+  'disk.indices'?: ByteSize
+  'disk.percent'?: Percentage
+  disk_ratio?: string
+  'disk.total'?: ByteSize
+  'disk.used'?: ByteSize
+  host?: string
+  ip?: string
   node: string
-  shards: string
+  shards?: string
 }
 
 export interface CatAllocationRequest extends CatRequestBase {
