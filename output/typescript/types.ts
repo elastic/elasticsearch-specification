e31b0d9bc9f84ad542f2fd8d21f0038b97ca6d91
elastic <philip@philipkrauss.at>
2021-03-09 20:10:36 +0100

    security.authenticate response

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -626,8 +626,8 @@ export interface AuthenticateRequest extends RequestBase {
 
 export interface AuthenticateResponse extends ResponseBase {
   authentication_realm: RealmInfo
-  email: string
-  full_name: string
+  email?: string
+  full_name?: string
   lookup_realm: RealmInfo
   metadata: Record<string, any>
   roles: Array<string>
--- specification/specs/x_pack/security/authenticate/AuthenticateResponse.ts
+++ specification/specs/x_pack/security/authenticate/AuthenticateResponse.ts
@@ -19,8 +19,8 @@
 
 class AuthenticateResponse extends ResponseBase {
   authentication_realm: RealmInfo
-  email: string
-  full_name: string
+  email?: string
+  full_name?: string
   lookup_realm: RealmInfo
   metadata: Dictionary<string, UserDefinedValue>
   roles: string[]
