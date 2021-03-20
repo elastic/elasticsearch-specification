ef06c8be857defc3fc329929214c6726b03e1598
philkra <philip@philipkrauss.at>
2021-03-20 19:14:08 +0100

    missing new line

--- specification/specs/search/multi_search_template/MultiSearchTemplateResponse.ts
+++ specification/specs/search/multi_search_template/MultiSearchTemplateResponse.ts
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 class MultiSearchTemplateResponse extends ResponseBase {
   responses: SearchResponse<UserDefinedValue>[]
   took: long
