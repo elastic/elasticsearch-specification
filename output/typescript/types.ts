985bc50a347409f23f7da54ef5c09a92d54f53f2
philkra <philip@philipkrauss.at>
2021-03-28 19:56:46 +0200

    Fix cluster.get_component_template Request

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -3078,11 +3078,10 @@ export interface ClusterFileSystem {
 }
 
 export interface ClusterGetComponentTemplateRequest extends RequestBase {
-  stub_a: string
-  stub_b: string
-  body?: {
-    stub_c: string
-  }
+  name?: Name
+  flat_settings?: boolean
+  local?: boolean
+  master_timeout?: Time
 }
 
 export interface ClusterGetComponentTemplateResponse extends ResponseBase {
