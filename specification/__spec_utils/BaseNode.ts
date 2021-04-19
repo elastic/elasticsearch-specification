c50e1c8b9eb727340f19bf3cd8d09bcf8f3c4106
philkra <philip@philipkrauss.at>
2021-04-19 14:57:03 +0200

    move to networking

--- specification/__spec_utils/BaseNode.ts
+++ specification/__spec_utils/BaseNode.ts
@@ -20,7 +20,7 @@
 import { Name } from '@common/common'
 import { NodeRole, NodeRoles } from '@nodes/nodes_info/NodeRole'
 import { Dictionary } from './Dictionary'
-import { Host, Ip, TransportAddress } from './Networking'
+import { Host, Ip, TransportAddress } from '../__common/common/Networking'
 
 export class BaseNode {
   attributes: Dictionary<string, string>
