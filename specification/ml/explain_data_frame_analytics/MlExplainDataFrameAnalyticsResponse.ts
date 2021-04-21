d3182ebcd8b1cf3f8e143ec786334b0fc13dedad
philkra <philip@philipkrauss.at>
2021-04-21 17:14:31 +0200

    make contrib

--- specification/ml/explain_data_frame_analytics/MlExplainDataFrameAnalyticsResponse.ts
+++ specification/ml/explain_data_frame_analytics/MlExplainDataFrameAnalyticsResponse.ts
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { ResponseBase } from '@common/common_abstractions/response/ResponseBase'
+import { ResponseBase } from '@_types/Base'
 
 export class MlExplainDataFrameAnalyticsResponse extends ResponseBase {
   stub: boolean
