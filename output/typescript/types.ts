0d1939fee5b0327528fd259b9ecd516e28f1cbe4
philkra <philip@philipkrauss.at>
2021-04-26 20:27:14 +0200

    Fix security.authenticate Response

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -11529,9 +11529,6 @@ export interface XpackUsageSearchableSnapshotsUsage extends XpackUsageXPackUsage
 }
 
 export interface SecurityAuthenticateSecurityAuthenticateRequest extends RequestBase {
-  body?: {
-    stub: integer
-  }
 }
 
 export interface SecurityAuthenticateSecurityAuthenticateResponse extends ResponseBase {
@@ -11544,6 +11541,11 @@ export interface SecurityAuthenticateSecurityAuthenticateResponse extends Respon
   username: Username
   enabled: boolean
   authentication_type: string
+  token?: SecurityAuthenticateSecurityAuthenticateToken
+}
+
+export interface SecurityAuthenticateSecurityAuthenticateToken {
+  name: Name
 }
 
 export interface SecurityChangePasswordSecurityChangePasswordRequest extends RequestBase {
