4d35208272173bff8c001603d50fc7773c37d7cf
Philip Krauss <35487337+philkra@users.noreply.github.com>
2021-04-27 08:13:13 +0200

    Fix msearch_template Request (#382)

--- specification/_global/msearch_template/MultiSearchTemplateResponse.ts
+++ specification/_global/msearch_template/MultiSearchTemplateResponse.ts
@@ -22,7 +22,7 @@ import { UserDefinedValue } from '@spec_utils/UserDefinedValue'
 import { ResponseBase } from '@_types/Base'
 import { long } from '@_types/Numeric'
 
-export class MultiSearchTemplateResponse extends ResponseBase {
-  responses: SearchResponse<UserDefinedValue>[]
+export class MultiSearchTemplateResponse<TDocument> extends ResponseBase {
+  responses: SearchResponse<TDocument>[]
   took: long
 }
