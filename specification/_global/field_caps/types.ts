2f320f84c1471c68fd868325035431b51baf4901
philkra <philip@philipkrauss.at>
2021-05-04 17:44:01 +0200

    rename

--- specification/_global/field_caps/types.ts
+++ specification/_global/field_caps/types.ts
@@ -22,12 +22,9 @@ import { Indices } from '@_types/common'
 
 export class FieldCapabilities {
   aggregatable: boolean
-  /** @prop_serializer IndicesFormatter */
   indices?: Indices
   meta?: Dictionary<string, string[]>
-  /** @prop_serializer IndicesFormatter */
   non_aggregatable_indices?: Indices
-  /** @prop_serializer IndicesFormatter */
   non_searchable_indices?: Indices
   searchable: boolean
   type: string
