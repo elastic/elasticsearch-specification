5aff24a416b2813187e3a24839609d79982a029f
delvedor <tommydelved@gmail.com>
2021-05-10 11:27:11 +0200

    Reorg cat

---
+++ specification/cat/snapshots/types.ts
@@ -0,0 +1,88 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { DateString, EpochMillis, Time } from '@_types/Time'
+
+export class SnapshotsRecord {
+  /**
+   * unique snapshot
+   * @aliases snapshot
+   */
+  'id'?: string
+  /**
+   * repository name
+   * @aliases re,repo
+   */
+  'repository'?: string
+  /**
+   * snapshot name
+   * @aliases s
+   */
+  'status'?: string
+  /**
+   * start time in seconds since 1970-01-01 00:00:00
+   * @aliases ste,startEpoch
+   */
+  'start_epoch'?: EpochMillis
+  /**
+   * start time in HH:MM:SS
+   * @aliases sti,startTime
+   */
+  'start_time'?: DateString
+  /**
+   * end time in seconds since 1970-01-01 00:00:00
+   * @aliases ete,endEpoch
+   */
+  'end_epoch'?: EpochMillis
+  /**
+   * end time in HH:MM:SS
+   * @aliases eti,endTime
+   */
+  'end_time'?: DateString
+  /**
+   * duration
+   * @aliases dur
+   */
+  'duration'?: Time
+  /**
+   * number of indices
+   * @aliases i
+   */
+  'indices'?: string
+  /**
+   * number of successful shards
+   * @aliases ss
+   */
+  'successful_shards'?: string
+  /**
+   * number of failed shards
+   * @aliases fs
+   */
+  'failed_shards'?: string
+  /**
+   * number of total shards
+   * @aliases ts
+   */
+  'total_shards'?: string
+  /**
+   * reason for failures
+   * @aliases r
+   */
+  'reason'?: string
+}
---
+++ specification/cat/tasks/types.ts
@@ -0,0 +1,101 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Id, NodeId, Type, VersionString } from '@_types/common'
+
+export class TasksRecord {
+  /**
+   * id of the task with the node
+   */
+  'id'?: Id
+  /**
+   * task action
+   * @aliases ac
+   */
+  'action'?: string
+  /**
+   * unique task id
+   * @aliases ti
+   */
+  'task_id'?: Id
+  /**
+   * parent task id
+   * @aliases pti
+   */
+  'parent_task_id'?: string
+  /**
+   * task type
+   * @aliases ty
+   */
+  'type'?: Type
+  /**
+   * start time in ms
+   * @aliases start
+   */
+  'start_time'?: string
+  /**
+   * start time in HH:MM:SS
+   * @aliases ts,hms,hhmmss
+   */
+  'timestamp'?: string
+  /**
+   * running time ns
+   */
+  'running_time_ns'?: string
+  /**
+   * running time
+   * @aliases time
+   */
+  'running_time'?: string
+  /**
+   * unique node id
+   * @aliases ni
+   */
+  'node_id'?: NodeId
+  /**
+   * ip address
+   * @aliases i
+   */
+  'ip'?: string
+  /**
+   * bound transport port
+   * @aliases po
+   */
+  'port'?: string
+  /**
+   * node name
+   * @aliases n
+   */
+  'node'?: string
+  /**
+   * es version
+   * @aliases v
+   */
+  'version'?: VersionString
+  /**
+   * X-Opaque-ID header
+   * @aliases x
+   */
+  'x_opaque_id'?: string
+  /**
+   * task action
+   * @aliases desc
+   */
+  'description'?: string
+}
