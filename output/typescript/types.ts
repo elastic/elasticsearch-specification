925b30bb73c94d41ad2c80ac56d3a6f609f200c9
Seth Michael Larson <seth.larson@elastic.co>
2021-07-29 14:29:22 -0500

    Validate 'ml.put_calendar' API

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -11606,13 +11606,14 @@ export interface MlPreviewDatafeedResponse<TDocument = unknown> {
 export interface MlPutCalendarRequest extends RequestBase {
   calendar_id: Id
   body?: {
+    job_ids?: Ids
     description?: string
   }
 }
 
 export interface MlPutCalendarResponse {
   calendar_id: Id
-  description: string
+  description?: string
   job_ids: Ids
 }
 
