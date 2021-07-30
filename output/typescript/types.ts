0cde0939dc26d5abff229237015876ec203829e2
Seth Michael Larson <seth.larson@elastic.co>
2021-07-30 09:18:50 -0500

    Validate 'ml.post_data' API (#515)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -11554,18 +11554,22 @@ export interface MlPostCalendarEventsResponse {
   events: MlCalendarEvent[]
 }
 
-export interface MlPostJobDataRequest extends RequestBase {
+export type MlPostDataInput = any | MlPostDataMultipleInputs
+
+export interface MlPostDataMultipleInputs {
+  data: any[]
+}
+
+export interface MlPostDataRequest extends RequestBase {
   job_id: Id
   reset_end?: DateString
   reset_start?: DateString
-  body?: {
-    data?: any[]
-  }
+  body?: MlPostDataInput
 }
 
-export interface MlPostJobDataResponse {
+export interface MlPostDataResponse {
   bucket_count: long
-  earliest_record_timestamp: integer
+  earliest_record_timestamp?: integer
   empty_bucket_count: long
   input_bytes: long
   input_field_count: long
@@ -11573,7 +11577,7 @@ export interface MlPostJobDataResponse {
   invalid_date_count: long
   job_id: Id
   last_data_time: integer
-  latest_record_timestamp: integer
+  latest_record_timestamp?: integer
   missing_field_count: long
   out_of_order_timestamp_count: long
   processed_field_count: long
