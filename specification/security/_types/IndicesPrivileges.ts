0a84075d9008725efd38a7de8af6ce5ae95ee446
Adam Locke <adam.locke@elastic.co>
2021-08-17 13:39:04 -0400

    [DOCS] Update indices parameter descriptions

--- specification/security/_types/IndicesPrivileges.ts
+++ specification/security/_types/IndicesPrivileges.ts
@@ -22,9 +22,26 @@ import { Indices } from '@_types/common'
 import { QueryContainer } from '@_types/query_dsl/abstractions'
 
 export class IndicesPrivileges {
+  /**
+   * The document fields that the owners of the role have read access to.
+   * @doc_url https://www.elastic.co/guide/en/elasticsearch/reference/current/field-and-document-access-control.html
+   */
   field_security?: FieldSecurity
+  /**
+   * A list of indices (or index name patterns) to which the permissions in this entry apply.
+   */
   names: Indices
+  /**
+   * The index level privileges that owners of the role have on the specified indices.
+   */
   privileges: string[]
+  /**
+   * A search query that defines the documents the owners of the role have read access to. A document within the specified indices must match this query for it to be accessible by the owners of the role.
+   */
   query?: string | QueryContainer
+  /**
+   * Set to `true` if using wildcard or regular expressions for patterns that cover restricted indices. Implicitly, restricted indices have limited privileges that can cause pattern tests to fail. If restricted indices are explicitly included in the `names` list, Elasticsearch checks privileges against these indices regardless of the value set for `allow_restricted_indices`.
+   * @server_default false
+   */
   allow_restricted_indices?: boolean
 }
