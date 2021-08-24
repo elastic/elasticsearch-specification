1aef91ab2bfa8a34995e367c370f0e09d8e1883b
delvedor <tommydelved@gmail.com>
2021-08-24 16:51:37 +0200

    Nit

--- specification/features/reset_features/ResetFeaturesRequest.ts
+++ specification/features/reset_features/ResetFeaturesRequest.ts
@@ -22,6 +22,6 @@ import { RequestBase } from '@_types/Base'
 /**
  * @rest_spec_name features.reset_features
  * @since 7.12.0
- * @stability TODO
+ * @stability experimental
  */
 export interface Request extends RequestBase {}
