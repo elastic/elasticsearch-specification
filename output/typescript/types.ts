d60dc12953dd9a10551c3ee110356993d418d0e1
philkra <philip@philipkrauss.at>
2021-08-24 14:18:26 +0200

    fix transform requests

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -798,7 +798,7 @@ export interface RankEvalUnratedDocument {
 }
 
 export interface ReindexDestination {
-  index: IndexName
+  index?: IndexName
   op_type?: OpType
   pipeline?: string
   routing?: Routing
@@ -859,6 +859,12 @@ export interface ReindexSource {
   slice?: SlicedScroll
   sort?: SearchSort
   _source?: Fields
+  runtime_mappings?: Record<string, ReindexSourceRuntimeMapping>
+}
+
+export interface ReindexSourceRuntimeMapping {
+  type: string
+  script?: string
 }
 
 export interface ReindexRethrottleReindexNode extends SpecUtilsBaseNode {
