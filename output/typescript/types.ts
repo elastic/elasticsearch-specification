41ee977605f5912863a65c774c46e97a5d1dcd77
Sylvain Wallez <sylvain@elastic.co>
2021-08-25 12:52:10 +0200

    Add shutdown API specs (#592)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13469,33 +13469,52 @@ export interface SecurityPutUserResponse {
 }
 
 export interface ShutdownDeleteNodeRequest extends RequestBase {
-  body?: {
-    stub: string
-  }
+  node_id: NodeId
 }
 
-export interface ShutdownDeleteNodeResponse {
-  stub: boolean
+export interface ShutdownDeleteNodeResponse extends AcknowledgedResponseBase {
+}
+
+export interface ShutdownGetNodeNodeShutdownStatus {
+  node_id: NodeId
+  type: ShutdownGetNodeShutdownType
+  reason: string
+  shutdown_startedmillis: EpochMillis
+  status: ShutdownGetNodeShutdownStatus
+  shard_migration: ShutdownGetNodeShardMigrationStatus
+  persistent_tasks: ShutdownGetNodePersistentTaskStatus
+  plugins: ShutdownGetNodePluginsStatus
+}
+
+export interface ShutdownGetNodePersistentTaskStatus {
+  status: ShutdownGetNodeShutdownStatus
+}
+
+export interface ShutdownGetNodePluginsStatus {
+  status: ShutdownGetNodeShutdownStatus
 }
 
 export interface ShutdownGetNodeRequest extends RequestBase {
-  body?: {
-    stub: string
-  }
+  node_id: NodeId[]
 }
 
 export interface ShutdownGetNodeResponse {
-  stub: boolean
+  nodes: ShutdownGetNodeNodeShutdownStatus[]
 }
+
+export interface ShutdownGetNodeShardMigrationStatus {
+  status: ShutdownGetNodeShutdownStatus
+}
+
+export type ShutdownGetNodeShutdownStatus = 'not_started' | 'in_progress' | 'stalled' | 'complete'
+
+export type ShutdownGetNodeShutdownType = 'remove' | 'restart'
 
 export interface ShutdownPutNodeRequest extends RequestBase {
-  body?: {
-    stub: string
-  }
+  node_id: NodeId
 }
 
-export interface ShutdownPutNodeResponse {
-  stub: boolean
+export interface ShutdownPutNodeResponse extends AcknowledgedResponseBase {
 }
 
 export interface SlmConfiguration {
