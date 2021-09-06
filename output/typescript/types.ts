22f3b4daaf63cfba277d3dfb62fb8389fcdd7c07
Philip Krauss <35487337+philkra@users.noreply.github.com>
2021-09-06 09:20:50 +0200

    Fix security.authenticate Response (#679)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13011,6 +13011,7 @@ export interface SecurityAuthenticateResponse {
 
 export interface SecurityAuthenticateToken {
   name: Name
+  type?: string
 }
 
 export interface SecurityChangePasswordRequest extends RequestBase {
