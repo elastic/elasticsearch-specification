0ab58b1a4dda3c9e98376b412998adeadb47924f
Philip Krauss <35487337+philkra@users.noreply.github.com>
2021-09-08 09:47:27 +0200

    Fix cluster.stats Response (#697)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -7677,7 +7677,7 @@ export interface ClusterStatsClusterOperatingSystem {
   available_processors: integer
   mem: ClusterStatsOperatingSystemMemoryInfo
   names: ClusterStatsClusterOperatingSystemName[]
-  pretty_names: ClusterStatsClusterOperatingSystemName[]
+  pretty_names: ClusterStatsClusterOperatingSystemPrettyName[]
   architectures?: ClusterStatsClusterOperatingSystemArchitecture[]
 }
 
@@ -7689,6 +7689,11 @@ export interface ClusterStatsClusterOperatingSystemArchitecture {
 export interface ClusterStatsClusterOperatingSystemName {
   count: integer
   name: Name
+}
+
+export interface ClusterStatsClusterOperatingSystemPrettyName {
+  count: integer
+  pretty_name: Name
 }
 
 export interface ClusterStatsClusterProcess {
