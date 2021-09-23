b3743721ebee8bfb3466a37256f7e2c4f5c6c280
lcawl <lcawley@elastic.co>
2021-09-22 18:20:49 -0700

    Refresh rank_eval and modeling guide

--- specification/_global/rank_eval/RankEvalRequest.ts
+++ specification/_global/rank_eval/RankEvalRequest.ts
@@ -22,38 +22,39 @@ import { ExpandWildcards, Indices } from '@_types/common'
 import { RankEvalMetric, RankEvalRequestItem } from './types'
 
 /**
+ * Enables you to evaluate the quality of ranked search results over a set of typical search queries.
  * @rest_spec_name rank_eval
  * @since 6.2.0
  * @stability experimental
+ * @index_privileges read
  */
 export interface Request extends RequestBase {
   path_parts: {
     /**
-     * Comma-separated list of data streams, indices, and index aliases used to limit the request. Wildcard (*) expressions are supported.
-     * To target all data streams and indices in a cluster, omit this parameter or use _all or *.
+     * Comma-separated list of data streams, indices, and index aliases used to limit the request. Wildcard (`*`) expressions are supported.
+     * To target all data streams and indices in a cluster, omit this parameter or use `_all` or `*`.
      */
     index: Indices
   }
   query_parameters: {
     /**
-     * If false, the request returns an error if any wildcard expression, index alias, or _all value targets only missing or closed indices. This behavior applies even if the request targets other open indices. For example, a request targeting foo*,bar* returns an error if an index starts with foo but no index starts with bar.
+     * If `false`, the request returns an error if any wildcard expression, index alias, or `_all` value targets only missing or closed indices. This behavior applies even if the request targets other open indices. For example, a request targeting `foo*,bar*` returns an error if an index starts with `foo` but no index starts with `bar`.
      * @server_default true
      */
     allow_no_indices?: boolean
     expand_wildcards?: ExpandWildcards
     /**
-     * If true, missing or closed indices are not included in the response.
+     * If `true`, missing or closed indices are not included in the response.
      * @server_default false
      */
     ignore_unavailable?: boolean
     search_type?: string
   }
   body: {
-    /** A set of typical search requests, together with their provided ratings */
+    /** A set of typical search requests, together with their provided ratings. */
     requests: RankEvalRequestItem[]
     /**
-     * Definition of the evaluation metric to calculate
-     * @doc_url https://www.elastic.co/guide/en/elasticsearch/reference/current/search-rank-eval.html#_available_evaluation_metrics
+     * Definition of the evaluation metric to calculate.
      */
     metric?: RankEvalMetric
   }
