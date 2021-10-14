053415f5aa73bb1a43352a12338d84fb09c3ab4e
István Zoltán Szabó <istvan.szabo@elastic.co>
2021-10-14 15:30:37 +0200

    [ML] Updates specs for delete filter, forecast, job, and model snapshot APIs

--- specification/ml/delete_model_snapshot/MlDeleteModelSnapshotRequest.ts
+++ specification/ml/delete_model_snapshot/MlDeleteModelSnapshotRequest.ts
@@ -21,13 +21,24 @@ import { RequestBase } from '@_types/Base'
 import { Id } from '@_types/common'
 
 /**
+ * Deletes an existing model snapshot.
+ * You cannot delete the active model snapshot. To delete that snapshot, first
+ * revert to a different one. To identify the active model snapshot, refer to
+ * the `model_snapshot_id` in the results from the get jobs API.
  * @rest_spec_name ml.delete_model_snapshot
  * @since 5.4.0
  * @stability stable
+ * @cluster_privileges manage_ml
  */
 export interface Request extends RequestBase {
   path_parts: {
+    /**
+     * Identifier for the anomaly detection job.
+     */
     job_id: Id
+    /**
+     * Identifier for the model snapshot.
+     */
     snapshot_id: Id
   }
 }
