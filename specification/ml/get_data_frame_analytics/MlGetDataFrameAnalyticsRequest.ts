ec18aabdddce104cac2cc84a3d6296720570039d
István Zoltán Szabó <szabosteve@gmail.com>
2021-10-15 16:00:30 +0200

    [ML] Updates spec for GET DFA API.

--- specification/ml/get_data_frame_analytics/MlGetDataFrameAnalyticsRequest.ts
+++ specification/ml/get_data_frame_analytics/MlGetDataFrameAnalyticsRequest.ts
@@ -22,17 +22,37 @@ import { Id } from '@_types/common'
 import { integer } from '@_types/Numeric'
 
 /**
+ * Retrieves configuration information for data frame analytics jobs.
+ * You can get information for multiple data frame analytics jobs in a single
+ * API request by using a comma-separated list of data frame analytics jobs or a
+ * wildcard expression.
  * @rest_spec_name ml.get_data_frame_analytics
  * @since 7.3.0
  * @stability stable
+ * @cluster_privileges monitor_ml
  */
 export interface Request extends RequestBase {
   path_parts: {
-    /** Identifier for the data frame analytics job. If you do not specify this option, the API returns information for the first hundred data frame analytics jobs. */
+    /**
+     * Identifier for the data frame analytics job. If you do not specify this
+     * option, the API returns information for the first hundred data frame
+     * analytics jobs.
+     */
     id?: Id
   }
   query_parameters: {
     /**
+     * Specifies what to do when the request:
+     *
+     * 1. Contains wildcard expressions and there are no data frame analytics
+     * jobs that match.
+     * 2. Contains the _all string or no identifiers and there are no matches.
+     * 3. Contains wildcard expressions and there are only partial matches.
+     *
+     * The default value returns an empty data_frame_analytics array when there
+     * are no matches and the subset of results when there are partial matches.
+     * If this parameter is `false`, the request returns a 404 status code when
+     * there are no matches or only partial matches.
      * @server_default true
      */
     allow_no_match?: boolean
@@ -47,7 +67,9 @@ export interface Request extends RequestBase {
      */
     size?: integer
     /**
-     * Indicates if certain fields should be removed from the configuration on retrieval. This allows the configuration to be in an acceptable format to be retrieved and then added to another cluster.
+     * Indicates if certain fields should be removed from the configuration on
+     * retrieval. This allows the configuration to be in an acceptable format to
+     * be retrieved and then added to another cluster.
      * @server_default false
      */
     exclude_generated?: boolean
