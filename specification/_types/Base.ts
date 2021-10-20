caf0c63528e1cace3a072942cab353f0074639b0
Tomas Della Vedova <delvedor@users.noreply.github.com>
2021-10-20 17:31:06 +0200

    Update ErrorResponseBase (#923)
    Co-authored-by: Sylvain Wallez <sylvain@elastic.co>

--- specification/_types/Base.ts
+++ specification/_types/Base.ts
@@ -71,16 +71,12 @@ export class ElasticsearchVersionInfo {
  * The response returned by Elasticsearch when request execution did not succeed.
  */
 export class ErrorResponseBase {
-  error: TopLevelError
+  // In some edge cases `error` can be a string that is a shortcut to `error.reason`, for example if you call `GET _cat/foo`.
+  // If the error is a string, it means that it was not caused by an exception on ES side, but on the HTTP routing layer.
+  // This should never happen in clients, because we assume we will never send malformed request.
+  error: ErrorCause
   status: integer
 }
-
-/**
- * @shortcut_property reason
- */
-// Shortcut property needed to handle cases where the error is not caused by an exception. They happen only when the
-// HTTP routing layer rejects the request, and thus only at the top level. Example: "GET _cat/foo".
-export class TopLevelError extends ErrorCause {}
 
 export class IndicesResponseBase extends AcknowledgedResponseBase {
   _shards?: ShardStatistics
