9bb14a6bf5790d16756b0ab51b4fdab2fcac1a62
delvedor <tommydelved@gmail.com>
2021-10-25 10:38:12 +0200

    make contrib

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -1925,7 +1925,7 @@ export interface EmptyObject {
 export type EpochMillis = string | long
 
 export interface ErrorCauseKeys {
-  type?: string
+  type: string
   reason: string
   stack_trace?: string
   caused_by?: ErrorCause
