d170b7a392ffe6ab243575fffc6b21adb0357b80
lcawl <lcawley@elastic.co>
2021-11-03 13:11:46 -0700

    Add descriptions for stop datafeed API

--- specification/ml/stop_datafeed/MlStopDatafeedRequest.ts
+++ specification/ml/stop_datafeed/MlStopDatafeedRequest.ts
@@ -22,24 +22,65 @@ import { Id } from '@_types/common'
 import { Time } from '@_types/Time'
 
 /**
+ * Stops one or more datafeeds.
+ * A datafeed that is stopped ceases to retrieve data from Elasticsearch. A datafeed can be started and stopped
+ * multiple times throughout its lifecycle.
  * @rest_spec_name ml.stop_datafeed
  * @since 5.4.0
  * @stability stable
+ * @cluster_privileges manage_ml
  */
 export interface Request extends RequestBase {
   path_parts: {
+    /**
+     * Identifier for the datafeed. You can stop multiple datafeeds in a single API request by using a comma-separated
+     * list of datafeeds or a wildcard expression. You can close all datafeeds by using `_all` or by specifying `*` as
+     * the identifier.
+     */
     datafeed_id: Id
   }
   query_parameters: {
-    /** @server_default true */
+    /**
+     * Specifies what to do when the request:
+     * 
+     * * Contains wildcard expressions and there are no datafeeds that match.
+     * * Contains the `_all` string or no identifiers and there are no matches.
+     * * Contains wildcard expressions and there are only partial matches.
+     * 
+     * If `true`, the API returns an empty datafeeds array when there are no matches and the subset of results when
+     * there are partial matches. If `false`, the API returns a 404 status code when there are no matches or only
+     * partial matches. 
+     * @server_default true */
     allow_no_match?: boolean
-    /** @server_default false */
+    /**
+     * If `true`, the datafeed is stopped forcefully. 
+     * @server_default false */
     force?: boolean
+    /**
+     * Specifies the amount of time to wait until a datafeed stops.
+     *  @server_default 20s */
+    timeout?: Time
   }
   body: {
-    /** @server_default false */
+    /**
+     * Specifies what to do when the request:
+     * 
+     * * Contains wildcard expressions and there are no datafeeds that match.
+     * * Contains the `_all` string or no identifiers and there are no matches.
+     * * Contains wildcard expressions and there are only partial matches.
+     * 
+     * If `true`, the API returns an empty datafeeds array when there are no matches and the subset of results when
+     * there are partial matches. If `false`, the API returns a 404 status code when there are no matches or only
+     * partial matches. 
+     * @server_default true */
+    allow_no_match?: boolean
+    /**
+     * If `true`, the datafeed is stopped forcefully. 
+     * @server_default false */
     force?: boolean
-    /** @server_default 20s */
+    /**
+     * Specifies the amount of time to wait until a datafeed stops.
+     *  @server_default 20s */
     timeout?: Time
   }
 }
