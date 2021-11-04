898e71ed1eece590b26ef7b828b473262377ce36
István Zoltán Szabó <szabosteve@gmail.com>
2021-11-04 17:51:17 +0100

    [ML] Updates DFA and AD related API specs.

--- specification/ml/update_job/MlUpdateJobRequest.ts
+++ specification/ml/update_job/MlUpdateJobRequest.ts
@@ -31,27 +31,52 @@ import { long } from '@_types/Numeric'
 import { Time } from '@_types/Time'
 
 /**
+ * Updates certain properties of an anomaly detection job.
  * @rest_spec_name ml.update_job
  * @since 5.5.0
  * @stability stable
+ * @cluster_privileges manage_ml
  */
 export interface Request extends RequestBase {
   path_parts: {
-    /** Identifier for the job */
+    /**
+     * Identifier for the job.
+     */
     job_id: Id
   }
   body: {
+    /**
+     * Advanced configuration option. Specifies whether this job can open when
+     * there is insufficient machine learning node capacity for it to be
+     * immediately assigned to a node. If `false` and a machine learning node
+     * with capacity to run the job cannot immediately be found, the open
+     * anomaly detection jobs API returns an error. However, this is also
+     * subject to the cluster-wide `xpack.ml.max_lazy_ml_nodes` setting. If this
+     * option is set to `true`, the open anomaly detection jobs API does not
+     * return an error and the job waits in the opening state until sufficient
+     * machine learning node capacity is available.
+     * @server_default false
+     * @doc_url https://www.elastic.co/guide/en/elasticsearch/reference/master/ml-settings.html#advanced-ml-settings
+     */
     allow_lazy_open?: boolean
     analysis_limits?: AnalysisMemoryLimit
     /**
-     * Advanced configuration option. The time between each periodic persistence of the model.
-     * The default value is a randomized value between 3 to 4 hours, which avoids all jobs persisting at exactly the same time. The smallest allowed value is 1 hour.
-     * For very large models (several GB), persistence could take 10-20 minutes, so do not set the value too low.
-     * If the job is open when you make the update, you must stop the datafeed, close the job, then reopen the job and restart the datafeed for the changes to take effect.
+     * Advanced configuration option. The time between each periodic persistence
+     * of the model.
+     * The default value is a randomized value between 3 to 4 hours, which
+     * avoids all jobs persisting at exactly the same time. The smallest allowed
+     * value is 1 hour.
+     * For very large models (several GB), persistence could take 10-20 minutes,
+     * so do not set the value too low.
+     * If the job is open when you make the update, you must stop the datafeed,
+     * close the job, then reopen the job and restart the datafeed for the
+     * changes to take effect.
      */
     background_persist_interval?: Time
     /**
-     * Advanced configuration option. Contains custom meta data about the job. For example, it can contain custom URL information as shown in Adding custom URLs to machine learning results.
+     * Advanced configuration option. Contains custom meta data about the job.
+     * For example, it can contain custom URL information as shown in Adding
+     * custom URLs to machine learning results.
      * @doc_id ml.customUrls
      */
     custom_settings?: Dictionary<string, UserDefinedValue>
@@ -62,20 +87,38 @@ export interface Request extends RequestBase {
     description?: string
     model_plot_config?: ModelPlotConfig
     /**
+     * Advanced configuration option, which affects the automatic removal of old
+     * model snapshots for this job. It specifies a period of time (in days)
+     * after which only the first snapshot per day is retained. This period is
+     * relative to the timestamp of the most recent snapshot for this job. Valid
+     * values range from 0 to `model_snapshot_retention_days`. For jobs created
+     * before version 7.8.0, the default value matches
+     * `model_snapshot_retention_days`.
      * @server_default 1
+     * @doc_url https://www.elastic.co/guide/en/machine-learning/master/ml-ad-finding-anomalies.html#ml-ad-model-snapshots
      */
     daily_model_snapshot_retention_after_days?: long
     /**
-     * Advanced configuration option, which affects the automatic removal of old model snapshots for this job. It specifies the maximum period of time (in days) that snapshots are retained. This period is relative to the timestamp of the most recent snapshot for this job.
+     * Advanced configuration option, which affects the automatic removal of old
+     * model snapshots for this job. It specifies the maximum period of time (in
+     * days) that snapshots are retained. This period is relative to the
+     * timestamp of the most recent snapshot for this job.
      * @server_default 10
+     * @doc_url https://www.elastic.co/guide/en/machine-learning/master/ml-ad-finding-anomalies.html#ml-ad-model-snapshots
      */
     model_snapshot_retention_days?: long
     /**
-     * Advanced configuration option. The period over which adjustments to the score are applied, as new data is seen.
+     * Advanced configuration option. The period over which adjustments to the
+     * score are applied, as new data is seen.
      */
     renormalization_window_days?: long
     /**
-     * Advanced configuration option. The period of time (in days) that results are retained. Age is calculated relative to the timestamp of the latest bucket result. If this property has a non-null value, once per day at 00:30 (server time), results that are the specified number of days older than the latest bucket result are deleted from Elasticsearch. The default value is null, which means all results are retained.
+     * Advanced configuration option. The period of time (in days) that results
+     * are retained. Age is calculated relative to the timestamp of the latest
+     * bucket result. If this property has a non-null value, once per day at
+     * 00:30 (server time), results that are the specified number of days older
+     * than the latest bucket result are deleted from Elasticsearch. The default
+     * value is null, which means all results are retained.
      */
     results_retention_days?: long
     /**
