8fd3ef783e03622178467b16212041879367d33f
lcawl <lcawley@elastic.co>
2021-11-25 14:19:08 -0800

    Adds more intro

--- specification/transform/put_transform/PutTransformRequest.ts
+++ specification/transform/put_transform/PutTransformRequest.ts
@@ -32,8 +32,27 @@ import { Dictionary } from '@spec_utils/Dictionary'
 
 /**
  * Creates a transform.
- * You must have `create_index`, `index`, and `read` privileges on the destination index and `read` and
- * `view_index_metadata` privileges on the source indices.
+ * 
+ * A transform copies data from source indices, transforms it, and persists it into an entity-centric destination index. You can also think of the destination index as a two-dimensional tabular data structure (known as
+ * a data frame). The ID for each document in the data frame is generated from a hash of the entity, so there is a
+ * unique row per entity.
+ * 
+ * You must choose either the latest or pivot method for your transform; you cannot use both in a single transform. If
+ * you choose to use the pivot method for your transform, the entities are defined by the set of `group_by` fields in
+ * the pivot object. If you choose to use the latest method, the entities are defined by the `unique_key` field values
+ * in the latest object.
+ * 
+ * You must have `create_index`, `index`, and `read` privileges on the destination index and `read` and 
+ * `view_index_metadata` privileges on the source indices. When Elasticsearch security features are enabled, the
+ * transform remembers which roles the user that created it had at the time of creation and uses those same roles. If
+ * those roles do not have the required privileges on the source and destination indices, the transform fails when it
+ * attempts unauthorized operations.
+ * 
+ * NOTE: You must use Kibana or this API to create a transform. Do not add a transform directly into any
+ * `.transform-internal*` indices using the Elasticsearch index API. If Elasticsearch security features are enabled, do
+ * not give users any privileges on `.transform-internal*` indices. If you used transforms prior to 7.5, also do not
+ * give users any privileges on `.data-frame-internal*` indices.
+ * 
  * @rest_spec_name transform.put_transform
  * @since 7.2.0
  * @stability stable
@@ -49,8 +68,11 @@ export interface Request extends RequestBase {
   }
   query_parameters: {
     /**
-     * When `true`, deferrable validations are not run. This behavior may be desired if the source index does not exist
-     * until after the transform is created.
+     * When the transform is created, a series of validations occur to ensure its success. For example, there is a
+     * check for the existence of the source indices and a check that the destination index is not part of the source
+     * index pattern. You can use this parameter to skip the checks, for example when the source index does not exist
+     * until after the transform is created. The validations are always run when you start the transform, however, with
+     * the exception of privilege checks.
      * @server_default false
      */
     defer_validation?: boolean
