ca49817f5eeeda211f24d175422fb63393d7e0ec
Lisa Cawley <lcawley@elastic.co>
2021-11-30 11:08:58 -0800

    Adds missing details in the create transform API (#1067)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15223,7 +15223,6 @@ export interface TransformPivot {
   aggregations?: Record<string, AggregationsAggregationContainer>
   aggs?: Record<string, AggregationsAggregationContainer>
   group_by?: Record<string, TransformPivotGroupByContainer>
-  max_page_search_size?: integer
 }
 
 export interface TransformPivotGroupByContainer {
@@ -15243,6 +15242,7 @@ export interface TransformRetentionPolicyContainer {
 }
 
 export interface TransformSettings {
+  align_checkpoints?: boolean
   dates_as_epoch_millis?: boolean
   docs_per_second?: float
   max_page_search_size?: integer
@@ -15369,12 +15369,13 @@ export interface TransformPutTransformRequest extends RequestBase {
     dest: ReindexDestination
     description?: string
     frequency?: Time
-    pivot?: TransformPivot
-    source: ReindexSource
-    settings?: TransformSettings
-    sync?: TransformSyncContainer
-    retention_policy?: TransformRetentionPolicyContainer
     latest?: TransformLatest
+    _meta?: Record<string, string>
+    pivot?: TransformPivot
+    retention_policy?: TransformRetentionPolicyContainer
+    settings?: TransformSettings
+    source: ReindexSource
+    sync?: TransformSyncContainer
   }
 }
 
