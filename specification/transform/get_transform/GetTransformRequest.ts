284e9bffd70e7adea8fd54f381f9be70899a92c8
István Zoltán Szabó <szabosteve@gmail.com>
2021-12-06 16:46:11 +0100

    [Transform] Updates GET and GET stats Transform API specs.

--- specification/transform/get_transform/GetTransformRequest.ts
+++ specification/transform/get_transform/GetTransformRequest.ts
@@ -22,18 +22,51 @@ import { Name } from '@_types/common'
 import { integer } from '@_types/Numeric'
 
 /**
+ * Retrieves configuration information for transforms.
  * @rest_spec_name transform.get_transform
  * @since 7.5.0
  * @stability stable
+ * @cluster_privileges monitor_transform
  */
 export interface Request extends RequestBase {
   path_parts: {
+    /**
+     * Identifier for the transform. It can be a transform identifier or a
+     * wildcard expression. You can get information for all transforms by using
+     * `_all`, by specifying `*` as the `<transform_id>`, or by omitting the
+     * `<transform_id>`.
+     */
     transform_id?: Name
   }
   query_parameters: {
+    /**
+     * Specifies what to do when the request:
+     *
+     * 1. Contains wildcard expressions and there are no transforms that match.
+     * 2. Contains the _all string or no identifiers and there are no matches.
+     * 3. Contains wildcard expressions and there are only partial matches.
+     *
+     * If this parameter is false, the request returns a 404 status code when
+     * there are no matches or only partial matches.
+     * @server_default true
+     */
     allow_no_match?: boolean
+    /**
+     * Skips the specified number of transforms.
+     * @server_default 0
+     */
     from?: integer
+    /**
+     * Specifies the maximum number of transforms to obtain.
+     * @server_default 100
+     */
     size?: integer
+    /**
+     * Excludes fields that were automatically added when creating the
+     * transform. This allows the configuration to be in an acceptable format to
+     * be retrieved and then added to another cluster.
+     * @server_default false
+     */
     exclude_generated?: boolean
   }
 }
