9f5f35c92ad2839983bb7fa8522f3ceea7ee627b
Sylvain Wallez <sylvain@elastic.co>
2021-12-16 17:32:06 +0100

    Better with "make contrib"

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -1246,7 +1246,7 @@ export type SearchHighlighterType = SearchBuiltinHighlighterType | string
 export interface SearchHit<TDocument = unknown> {
   _index: IndexName
   _id: Id
-  _score?: double | null
+  _score: double | null
   _explanation?: ExplainExplanation
   fields?: Record<string, any>
   highlight?: Record<string, string[]>
@@ -1267,7 +1267,7 @@ export interface SearchHit<TDocument = unknown> {
 export interface SearchHitsMetadata<T = unknown> {
   total?: SearchTotalHits | long
   hits: SearchHit<T>[]
-  max_score?: double | null
+  max_score: double | null
 }
 
 export interface SearchInnerHits {
