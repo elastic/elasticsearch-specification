a824177b2f84e18bf3d8103e2401566b537001da
philkra <philip@philipkrauss.at>
2021-12-21 12:49:51 +0100

    do the linty lint

--- specification/ilm/explain_lifecycle/ExplainLifecycleRequest.ts
+++ specification/ilm/explain_lifecycle/ExplainLifecycleRequest.ts
@@ -49,11 +49,11 @@ export interface Request extends RequestBase {
      * Period to wait for a connection to the master node. If no response is received before the timeout expires, the request fails and returns an error.
      * @server_default 30s
      */
-     master_timeout?: Time
-     /**
-      * Period to wait for a response. If no response is received before the timeout expires, the request fails and returns an error.
-      * @server_default 30s
-      */
-     timeout?: Time
+    master_timeout?: Time
+    /**
+     * Period to wait for a response. If no response is received before the timeout expires, the request fails and returns an error.
+     * @server_default 30s
+     */
+    timeout?: Time
   }
 }
