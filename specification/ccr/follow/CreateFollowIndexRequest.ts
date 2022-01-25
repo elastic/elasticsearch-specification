268a91029d94a1e4b316c1e101ab4ed32bd1ccb4
lcawl <lcawley@elastic.co>
2022-01-24 18:32:32 -0800

    Add more doc_ids for CCR URLs

--- specification/ccr/follow/CreateFollowIndexRequest.ts
+++ specification/ccr/follow/CreateFollowIndexRequest.ts
@@ -26,6 +26,7 @@ import { Time } from '@_types/Time'
  * @rest_spec_name ccr.follow
  * @since 6.5.0
  * @stability stable
+ * @doc_id ccr-put-follow
  */
 export interface Request extends RequestBase {
   path_parts: {
