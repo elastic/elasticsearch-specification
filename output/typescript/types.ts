0dc39c6ef8ae356eb85803bc61704a063f0ff960
Sylvain Wallez <sylvain@elastic.co>
2022-01-25 18:30:31 +0100

    Make snapshot info in CreateSnapshotResponse optional (#1326)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15095,7 +15095,7 @@ export interface SnapshotCreateRequest extends RequestBase {
 
 export interface SnapshotCreateResponse {
   accepted?: boolean
-  snapshot: SnapshotSnapshotInfo
+  snapshot?: SnapshotSnapshotInfo
 }
 
 export interface SnapshotCreateRepositoryRequest extends RequestBase {
