b353b4df3bab361ce72e92b80f18f0827743dacf
Sylvain Wallez <sylvain@elastic.co>
2022-01-25 18:29:56 +0100

    Fixes TopHitsAggregate which a simple aggregate (no buckets) (#1327)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -3665,7 +3665,8 @@ export interface AggregationsTopMetrics {
   metrics: Record<string, FieldValue | null>
 }
 
-export interface AggregationsTopMetricsAggregate extends AggregationsMultiBucketAggregateBase<AggregationsTopMetricsBucket> {
+export interface AggregationsTopMetricsAggregate extends AggregationsAggregateBase {
+  top: AggregationsTopMetrics[]
 }
 
 export interface AggregationsTopMetricsAggregation extends AggregationsMetricAggregationBase {
@@ -3673,12 +3674,6 @@ export interface AggregationsTopMetricsAggregation extends AggregationsMetricAgg
   size?: integer
   sort?: Sort
 }
-
-export interface AggregationsTopMetricsBucketKeys extends AggregationsMultiBucketBase {
-  top: AggregationsTopMetrics[]
-}
-export type AggregationsTopMetricsBucket = AggregationsTopMetricsBucketKeys
-  & { [property: string]: AggregationsAggregate | AggregationsTopMetrics[] | long }
 
 export interface AggregationsTopMetricsValue {
   field: Field
