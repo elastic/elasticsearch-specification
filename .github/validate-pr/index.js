0c09fd4b57d9943dcdb689bd108df937f9544ba5
delvedor <tommydelved@gmail.com>
2022-01-26 12:21:56 +0100

    wip

---
+++ .github/validate-pr/index.js
@@ -0,0 +1,40 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* global $ argv, path, cd, nothrow */
+
+'use strict'
+
+require('zx/globals')
+const assert = require('assert')
+const core = require('@actions/core')
+const github = require('@actions/github')
+// const octokit = new github.GitHub(argv.token)
+
+async function run () {
+  const context = github.context
+  assert(context.payload.pull_request, 'We should be in a PR context')
+  console.log(context.payload.pull_request)
+  console.log((await $`ls ..`).toString())
+}
+
+run().catch(err => {
+  core.error(err)
+  process.exit(1)
+})
