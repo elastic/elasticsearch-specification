c47a7460f48ff69d8fcfefad9adaa6ad33a1f054
Steve Gordon <sgordon@hotmail.co.uk>
2022-02-10 16:55:43 +0000

    Add missing properties to SQL query API (#1431)
    * Add missing properties to SQL query API
    
    * make contrib

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15941,6 +15941,7 @@ export interface SqlGetAsyncStatusResponse {
 export interface SqlQueryRequest extends RequestBase {
   format?: string
   body?: {
+    catalog?: string
     columnar?: boolean
     cursor?: string
     fetch_size?: integer
@@ -15950,6 +15951,12 @@ export interface SqlQueryRequest extends RequestBase {
     page_timeout?: Time
     time_zone?: string
     field_multi_value_leniency?: boolean
+    runtime_mappings?: MappingRuntimeFields
+    wait_for_completion_timeout?: Time
+    params?: Record<string, any>
+    keep_alive?: Time
+    keep_on_completion?: boolean
+    index_using_frozen?: boolean
   }
 }
 
