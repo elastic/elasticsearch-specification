91e08714774302a2056a7003754c218117484ac2
Philip Krauss <35487337+philkra@users.noreply.github.com>
2022-02-15 13:01:53 +0100

    Fix watcher.execute_watch (#1456)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -16597,6 +16597,7 @@ export interface WatcherDataEmailAttachment {
 export type WatcherDay = 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday'
 
 export interface WatcherEmail {
+  id?: Id
   bcc?: string[]
   body?: WatcherEmailBody
   cc?: string[]
