2f6791478ae905ec900ba95c0182eb01923eb5e0
Lisa Cawley <lcawley@elastic.co>
2022-03-03 09:37:58 -0800

    Replace hard-coded doc URLs with @doc_id (#1505)

--- specification/_global/get_source/SourceRequest.ts
+++ specification/_global/get_source/SourceRequest.ts
@@ -48,7 +48,7 @@ export interface Request {
     /**
      *  Boolean) If true, the request is real-time as opposed to near-real-time.
      * @server_default true
-     * @doc_url https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-get.html#realtime
+     * @doc_id realtiime
      */
     realtime?: boolean
     /**
@@ -58,7 +58,7 @@ export interface Request {
     refresh?: boolean
     /**
      * Target the specified primary shard.
-     * @doc_url https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-get.html#get-routing
+     * @doc_id routing
      */
     routing?: Routing
     /**
