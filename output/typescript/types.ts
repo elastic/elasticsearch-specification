f0bfaf62d7e631538161e44cf809f23c8a79350d
Philip Krauss <35487337+philkra@users.noreply.github.com>
2022-03-28 16:35:39 +0200

    Fixed ml.get_memory_stats after tests (#1578)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -12807,42 +12807,43 @@ export interface MlGetJobsResponse {
 }
 
 export interface MlGetMemoryStatsJvmStats {
-  heap_max: ByteSize
+  heap_max?: ByteSize
   heap_max_in_bytes: integer
-  java_inference: ByteSize
+  java_inference?: ByteSize
   java_inference_in_bytes: integer
-  java_inference_max: ByteSize
+  java_inference_max?: ByteSize
   java_inference_max_in_bytes: integer
 }
 
 export interface MlGetMemoryStatsMemMlStats {
-  anomaly_detectors: ByteSize
+  anomaly_detectors?: ByteSize
   anomaly_detectors_in_bytes: integer
-  data_frame_analytics: ByteSize
+  data_frame_analytics?: ByteSize
   data_frame_analytics_in_bytes: integer
-  max: ByteSize
+  max?: ByteSize
   max_in_bytes: integer
-  native_code_overhead: ByteSize
+  native_code_overhead?: ByteSize
   native_code_overhead_in_bytes: integer
-  native_inference: ByteSize
+  native_inference?: ByteSize
   native_inference_in_bytes: integer
 }
 
 export interface MlGetMemoryStatsMemStats {
-  adjusted_total: ByteSize
+  adjusted_total?: ByteSize
   adjusted_total_in_bytes: integer
-  total: ByteSize
+  total?: ByteSize
   total_in_bytes: integer
   ml: MlGetMemoryStatsMemMlStats
 }
 
 export interface MlGetMemoryStatsMemory {
-  attributes: string[]
+  attributes: Record<string, string>
   jvm: MlGetMemoryStatsJvmStats
   mem: MlGetMemoryStatsMemStats
   name: Name
   roles: string[]
   transport_address: TransportAddress
+  ephemeral_id: Id
 }
 
 export interface MlGetMemoryStatsRequest extends RequestBase {
@@ -12854,7 +12855,7 @@ export interface MlGetMemoryStatsRequest extends RequestBase {
 
 export interface MlGetMemoryStatsResponse {
   _nodes: NodeStatistics
-  cluser_name: Name
+  cluster_name: Name
   nodes: Record<Id, MlGetMemoryStatsMemory>
 }
 
