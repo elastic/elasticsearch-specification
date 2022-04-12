2b88656a7877134bd531b93ee37a2b4ca9f6660c
Miriam Eid <93708060+miriam-eid@users.noreply.github.com>
2022-04-12 18:58:04 +0300

    fix transform api (#1622)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -16648,7 +16648,7 @@ export interface TransformUpdateTransformRequest extends RequestBase {
     source?: TransformSource
     settings?: TransformSettings
     sync?: TransformSyncContainer
-    retention_policy?: TransformRetentionPolicyContainer
+    retention_policy?: TransformRetentionPolicyContainer | null
   }
 }
 
