f9dfbe82bd6d5ffd098cf635ab27b0b81ab8ee39
Miriam Eid <93708060+miriam-eid@users.noreply.github.com>
2022-05-06 18:18:12 +0300

    Add all properties to MultisearchBody

--- specification/_global/msearch/types.ts
+++ specification/_global/msearch/types.ts
@@ -21,12 +21,26 @@ import { PointInTimeReference } from '@global/search/_types/PointInTimeReference
 import { Suggester } from '@global/search/_types/suggester'
 import { Dictionary } from '@spec_utils/Dictionary'
 import { AggregationContainer } from '@_types/aggregations/AggregationContainer'
-import { ExpandWildcards, Indices, SearchType } from '@_types/common'
-import { integer, long } from '@_types/Numeric'
-import { QueryContainer } from '@_types/query_dsl/abstractions'
+import {
+  ExpandWildcards,
+  Fields,
+  IndexName,
+  Indices,
+  SearchType
+} from '@_types/common'
+import { double, integer, long } from '@_types/Numeric'
+import { FieldAndFormat, QueryContainer } from '@_types/query_dsl/abstractions'
 import { ResponseBody as SearchResponse } from '@global/search/SearchResponse'
 import { TrackHits } from '@global/search/_types/hits'
 import { ErrorResponseBase } from '@_types/Base'
+import { Sort, SortResults } from '@_types/sort'
+import { FieldCollapse } from '@global/search/_types/FieldCollapse'
+import { Highlight } from '@global/search/_types/highlighting'
+import { Rescore } from '@global/search/_types/rescoring'
+import { SourceConfig } from '@global/search/_types/SourceFilter'
+import { RuntimeFields } from '@_types/mapping/RuntimeFields'
+import { ScriptField } from '@_types/Scripting'
+import { SlicedScroll } from '@_types/SlicedScroll'
 
 /**
  * @codegen_names header, body
@@ -48,14 +62,127 @@ export class MultisearchHeader {
 }
 
 export class MultisearchBody {
-  /** @aliases aggs */
+  /** @aliases aggs */ // ES uses "aggregations" in serialization
   aggregations?: Dictionary<string, AggregationContainer>
-  query?: QueryContainer
+  collapse?: FieldCollapse
+  /**
+   * If true, returns detailed information about score computation as part of a hit.
+   * @server_default false
+   */
+  explain?: boolean
+  /**
+   * Starting document offset. By default, you cannot page through more than 10,000
+   * hits using the from and size parameters. To page through more hits, use the
+   * search_after parameter.
+   * @server_default 0
+   */
   from?: integer
-  size?: integer
-  pit?: PointInTimeReference
+  highlight?: Highlight
+  /**
+   * Number of hits matching the query to count accurately. If true, the exact
+   * number of hits is returned at the cost of some performance. If false, the
+   * response does not include the total number of hits matching the query.
+   * Defaults to 10,000 hits.
+   */
   track_total_hits?: TrackHits
+  /**
+   * Boosts the _score of documents from specified indices.
+   */
+  indices_boost?: Array<Dictionary<IndexName, double>>
+  /**
+   * Array of wildcard (*) patterns. The request returns doc values for field
+   * names matching these patterns in the hits.fields property of the response.
+   */
+  docvalue_fields?: FieldAndFormat[]
+  /**
+   * Minimum _score for matching documents. Documents with a lower _score are
+   * not included in the search results.
+   */
+  min_score?: double
+  post_filter?: QueryContainer
+  profile?: boolean
+  /**
+   * Defines the search definition using the Query DSL.
+   */
+  query?: QueryContainer
+  rescore?: Rescore | Rescore[]
+  /**
+   * Retrieve a script evaluation (based on different fields) for each hit.
+   */
+  script_fields?: Dictionary<string, ScriptField>
+  search_after?: SortResults
+  /**
+   * The number of hits to return. By default, you cannot page through more
+   * than 10,000 hits using the from and size parameters. To page through more
+   * hits, use the search_after parameter.
+   * @server_default 10
+   */
+  size?: integer
+  slice?: SlicedScroll
+  /** @doc_id sort-search-results */
+  sort?: Sort
+  /**
+   * Indicates which source fields are returned for matching documents. These
+   * fields are returned in the hits._source property of the search response.
+   */
+  _source?: SourceConfig
+  /**
+   * Array of wildcard (*) patterns. The request returns values for field names
+   * matching these patterns in the hits.fields property of the response.
+   */
+  fields?: Array<FieldAndFormat>
   suggest?: Suggester
+  /**
+   * Maximum number of documents to collect for each shard. If a query reaches this
+   * limit, Elasticsearch terminates the query early. Elasticsearch collects documents
+   * before sorting. Defaults to 0, which does not terminate query execution early.
+   * @server_default 0
+   */
+  terminate_after?: long
+  /**
+   * Specifies the period of time to wait for a response from each shard. If no response
+   * is received before the timeout expires, the request fails and returns an error.
+   * Defaults to no timeout.
+   */
+  timeout?: string
+  /**
+   * If true, calculate and return document scores, even if the scores are not used for sorting.
+   * @server_default false
+   */
+  track_scores?: boolean
+  /**
+   * If true, returns document version as part of a hit.
+   * @server_default false
+   */
+  version?: boolean
+  /**
+   * If true, returns sequence number and primary term of the last modification
+   * of each hit. See Optimistic concurrency control.
+   */
+  seq_no_primary_term?: boolean
+  /**
+   * List of stored fields to return as part of a hit. If no fields are specified,
+   * no stored fields are included in the response. If this field is specified, the _source
+   * parameter defaults to false. You can pass _source: true to return both source fields
+   * and stored fields in the search response.
+   */
+  stored_fields?: Fields
+  /**
+   * Limits the search to a point in time (PIT). If you provide a PIT, you
+   * cannot specify an <index> in the request path.
+   */
+  pit?: PointInTimeReference
+  /**
+   * Defines one or more runtime fields in the search request. These fields take
+   * precedence over mapped fields with the same name.
+   */
+  runtime_mappings?: RuntimeFields
+  /**
+   * Stats groups to associate with the search. Each group maintains a statistics
+   * aggregation for its associated searches. You can retrieve these stats using
+   * the indices stats API.
+   */
+  stats?: string[]
 }
 
 export class MultiSearchResult<TDocument> {
