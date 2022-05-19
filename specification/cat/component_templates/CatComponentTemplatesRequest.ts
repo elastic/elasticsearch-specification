19454c1425cd463f1b88e8ec1e29c13415de2ca8
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2022-05-19 15:48:35 +0200

    Remove wrong doc-id decorator

--- specification/cat/component_templates/CatComponentTemplatesRequest.ts
+++ specification/cat/component_templates/CatComponentTemplatesRequest.ts
@@ -23,7 +23,6 @@ import { CatRequestBase } from '@cat/_types/CatBase'
  * @rest_spec_name cat.component_templates
  * @since 0.0.0
  * @stability stable
- * @doc_id cat-indices
  */
 export interface Request extends CatRequestBase {
   path_parts: {
