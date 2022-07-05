28b4a9de198e83cf8c49885401db110a415ccef7
lcawl <lcawley@elastic.co>
2022-07-05 12:46:34 -0700

    Make contrib

--- specification/ml/_types/Authorization.ts
+++ specification/ml/_types/Authorization.ts
@@ -18,54 +18,54 @@
  */
 
 export class ApiKeyAuthorization {
-    /**
-     * The identifier for the API key.
-     */
-    id: string
-    /**
-     * The name of the API key.
-     */
-    name: string
-  }
+  /**
+   * The identifier for the API key.
+   */
+  id: string
+  /**
+   * The name of the API key.
+   */
+  name: string
+}
 
 export class DatafeedAuthorization {
-    /**
-     *  If an API key was used for the most recent update to the datafeed, its name and identifier are listed in the response.
-     */
-    api_key?: ApiKeyAuthorization
-    /**
-     *  If a user ID was used for the most recent update to the datafeed, its roles at the time of the update are listed in the response. */
-    roles?: string[]
-    /**
-     * If a service account was used for the most recent update to the datafeed, the account name is listed in the response.
-     */
-    service_account?: string
-  }
+  /**
+   *  If an API key was used for the most recent update to the datafeed, its name and identifier are listed in the response.
+   */
+  api_key?: ApiKeyAuthorization
+  /**
+   *  If a user ID was used for the most recent update to the datafeed, its roles at the time of the update are listed in the response. */
+  roles?: string[]
+  /**
+   * If a service account was used for the most recent update to the datafeed, the account name is listed in the response.
+   */
+  service_account?: string
+}
 
-  export class DataframeAnalyticsAuthorization {
-    /**
-     *  If an API key was used for the most recent update to the job, its name and identifier are listed in the response.
-     */
-     api_key?: ApiKeyAuthorization
-     /**
-      *  If a user ID was used for the most recent update to the job, its roles at the time of the update are listed in the response. */
-     roles?: string[]
-     /**
-      * If a service account was used for the most recent update to the job, the account name is listed in the response.
-      */
-     service_account?: string
-  }
+export class DataframeAnalyticsAuthorization {
+  /**
+   *  If an API key was used for the most recent update to the job, its name and identifier are listed in the response.
+   */
+  api_key?: ApiKeyAuthorization
+  /**
+   *  If a user ID was used for the most recent update to the job, its roles at the time of the update are listed in the response. */
+  roles?: string[]
+  /**
+   * If a service account was used for the most recent update to the job, the account name is listed in the response.
+   */
+  service_account?: string
+}
 
-  export class TransformAuthorization {
-    /**
-     *  If an API key was used for the most recent update to the transform, its name and identifier are listed in the response.
-     */
-     api_key?: ApiKeyAuthorization
-     /**
-      *  If a user ID was used for the most recent update to the transform, its roles at the time of the update are listed in the response. */
-     roles?: string[]
-     /**
-      * If a service account was used for the most recent update to the transform, the account name is listed in the response.
-      */
-     service_account?: string
-  }
+export class TransformAuthorization {
+  /**
+   *  If an API key was used for the most recent update to the transform, its name and identifier are listed in the response.
+   */
+  api_key?: ApiKeyAuthorization
+  /**
+   *  If a user ID was used for the most recent update to the transform, its roles at the time of the update are listed in the response. */
+  roles?: string[]
+  /**
+   * If a service account was used for the most recent update to the transform, the account name is listed in the response.
+   */
+  service_account?: string
+}
