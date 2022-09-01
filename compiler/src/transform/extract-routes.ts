f7099082222ff9b9210bd5d8d409cde2c411756c
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2022-09-01 12:03:19 +0200

    Add comments

--- compiler/src/transform/extract-routes.ts
+++ compiler/src/transform/extract-routes.ts
@@ -31,6 +31,11 @@ function matches(subject: string, search: string): string {
   for (let i = 0; i < subject.length; i++) {
     if (subject[i] === search[i]) {
       output += subject[i];
+
+      // We shortcut the match if we encounter a variable.
+      if (subject[i] === "{") {
+        return output;
+      }
     } else {
       return output;
     }
@@ -40,12 +45,25 @@ function matches(subject: string, search: string): string {
 
 
 function insert(node: Node, url: string) {
+  // Check if we're not on root anymore,
+  // otherwise the value of url is set as
+  // the default path for the current node
+  // to bootstrap the insertion making this
+  // de facto root of the tree.
   if (url[0] === node.path[0]) {
     const match = matches(url, node.path);
 
+    // If the matched part is shorter than the current node path
+    // we need to split the node.
+    // eg: _search with _select added becomes
+    // _se ->
+    //  -> arch
+    //  -> lect
     if (match.length < node.path.length) {
       let child = new Node();
       child.path = node.path.slice(match.length, node.path.length);
+
+      // If the current node already has children we move them to the newly created node.
       if (node.children.length > 0) {
         child.children = node.children;
         node.children = [];
@@ -57,15 +75,22 @@ function insert(node: Node, url: string) {
       node.path = match;
     }
 
+    // if the url is lengthier than the current node we have some children to populate/match
     if (url.length > node.path.length) {
       let found: boolean = false;
+
+      // rest is the url after the match that will be passed on to children
       let rest = url.slice(match.length, url.length);
+
+      // first we iterate over existing children to check if there's a match
       for (const child of node.children) {
         if (child.path[0] === rest[0]) {
           found = true;
           insert(child, rest);
         }
       }
+
+      // if there's no match found we create a new child to this node
       if (!found) {
         let n = new Node();
         n.path = rest;
