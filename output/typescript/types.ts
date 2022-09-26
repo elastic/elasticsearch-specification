985334864fcac4525f6889b974c229e9b2ee22d9
Seth Michael Larson <seth.larson@elastic.co>
2022-09-26 13:49:13 -0500

    [7.17] Make 'dir' optional on IcuTransformTokenFilter
    Co-authored-by: mashijp <mashijp@gmail.com>

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -3965,7 +3965,7 @@ export type AnalysisIcuTransformDirection = 'forward' | 'reverse'
 
 export interface AnalysisIcuTransformTokenFilter extends AnalysisTokenFilterBase {
   type: 'icu_transform'
-  dir: AnalysisIcuTransformDirection
+  dir?: AnalysisIcuTransformDirection
   id: string
 }
 
