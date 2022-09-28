5430ac7bf404d8b92827021be54d71f85092a42f
Seth Michael Larson <seth.larson@elastic.co>
2022-09-28 14:48:25 -0500

    Mark ML model APIs as beta

--- specification/ml/put_trained_model_definition_part/MlPutTrainedModelDefinitionPartRequest.ts
+++ specification/ml/put_trained_model_definition_part/MlPutTrainedModelDefinitionPartRequest.ts
@@ -25,7 +25,7 @@ import { Id } from '@_types/common'
  * Creates part of a trained model definition.
  * @rest_spec_name ml.put_trained_model_definition_part
  * @since 8.0.0
- * @stability experimental
+ * @stability beta
  * @cluster_privileges manage_ml
  */
 export interface Request extends RequestBase {
