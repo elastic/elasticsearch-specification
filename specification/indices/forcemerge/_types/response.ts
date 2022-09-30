fa9fd4d2c23f2631869c0be2ac726c168462ae45
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2022-09-30 16:06:51 +0200

    Extract forcemerge.response, add task optional property

--- specification/indices/forcemerge/_types/response.ts
+++ specification/indices/forcemerge/_types/response.ts
@@ -17,8 +17,12 @@
  * under the License.
  */
 
-import {ForceMergeResponse} from "@indices/forcemerge/_types/response";
+import {ShardsOperationResponseBase} from "@_types/Base";
 
-export class Response {
-  body: ForceMergeResponse
+export class ForceMergeResponse extends ShardsOperationResponseBase {
+  /**
+   * task contains a task id returned when wait_for_completion=false,
+   * you can use the task_id to get the status of the task at _tasks/<task_id>
+   */
+  task?: string
 }
