edc742b074bfbfe1b707ebff53be74d7640497e2
Sylvain Wallez <sylvain@elastic.co>
2022-10-17 19:26:57 +0200

    IndicesPrivileges can contain multiple queries (#1963)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15293,7 +15293,7 @@ export interface SecurityIndicesPrivileges {
   field_security?: SecurityFieldSecurity | SecurityFieldSecurity[]
   names: Indices
   privileges: SecurityIndexPrivilege[]
-  query?: SecurityIndicesPrivilegesQuery
+  query?: SecurityIndicesPrivilegesQuery | SecurityIndicesPrivilegesQuery[]
   allow_restricted_indices?: boolean
 }
 
