0e5b9f1fc44eecffcc6092a92b23faedf2686a85
cdelgado <carlos.delgado@elastic.co>
2023-04-21 11:51:27 +0200

    Changed visibility from beta to experimental

--- specification/search_application/search/SearchApplicationsSearchRequest.ts
+++ specification/search_application/search/SearchApplicationsSearchRequest.ts
@@ -25,7 +25,7 @@ import { UserDefinedValue } from '@spec_utils/UserDefinedValue'
  * Perform a search against a search application
  * @rest_spec_name search_application.search
  * @since 8.8.0
- * @stability beta
+ * @stability experimental
  */
 interface Request extends RequestBase {
   path_parts: {
