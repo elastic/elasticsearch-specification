a207eb6ad7451c2bd5719b64d170135f062a3253
gmarouli <mary.gouseti@elastic.co>
2023-05-03 14:49:00 +0300

    Add new CRUD data lifecycle endpoints

---
+++ specification/indices/put_data_lifecycle/IndicesPutDataLifecycleRequest.ts
@@ -0,0 +1,42 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { RequestBase } from '@_types/Base'
+import {DataStreamNames, ExpandWildcards} from '@_types/common'
+import { Duration } from '@_types/Time'
+
+/**
+ * Update the data lifecycle of the specified data streams.
+ * @rest_spec_name indices.put_data_lifecycle
+ * @since 8.8.0
+ * @stability experimental
+ */
+export interface Request extends RequestBase {
+  path_parts: {
+    name: DataStreamNames
+  }
+  query_parameters: {
+    expand_wildcards?: ExpandWildcards
+    master_timeout?: Duration
+    timeout?: Duration
+  }
+  body: {
+    data_retention?: Duration
+  }
+}
