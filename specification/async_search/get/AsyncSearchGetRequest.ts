02a2a61fe10f6b4c4f455ab84096053d9a40cd20
lcawl <lcawley@elastic.co>
2023-05-15 09:32:59 -0700

    Add documentation for async_search

--- specification/async_search/get/AsyncSearchGetRequest.ts
+++ specification/async_search/get/AsyncSearchGetRequest.ts
@@ -22,6 +22,8 @@ import { Id } from '@_types/common'
 import { Duration } from '@_types/Time'
 
 /**
+ * Retrieves the results of a previously submitted async search request given its identifier.
+ * If the Elasticsearch security features are enabled, access to the results of a specific async search is restricted to the user or API key that submitted it.
  * @rest_spec_name async_search.get
  * @since 7.7.0
  * @stability stable
@@ -29,11 +31,24 @@ import { Duration } from '@_types/Time'
  */
 export interface Request extends RequestBase {
   path_parts: {
+    /** A unique identifier for the async search. */
     id: Id
   }
   query_parameters: {
+    /**
+     * Specifies how long the async search should be available in the cluster.
+     * When not specified, the `keep_alive` set with the corresponding submit async request will be used.
+     * Otherwise, it is possible to override the value and extend the validity of the request.
+     * When this period expires, the search, if still running, is cancelled.
+     * If the search is completed, its saved results are deleted.
+     */
     keep_alive?: Duration
     typed_keys?: boolean
+    /**
+     * Specifies to wait for the search to be completed up until the provided timeout.
+     * Final results will be returned if available before the timeout expires, otherwise the currently available results will be returned once the timeout expires.
+     * By default no timeout is set meaning that the currently available results will be returned without any additional wait.
+     */
     wait_for_completion_timeout?: Duration
   }
 }
