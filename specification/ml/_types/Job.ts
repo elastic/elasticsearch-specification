3e8e44e36aed7122d36ed4519c0c6e0361529084
lcawl <lcawley@elastic.co>
2023-06-06 15:16:13 -0700

    Add descriptions for more cat types

--- specification/ml/_types/Job.ts
+++ specification/ml/_types/Job.ts
@@ -34,10 +34,20 @@ import { ModelSizeStats } from './Model'
 import { Datafeed, DatafeedConfig } from '@ml/_types/Datafeed'
 
 export enum JobState {
+  /** The job close action is in progress and has not yet completed. A closing job cannot accept further data. */
   closing = 0,
+  /** The job finished successfully with its model state persisted. The job must be opened before it can accept further data. */
   closed = 1,
+  /** The job is available to receive and process data. */
   opened = 2,
+  /**
+   * The job did not finish successfully due to an error.
+   * This situation can occur due to invalid input data, a fatal error occurring during the analysis, or an external interaction such as the process being killed by the Linux out of memory (OOM) killer.
+   * If the job had irrevocably failed, it must be force closed and then deleted.
+   * If the datafeed can be corrected, the job can be closed and then re-opened.
+   */
   failed = 3,
+  /** The job open action is in progress and has not yet completed. */
   opening = 4
 }
 
