5c59499f2f5e74b9a07dd53cfdf75f132d4375c2
Lisa Cawley <lcawley@elastic.co>
2023-06-06 05:50:10 -0700

    Add descriptions for more cat APIs

--- specification/cat/nodes/types.ts
+++ specification/cat/nodes/types.ts
@@ -22,519 +22,521 @@ import { Percentage } from '@_types/Numeric'
 
 export class NodesRecord {
   /**
-   * unique node id
+   * The unique node identifier.
    * @aliases nodeId
    */
   'id'?: Id
   /**
-   * process id
+   * The process identifier.
    * @aliases p
    */
   'pid'?: string
   /**
-   * ip address
+   * The IP address.
    * @aliases i
    */
   'ip'?: string
   /**
-   * bound transport port
+   * The bound transport port.
    * @aliases po
    */
   'port'?: string
   /**
-   * bound http address
+   * The bound HTTP address.
    * @aliases http
    */
   'http_address'?: string
 
   /**
-   * es version
+   * The Elasticsearch version.
    * @aliases v
    */
   'version'?: VersionString
   /**
-   * es distribution flavor
+   * The Elasticsearch distribution flavor.
    * @aliases f
    */
   'flavor'?: string
   /**
-   * es distribution type
+   * The Elasticsearch distribution type.
    * @aliases t
    */
   'type'?: string
   /**
-   * es build hash
+   * The Elasticsearch build hash.
    * @aliases b
    */
   'build'?: string
   /**
-   * jdk version
+   * The Java version.
    * @aliases j
    */
   'jdk'?: string
   /**
-   * total disk space
+   * The total disk space.
    * @aliases dt,diskTotal
    */
   'disk.total'?: ByteSize
   /**
-   * used disk space
+   * The used disk space.
    * @aliases du,diskUsed
    */
   'disk.used'?: ByteSize
   /**
-   * available disk space
+   * The available disk space.
    * @aliases d,da,disk,diskAvail
    */
   'disk.avail'?: ByteSize
   /**
-   * used disk space percentage
+   * The used disk space percentage.
    * @aliases dup,diskUsedPercent
    */
   'disk.used_percent'?: Percentage
   /**
-   * used heap
+   * The used heap.
    * @aliases hc,heapCurrent
    */
   'heap.current'?: string
   /**
-   * used heap ratio
+   * The used heap ratio.
    * @aliases hp,heapPercent
    */
   'heap.percent'?: Percentage
   /**
-   * max configured heap
+   * The maximum configured heap.
    * @aliases hm,heapMax
    */
   'heap.max'?: string
   /**
-   * used machine memory
+   * The used machine memory.
    * @aliases rc,ramCurrent
    */
   'ram.current'?: string
   /**
-   * used machine memory ratio
+   * The used machine memory ratio.
    * @aliases rp,ramPercent
    */
   'ram.percent'?: Percentage
   /**
-   * total machine memory
+   * The total machine memory.
    * @aliases rn,ramMax
    */
   'ram.max'?: string
   /**
-   * used file descriptors
+   * The used file descriptors.
    * @aliases fdc,fileDescriptorCurrent
    */
   'file_desc.current'?: string
   /**
-   * used file descriptor ratio
+   * The used file descriptor ratio.
    * @aliases fdp,fileDescriptorPercent
    */
   'file_desc.percent'?: Percentage
   /**
-   * max file descriptors
+   * The maximum number of file descriptors.
    * @aliases fdm,fileDescriptorMax
    */
   'file_desc.max'?: string
 
   /**
-   * recent cpu usage
+   * The recent system CPU usage as a percentage.
    */
   'cpu'?: string
   /**
-   * 1m load avg
+   * The load average for the most recent minute.
    */
   'load_1m'?: string
   /**
-   * 5m load avg
+   * The load average for the last five minutes.
    */
   'load_5m'?: string
   /**
-   * 15m load avg
+   * The load average for the last fifteen minutes.
    * @aliases l
    */
   'load_15m'?: string
   /**
-   * node uptime
+   * The node uptime.
    * @aliases u
    */
   'uptime'?: string
   /**
-   * m:master eligible node, d:data node, i:ingest node, -:coordinating node only
+   * The roles of the node.
+   * Returned values include `c`(cold node), `d`(data node), `f`(frozen node), `h`(hot node), `i`(ingest node), `l`(machine learning node), `m` (master eligible node), `r`(remote cluster client node), `s`(content node), `t`(transform node), `v`(voting-only node), `w`(warm node),and `-`(coordinating node only).
    * @aliases r,role,nodeRole
    */
   'node.role'?: string
 
   /**
-   * *:current master
+   * Indicates whether the node is the elected master node.
+   * Returned values include `*`(elected master) and `-`(not elected master).
    * @aliases m
    */
   'master'?: string
 
   /**
-   * node name
+   * The node name.
    * @aliases n
    */
   'name'?: Name
 
   /**
-   * size of completion
+   * The size of completion.
    * @aliases cs,completionSize
    */
   'completion.size'?: string
 
   /**
-   * used fielddata cache
+   * The used fielddata cache.
    * @aliases fm,fielddataMemory
    */
   'fielddata.memory_size'?: string
 
   /**
-   * fielddata evictions
+   * The fielddata evictions.
    * @aliases fe,fielddataEvictions
    */
   'fielddata.evictions'?: string
 
   /**
-   * used query cache
+   * The used query cache.
    * @aliases qcm,queryCacheMemory
    */
   'query_cache.memory_size'?: string
 
   /**
-   * query cache evictions
+   * The query cache evictions.
    * @aliases qce,queryCacheEvictions
    */
   'query_cache.evictions'?: string
 
   /**
-   * query cache hit counts
+   * The query cache hit counts.
    * @aliases qchc,queryCacheHitCount
    */
   'query_cache.hit_count'?: string
 
   /**
-   * query cache miss counts
+   * The query cache miss counts.
    * @aliases qcmc,queryCacheMissCount
    */
   'query_cache.miss_count'?: string
 
   /**
-   * used request cache
+   * The used request cache.
    * @aliases rcm,requestCacheMemory
    */
   'request_cache.memory_size'?: string
 
   /**
-   * request cache evictions
+   * The request cache evictions.
    * @aliases rce,requestCacheEvictions
    */
   'request_cache.evictions'?: string
 
   /**
-   * request cache hit counts
+   * The request cache hit counts.
    * @aliases rchc,requestCacheHitCount
    */
   'request_cache.hit_count'?: string
 
   /**
-   * request cache miss counts
+   * The request cache miss counts.
    * @aliases rcmc,requestCacheMissCount
    */
   'request_cache.miss_count'?: string
 
   /**
-   * number of flushes
+   * The number of flushes.
    * @aliases ft,flushTotal
    */
   'flush.total'?: string
 
   /**
-   * time spent in flush
+   * The time spent in flush.
    * @aliases ftt,flushTotalTime
    */
   'flush.total_time'?: string
 
   /**
-   * number of current get ops
+   * The number of current get ops.
    * @aliases gc,getCurrent
    */
   'get.current'?: string
 
   /**
-   * time spent in get
+   * The time spent in get.
    * @aliases gti,getTime
    */
   'get.time'?: string
 
   /**
-   * number of get ops
+   * The number of get ops.
    * @aliases gto,getTotal
    */
   'get.total'?: string
 
   /**
-   * time spent in successful gets
+   * The time spent in successful gets.
    * @aliases geti,getExistsTime
    */
   'get.exists_time'?: string
 
   /**
-   * number of successful gets
+   * The number of successful get operations.
    * @aliases geto,getExistsTotal
    */
   'get.exists_total'?: string
 
   /**
-   * time spent in failed gets
+   * The time spent in failed gets.
    * @aliases gmti,getMissingTime
    */
   'get.missing_time'?: string
 
   /**
-   * number of failed gets
+   * The number of failed gets.
    * @aliases gmto,getMissingTotal
    */
   'get.missing_total'?: string
 
   /**
-   * number of current deletions
+   * The number of current deletions.
    * @aliases idc,indexingDeleteCurrent
    */
   'indexing.delete_current'?: string
 
   /**
-   * time spent in deletions
+   * The time spent in deletions.
    * @aliases idti,indexingDeleteTime
    */
   'indexing.delete_time'?: string
 
   /**
-   * number of delete ops
+   * The number of delete operations.
    * @aliases idto,indexingDeleteTotal
    */
   'indexing.delete_total'?: string
 
   /**
-   * number of current indexing ops
+   * The number of current indexing operations.
    * @aliases iic,indexingIndexCurrent
    */
   'indexing.index_current'?: string
 
   /**
-   * time spent in indexing
+   * The time spent in indexing.
    * @aliases iiti,indexingIndexTime
    */
   'indexing.index_time'?: string
 
   /**
-   * number of indexing ops
+   * The number of indexing operations.
    * @aliases iito,indexingIndexTotal
    */
   'indexing.index_total'?: string
 
   /**
-   * number of failed indexing ops
+   * The number of failed indexing operations.
    * @aliases iif,indexingIndexFailed
    */
   'indexing.index_failed'?: string
 
   /**
-   * number of current merges
+   * The number of current merges.
    * @aliases mc,mergesCurrent
    */
   'merges.current'?: string
 
   /**
-   * number of current merging docs
+   * The number of current merging docs.
    * @aliases mcd,mergesCurrentDocs
    */
   'merges.current_docs'?: string
 
   /**
-   * size of current merges
+   * The size of current merges.
    * @aliases mcs,mergesCurrentSize
    */
   'merges.current_size'?: string
 
   /**
-   * number of completed merge ops
+   * The number of completed merge operations.
    * @aliases mt,mergesTotal
    */
   'merges.total'?: string
 
   /**
-   * docs merged
+   * The docs merged.
    * @aliases mtd,mergesTotalDocs
    */
   'merges.total_docs'?: string
   /**
-   * size merged
+   * The size merged.
    * @aliases mts,mergesTotalSize
    */
   'merges.total_size'?: string
   /**
-   * time spent in merges
+   * The time spent in merges.
    * @aliases mtt,mergesTotalTime
    */
   'merges.total_time'?: string
 
   /**
-   * total refreshes
+   * The total refreshes.
    */
   'refresh.total'?: string
   /**
-   * time spent in refreshes
+   * The time spent in refreshes.
    */
   'refresh.time'?: string
   /**
-   * total external refreshes
+   * The total external refreshes.
    * @aliases rto,refreshTotal
    */
   'refresh.external_total'?: string
   /**
-   * time spent in external refreshes
+   * The time spent in external refreshes.
    * @aliases rti,refreshTime
    */
   'refresh.external_time'?: string
   /**
-   * number of pending refresh listeners
+   * The number of pending refresh listeners.
    * @aliases rli,refreshListeners
    */
   'refresh.listeners'?: string
 
   /**
-   * script compilations
+   * The total script compilations.
    * @aliases scrcc,scriptCompilations
    */
   'script.compilations'?: string
   /**
-   * script cache evictions
+   * The total compiled scripts evicted from the cache.
    * @aliases scrce,scriptCacheEvictions
    */
   'script.cache_evictions'?: string
   /**
-   * script cache compilation limit triggered
+   * The script cache compilation limit triggered.
    * @aliases scrclt,scriptCacheCompilationLimitTriggered
    */
   'script.compilation_limit_triggered'?: string
 
   /**
-   * current fetch phase ops
+   * The current fetch phase operations.
    * @aliases sfc,searchFetchCurrent
    */
   'search.fetch_current'?: string
   /**
-   * time spent in fetch phase
+   * The time spent in fetch phase.
    * @aliases sfti,searchFetchTime
    */
   'search.fetch_time'?: string
   /**
-   * total fetch ops
+   * The total fetch operations.
    * @aliases sfto,searchFetchTotal
    */
   'search.fetch_total'?: string
   /**
-   * open search contexts
+   * The open search contexts.
    * @aliases so,searchOpenContexts
    */
   'search.open_contexts'?: string
   /**
-   * current query phase ops
+   * The current query phase operations.
    * @aliases sqc,searchQueryCurrent
    */
   'search.query_current'?: string
   /**
-   * time spent in query phase
+   * The time spent in query phase.
    * @aliases sqti,searchQueryTime
    */
   'search.query_time'?: string
   /**
-   * total query phase ops
+   * The total query phase operations.
    * @aliases sqto,searchQueryTotal
    */
   'search.query_total'?: string
   /**
-   * open scroll contexts
+   * The open scroll contexts.
    * @aliases scc,searchScrollCurrent
    */
   'search.scroll_current'?: string
   /**
-   * time scroll contexts held open
+   * The time scroll contexts held open.
    * @aliases scti,searchScrollTime
    */
   'search.scroll_time'?: string
   /**
-   * completed scroll contexts
+   * The completed scroll contexts.
    * @aliases scto,searchScrollTotal
    */
   'search.scroll_total'?: string
 
   /**
-   * number of segments
+   * The number of segments.
    * @aliases sc,segmentsCount
    */
   'segments.count'?: string
   /**
-   * memory used by segments
+   * The memory used by segments.
    * @aliases sm,segmentsMemory
    */
   'segments.memory'?: string
   /**
-   * memory used by index writer
+   * The memory used by the index writer.
    * @aliases siwm,segmentsIndexWriterMemory
    */
   'segments.index_writer_memory'?: string
   /**
-   * memory used by version map
+   * The memory used by the version map.
    * @aliases svmm,segmentsVersionMapMemory
    */
   'segments.version_map_memory'?: string
   /**
-   * memory used by fixed bit sets for nested object field types  and export type filters for types referred in _parent fields
+   * The memory used by fixed bit sets for nested object field types and export type filters for types referred in _parent fields.
    * @aliases sfbm,fixedBitsetMemory
    */
   'segments.fixed_bitset_memory'?: string
 
   /**
-   * number of current suggest ops
+   * The number of current suggest operations.
    * @aliases suc,suggestCurrent
    */
   'suggest.current'?: string
   /**
-   * time spend in suggest
+   * The time spend in suggest.
    * @aliases suti,suggestTime
    */
   'suggest.time'?: string
   /**
-   * number of suggest ops
+   * The number of suggest operations.
    * @aliases suto,suggestTotal
    */
   'suggest.total'?: string
 
   /**
-   * number of bulk shard ops
+   * The number of bulk shard operations.
    * @aliases bto,bulkTotalOperations
    */
   'bulk.total_operations'?: string
   /**
-   * time spend in shard bulk
+   * The time spend in shard bulk.
    * @aliases btti,bulkTotalTime
    */
   'bulk.total_time'?: string
   /**
-   * total size in bytes of shard bulk
+   * The total size in bytes of shard bulk.
    * @aliases btsi,bulkTotalSizeInBytes
    */
   'bulk.total_size_in_bytes'?: string
   /**
-   * average time spend in shard bulk
+   * The average time spend in shard bulk.
    * @aliases bati,bulkAvgTime
    */
   'bulk.avg_time'?: string
   /**
-   * average size in bytes of shard bulk
+   * The average size in bytes of shard bulk.
    * @aliases basi,bulkAvgSizeInBytes
    */
   'bulk.avg_size_in_bytes'?: string
