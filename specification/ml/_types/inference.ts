d31921a854f57ad4b9e7e6723d67a60e9b3f53d7
Max Hniebergall <137079448+maxhniebergall@users.noreply.github.com>
2023-07-12 11:47:56 -0400

    [NLP] Support the different mask tokens used by NLP models for Fill Mask (#2178)
    Adds the specification for the new mask token field added in elastic/elasticsearch#97453, which enables users and Kibana to see the mask token for a particular model

--- specification/ml/_types/inference.ts
+++ specification/ml/_types/inference.ts
@@ -265,6 +265,12 @@ export class NerInferenceOptions {
 
 /** Fill mask inference options */
 export class FillMaskInferenceOptions {
+  /** The string/token which will be removed from incoming documents and replaced with the inference prediction(s).
+   * In a response, this field contains the mask token for the specified model/tokenizer. Each model and tokenizer
+   * has a predefined mask token which cannot be changed. Thus, it is recommended not to set this value in requests.
+   * However, if this field is present in a request, its value must match the predefined value for that model/tokenizer,
+   * otherwise the request will fail. */
+  mask_token?: string
   /** Specifies the number of top class predictions to return. Defaults to 0. */
   num_top_classes?: integer
   /** The tokenization options to update when inferring */
