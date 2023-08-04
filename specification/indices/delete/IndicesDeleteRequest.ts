13c4f786c2fca8edbf4bcd52a4dab411d6f28817
István Zoltán Szabó <szabosteve@gmail.com>
2023-08-04 11:40:14 +0200

    [DOCS] Adds description to delete index API.

--- specification/indices/delete/IndicesDeleteRequest.ts
+++ specification/indices/delete/IndicesDeleteRequest.ts
@@ -22,19 +22,52 @@ import { ExpandWildcards, Indices } from '@_types/common'
 import { Duration } from '@_types/Time'
 
 /**
+ * Deletes one or more indices.
  * @rest_spec_name indices.delete
  * @availability stack since=0.0.0 stability=stable
  * @availability serverless stability=stable visibility=public
  */
 export interface Request extends RequestBase {
   path_parts: {
+    /**
+     * Comma-separated list of indices to delete. 
+     * You cannot specify index aliases.
+     * By default, this parameter does not support wildcards (`*`) or `_all`. 
+     * To use wildcards or `_all`, set the `action.destructive_requires_name` cluster setting to `false`.
+     */
     index: Indices
   }
   query_parameters: {
+    /**
+     * If `false`, the request returns an error if any wildcard expression, index alias, or `_all` value targets only missing or closed indices.
+     * This behavior applies even if the request targets other open indices.
+     * @server_default true
+     */
     allow_no_indices?: boolean
+    /**
+     * Type of index that wildcard patterns can match. 
+     * If the request can target data streams, this argument determines whether wildcard expressions match hidden data streams. 
+     * Supports comma-separated values, such as `open,hidden`. 
+     * Valid values are: `all`, `open`, `closed`, `hidden`, `none`.
+     * @server_default open
+     */
     expand_wildcards?: ExpandWildcards
+    /**
+     * If `false`, the request returns an error if it targets a missing or closed index.
+     * @server_default false
+     */
     ignore_unavailable?: boolean
+    /**
+     * Period to wait for a connection to the master node. 
+     * If no response is received before the timeout expires, the request fails and returns an error.
+     * @server_default 30s
+     */
     master_timeout?: Duration
+    /**
+     * Period to wait for a response. 
+     * If no response is received before the timeout expires, the request fails and returns an error.
+     * @sever_default 30s
+     */
     timeout?: Duration
   }
 }
