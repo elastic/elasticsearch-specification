43ab3de810d68f474de3dc98d9af06fb1ac0ad19
István Zoltán Szabó <szabosteve@gmail.com>
2023-08-08 17:00:23 +0200

    [DOCS] Adds description to index APIs (P-S).

--- specification/indices/shrink/IndicesShrinkRequest.ts
+++ specification/indices/shrink/IndicesShrinkRequest.ts
@@ -25,21 +25,51 @@ import { IndexName, WaitForActiveShards } from '@_types/common'
 import { Duration } from '@_types/Time'
 
 /**
+ * Shrinks an existing index into a new index with fewer primary shards.
+ * @doc_id indices-shrink-index
  * @rest_spec_name indices.shrink
  * @availability stack since=5.0.0 stability=stable
  */
 export interface Request extends RequestBase {
   path_parts: {
+    /**
+     * Name of the source index to shrink.
+     */
     index: IndexName
+    /**
+     * Name of the target index to create.
+     */
     target: IndexName
   }
   query_parameters: {
+    /**
+     * Period to wait for a connection to the master node. 
+     * If no response is received before the timeout expires, the request fails and returns an error.
+     * @server_default 30s
+     */
     master_timeout?: Duration
+    /**
+     * Period to wait for a response. 
+     * If no response is received before the timeout expires, the request fails and returns an error.
+     * @server_default 30s
+     */
     timeout?: Duration
+    /**
+     * The number of shard copies that must be active before proceeding with the operation.
+     * Set to `all` or any positive integer up to the total number of shards in the index (`number_of_replicas+1`).
+     * @server_default 1
+     */
     wait_for_active_shards?: WaitForActiveShards
   }
   body: {
+    /**
+     * The key is the alias name.
+     * Index alias names support date math.
+     */
     aliases?: Dictionary<IndexName, Alias>
+    /**
+     * Configuration options for the target index.
+     */
     settings?: Dictionary<string, UserDefinedValue>
   }
 }
