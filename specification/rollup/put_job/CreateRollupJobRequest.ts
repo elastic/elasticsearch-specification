658677a2cc98e46a90d1fbbcec0011979a50bdf0
István Zoltán Szabó <szabosteve@gmail.com>
2023-08-28 17:30:06 +0200

    [DOCS] Adds description to rollup APIs.

--- specification/rollup/put_job/CreateRollupJobRequest.ts
+++ specification/rollup/put_job/CreateRollupJobRequest.ts
@@ -25,6 +25,7 @@ import { integer } from '@_types/Numeric'
 import { Duration } from '@_types/Time'
 
 /**
+ * Creates a rollup job.
  * @rest_spec_name rollup.put_job
  * @availability stack since=6.3.0 stability=experimental
  * @cluster_privileges manage, manage_rollup
