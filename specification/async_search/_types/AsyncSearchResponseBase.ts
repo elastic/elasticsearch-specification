843501e403bff8464085ae1926a88e45b8a12ba2
Michael Peterson <michael.peterson@elastic.co>
2023-08-29 09:53:18 -0400

    set completion_time_in_millis as fully optional in AsyncSearchResponseBase, since only present at completion of search

--- specification/async_search/_types/AsyncSearchResponseBase.ts
+++ specification/async_search/_types/AsyncSearchResponseBase.ts
@@ -41,10 +41,11 @@ export class AsyncSearchResponseBase {
   start_time?: DateTime
   start_time_in_millis: EpochTime<UnitMillis>
   /**
-   * Indicates when the async search completed
+   * Indicates when the async search completed. Only present
+   * when the search has completed.
    */
   completion_time?: DateTime
-  completion_time_in_millis: EpochTime<UnitMillis>
+  completion_time_in_millis?: EpochTime<UnitMillis>
 }
 export class AsyncSearchDocumentResponseBase<
   TDocument
