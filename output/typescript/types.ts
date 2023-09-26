c2e1c242d8d61fabd0b06d54f67c26eb863757bf
Quentin Pradet <quentin.pradet@elastic.co>
2023-09-26 11:34:06 +0400

    Add delete_dest_index in DeleteTransformRequest

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -17734,6 +17734,7 @@ export interface TransformTimeSync {
 export interface TransformDeleteTransformRequest extends RequestBase {
   transform_id: Id
   force?: boolean
+  delete_dest_index?: boolean
   timeout?: Duration
 }
 
