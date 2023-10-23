11d15f378a8ca4ad11b1745d2c7b78241c737a0d
Sylvain Wallez <sylvain@elastic.co>
2023-10-23 11:35:47 +0200

    Lint TS code

--- compiler/src/transform/schema-to-openapi.ts
+++ compiler/src/transform/schema-to-openapi.ts
@@ -17,23 +17,23 @@
  * under the License.
  */
 
-import {convert_schema_to_openapi} from 'schema-wasm-lib';
-import {argv} from "zx";
-import {join} from "path";
-import {readFileSync, writeFileSync} from "fs";
+import { convert_schema_to_openapi } from 'schema-wasm-lib'
+import { argv } from 'zx'
+import { join } from 'path'
+import { readFileSync, writeFileSync } from 'fs'
 
 const inputPath = argv.input ?? join(__dirname, '..', '..', '..', 'output', 'schema', 'schema.json')
 const outputPath = argv.output ?? join(__dirname, '..', '..', '..', 'output', 'openapi', 'elasticsearch-serverless-openapi.json')
 
 const inputText = readFileSync(
-    inputPath,
-    { encoding: 'utf8' }
-);
+  inputPath,
+  { encoding: 'utf8' }
+)
 
-const output = convert_schema_to_openapi(inputText, "serverless");
+const output = convert_schema_to_openapi(inputText, 'serverless')
 
 writeFileSync(
-    outputPath,
-    output,
-    'utf8'
+  outputPath,
+  output,
+  'utf8'
 )
