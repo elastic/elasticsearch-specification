55a7114227b94f9e0c773103d2461008c333bdbc
Sylvain Wallez <sylvain@elastic.co>
2023-12-12 15:34:07 +0100

    Add codegen_name annotations to ManagedBy (#2368)

--- specification/indices/_types/DataStream.ts
+++ specification/indices/_types/DataStream.ts
@@ -30,8 +30,11 @@ import { integer } from '@_types/Numeric'
 import { DataStreamLifecycleWithRollover } from '@indices/_types/DataStreamLifecycle'
 
 enum ManagedBy {
+  /** @codegen_name ilm */
   'Index Lifecycle Management',
+  /** @codegen_name datastream */
   'Data stream lifecycle',
+  /** @codegen_name unmanaged */
   'Unmanaged'
 }
 
