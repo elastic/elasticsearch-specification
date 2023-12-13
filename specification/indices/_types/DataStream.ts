91db66f89e1c2e769dfea33cac412f8b281af8f6
Laura <laura.trotta@elastic.co>
2023-12-13 11:31:07 +0100

    fixed comment structure

--- specification/indices/_types/DataStream.ts
+++ specification/indices/_types/DataStream.ts
@@ -32,9 +32,11 @@ import { DataStreamLifecycleWithRollover } from '@indices/_types/DataStreamLifec
 enum ManagedBy {
   /** @codegen_name ilm */
   'Index Lifecycle Management',
-  /** This should have been written with capital letters, it's a known typo and should not be corrected
+  /**
+   * This should have been written with capital letters, it's a known typo and should not be corrected
    * since the test cases have already been written
-   * @codegen_name datastream */
+   * @codegen_name datastream
+   */
   'Data stream lifecycle',
   /** @codegen_name unmanaged */
   'Unmanaged'
@@ -44,7 +46,8 @@ export class DataStream {
   /**
    * Custom metadata for the stream, copied from the `_meta` object of the stream’s matching index template.
    * If empty, the response omits this property.
-   * @doc_id mapping-meta-field */
+   * @doc_id mapping-meta-field
+   */
   _meta?: Metadata
   /**
    *  If `true`, the data stream allows custom routing on write request.
