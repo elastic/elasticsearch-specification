2e792ce2d49ad7eda201e9c3e9852cea7f972105
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2023-12-22 15:58:37 +0100

    Update specification/security/get_api_key/SecurityGetApiKeyRequest.ts
    Co-authored-by: Laurent Saint-Félix <laurent.saintfelix@elastic.co>

--- specification/security/get_api_key/SecurityGetApiKeyRequest.ts
+++ specification/security/get_api_key/SecurityGetApiKeyRequest.ts
@@ -71,7 +71,7 @@ export interface Request extends RequestBase {
     /**
      * A boolean flag that can be used to query API keys that are currently active. An API key is considered active if it is neither invalidated, nor expired at query time. You can specify this together with other parameters such as `owner` or `name`. If `active_only` is false, the response will include both active and inactive (expired or invalidated) keys.
      * @server_default false
-     * @availability stack since=8.12.1
+     * @availability stack since=8.10.0
      * @availability serverless
      */
     active_only?: boolean
