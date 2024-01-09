a3376c9a023a09ee1f07042e9419262472710015
Jedr Blaszyk <jedrazb@gmail.com>
2024-01-09 14:18:31 +0100

    Re-run make contrib

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -9161,7 +9161,7 @@ export interface ConnectorFilteringRule {
   value: string
 }
 
-export type ConnectorFilteringRuleRule = 'contains' | 'ends_with' | 'equals' | 'regex' | 'starts_with' | 'greater_than' | 'less_than'
+export type ConnectorFilteringRuleRule = 'contains' | 'ends_with' | 'equals' | 'regex' | 'starts_with' | '>' | '<'
 
 export interface ConnectorFilteringRules {
   advanced_snippet: ConnectorFilteringAdvancedSnippet
@@ -9182,12 +9182,12 @@ export interface ConnectorFilteringValidation {
 export type ConnectorFilteringValidationState = 'edited' | 'invalid' | 'valid'
 
 export interface ConnectorGreaterThanValidation {
-  type: '"greater_than"'
+  type: 'greater_than'
   constraint: double
 }
 
 export interface ConnectorIncludedInValidation {
-  type: '"included_in"'
+  type: 'included_in'
   constraint: string
 }
 
@@ -9199,17 +9199,17 @@ export interface ConnectorIngestPipelineParams {
 }
 
 export interface ConnectorLessThanValidation {
-  type: '"less_than"'
+  type: 'less_than'
   constraint: double
 }
 
 export interface ConnectorListTypeValidation {
-  type: '"list_type"'
+  type: 'list_type'
   constraint: ScalarValue[]
 }
 
 export interface ConnectorRegexValidation {
-  type: '"regex"'
+  type: 'regex'
   constraint: string
 }
 
