e88415925fb3d9d234c35c7d72431f61442cf84b
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2024-02-22 15:10:34 +0100

    [Inference API] update path arguments (#2430)
    * update path arguments for inference API
    
    * prettify json spec files
    
    (cherry picked from commit d16a19a78d590c4306e4375017368b7931c8a704)

--- specification/inference/inference/InferenceRequest.ts
+++ specification/inference/inference/InferenceRequest.ts
@@ -31,13 +31,13 @@ import { TaskSettings } from '@inference/_types/Services'
 export interface Request extends RequestBase {
   path_parts: {
     /**
-     * The model task type
+     * The task type
      */
-    task_type: TaskType
+    task_type?: TaskType
     /**
-     * The unique identifier of the inference model.
+     * The inference Id
      */
-    model_id: Id
+    inference_id: Id
   }
   body: {
     /**
