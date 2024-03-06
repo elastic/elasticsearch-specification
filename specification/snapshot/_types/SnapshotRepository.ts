a66fc6152411f23f5ab09903c32c51b012e404f0
Josh Mock <joshua.mock@elastic.co>
2024-03-06 10:53:43 -0600

    Expand snapshot repository types (#2255)

--- specification/snapshot/_types/SnapshotRepository.ts
+++ specification/snapshot/_types/SnapshotRepository.ts
@@ -17,22 +17,108 @@
  * under the License.
  */
 
-import { Uuid } from '@_types/common'
+import { ByteSize, Uuid } from '@_types/common'
 import { integer } from '@_types/Numeric'
+import { Time } from '@_types/Time'
 
-export class Repository {
-  type: string
+/**
+ * @variants internal tag='type'
+ * @non_exhaustive
+ */
+export type Repository =
+  | AzureRepository
+  | GcsRepository
+  | S3Repository
+  | SharedFileSystemRepository
+  | ReadOnlyUrlRepository
+  | SourceOnlyRepository
+
+export class RepositoryBase {
   uuid?: Uuid
-  settings: RepositorySettings
 }
 
-export class RepositorySettings {
-  chunk_size?: string
-  compress?: string | boolean
-  concurrent_streams?: string | integer
+export class AzureRepository extends RepositoryBase {
+  type: 'azure'
+  settings: AzureRepositorySettings
+}
+
+export class GcsRepository extends RepositoryBase {
+  type: 'gcs'
+  settings: GcsRepositorySettings
+}
+
+export class S3Repository extends RepositoryBase {
+  type: 's3'
+  settings: S3RepositorySettings
+}
+
+export class SharedFileSystemRepository extends RepositoryBase {
+  type: 'fs'
+  settings: SharedFileSystemRepositorySettings
+}
+
+export class ReadOnlyUrlRepository extends RepositoryBase {
+  type: 'url'
+  settings: ReadOnlyUrlRepositorySettings
+}
+
+export class SourceOnlyRepository extends RepositoryBase {
+  type: 'source'
+  settings: SourceOnlyRepositorySettings
+}
+
+export class RepositorySettingsBase {
+  chunk_size?: ByteSize
+  compress?: boolean
+  max_restore_bytes_per_sec?: ByteSize
+  max_snapshot_bytes_per_sec?: ByteSize
+}
+
+export class AzureRepositorySettings extends RepositorySettingsBase {
+  client?: string
+  container?: string
+  base_path?: string
+  readonly?: boolean
+  location_mode?: string
+}
+
+export class GcsRepositorySettings extends RepositorySettingsBase {
+  bucket: string
+  client?: string
+  base_path?: string
+  readonly?: boolean
+  application_name?: string
+}
+
+export class S3RepositorySettings extends RepositorySettingsBase {
+  bucket: string
+  client?: string
+  base_path?: string
+  readonly?: boolean
+  server_side_encryption?: boolean
+  buffer_size?: ByteSize
+  canned_acl?: string
+  storage_class?: string
+}
+
+export class SharedFileSystemRepositorySettings extends RepositorySettingsBase {
   location: string
+  max_number_of_snapshots?: integer
+  readonly?: boolean
+}
+
+export class ReadOnlyUrlRepositorySettings extends RepositorySettingsBase {
+  http_max_retries?: integer
+  http_socket_timeout?: Time
+  max_number_of_snapshots?: integer
+  url: string
+}
+
+export class SourceOnlyRepositorySettings extends RepositorySettingsBase {
+  delegate_type?: string
+  max_number_of_snapshots?: integer
   /**
    * @aliases readonly
    */
-  read_only?: string | boolean
+  read_only?: boolean
 }
