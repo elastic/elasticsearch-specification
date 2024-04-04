24c64e74fd1bea85315b3b9293fa86ce8a8583ba
Laura <laura.trotta@elastic.co>
2024-04-04 18:50:28 +0200

    regen, code style

--- specification/security/query_api_keys/QueryApiKeysRequest.ts
+++ specification/security/query_api_keys/QueryApiKeysRequest.ts
@@ -39,19 +39,19 @@ export interface Request extends RequestBase {
      * @availability serverless
      */
     with_limited_by?: boolean
-     /**
-      * Determines whether to also retrieve the profile uid, for the API key owner principal, if it exists.
-      * @server_default false
-      * @availability stack since=8.14.0
-      * @availability serverless
-      */
+    /**
+     * Determines whether to also retrieve the profile uid, for the API key owner principal, if it exists.
+     * @server_default false
+     * @availability stack since=8.14.0
+     * @availability serverless
+     */
     with_profile_uid?: boolean
-     /**
-      * Determines whether aggregation names are prefixed by their respective types in the response.
-      * @server_default false
-      * @availability stack since=8.14.0
-      * @availability serverless
-      */
+    /**
+     * Determines whether aggregation names are prefixed by their respective types in the response.
+     * @server_default false
+     * @availability stack since=8.14.0
+     * @availability serverless
+     */
     typed_keys?: boolean
   }
   body: {
