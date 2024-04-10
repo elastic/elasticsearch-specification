283679bd54358c88c4fbe68b7bce4172b4b668d6
Jonathan Buttner <jonathan.buttner@elastic.co>
2024-04-10 11:40:55 -0400

    fixing formatting

--- specification/inference/inference/InferenceRequest.ts
+++ specification/inference/inference/InferenceRequest.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- 
+
 import { Duration } from '@_types/Time'
 import { RequestBase } from '@_types/Base'
 import { Id } from '@_types/common'
