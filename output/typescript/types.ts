762486cd45dcab7019b45e83c13a6df1a75f9d71
Elastic Machine <elasticmachine@users.noreply.github.com>
2024-04-11 11:02:16 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -9213,12 +9213,12 @@ export interface ConnectorFilteringValidation {
 export type ConnectorFilteringValidationState = 'edited' | 'invalid' | 'valid'
 
 export interface ConnectorGreaterThanValidation {
-  type: '"greater_than"'
+  type: 'greater_than'
   constraint: double
 }
 
 export interface ConnectorIncludedInValidation {
-  type: '"included_in"'
+  type: 'included_in'
   constraint: string
 }
 
@@ -9230,17 +9230,17 @@ export interface ConnectorIngestPipelineParams {
 }
 
 export interface ConnectorLessThanValidation {
-  type: '"less_than"'
+  type: 'less_than'
   constraint: double
 }
 
 export interface ConnectorListTypeValidation {
-  type: '"list_type"'
+  type: 'list_type'
   constraint: ScalarValue[]
 }
 
 export interface ConnectorRegexValidation {
-  type: '"regex"'
+  type: 'regex'
   constraint: string
 }
 
