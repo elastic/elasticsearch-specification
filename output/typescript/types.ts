c7f15ad0e74f538910481d94ba6583d19ca66ed1
Elastic Machine <elasticmachine@users.noreply.github.com>
2024-05-22 15:52:25 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10723,7 +10723,7 @@ export interface IndicesStorage {
   allow_mmap?: boolean
 }
 
-export type IndicesStorageType = 'fs' | '' | 'niofs' | 'mmapfs' | 'hybridfs'| string
+export type IndicesStorageType = 'fs' | 'niofs' | 'mmapfs' | 'hybridfs'| string
 
 export interface IndicesTemplateMapping {
   aliases: Record<IndexName, IndicesAlias>
