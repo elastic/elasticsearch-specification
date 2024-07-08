64cd8823988ed2f74b20a48522937b5afdb02779
Albert Zaharovits <albert.zaharovits@elastic.co>
2024-07-08 18:58:33 +0300

    RoleQueryContainer

--- specification/security/query_roles/QueryRolesRequest.ts
+++ specification/security/query_roles/QueryRolesRequest.ts
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { RoleAggregationContainer } from './types'
+import { RoleQueryContainer } from './types'
 import { RequestBase } from '@_types/Base'
 import { integer } from '@_types/Numeric'
 import { Sort, SortResults } from '@_types/sort'
