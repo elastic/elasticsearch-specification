25f73d738e8b6aeb0f97a3c9f6df86a933642aee
Fernando Briano <fernando@picandocodigo.net>
2024-08-06 14:50:00 +0100

    Update CatCountRequest.ts documentation

--- specification/cat/count/CatCountRequest.ts
+++ specification/cat/count/CatCountRequest.ts
@@ -22,7 +22,7 @@ import { Indices } from '@_types/common'
 
 /**
  * Get a document count.
- * Provides quick access to a document count for a data stream, an index, or an entire cluster.n/
+ * Provides quick access to a document count for a data stream, an index, or an entire cluster.
  * The document count only includes live documents, not deleted documents which have not yet been removed by the merge process.
  *
  * CAT APIs are only intended for human consumption using the command line or Kibana console.
