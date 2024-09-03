1e187ac8cbda1e327d1da97ed067d2ed7e98f415
Quentin Pradet <quentin.pradet@elastic.co>
2024-09-03 11:08:01 +0400

    Switch to DurationValue<UnitMillis>

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10064,9 +10064,9 @@ export interface EnrichStatsCacheStats {
   node_id: Id
   count: integer
   hits: integer
-  hits_time_in_millis: long
+  hits_time_in_millis: DurationValue<UnitMillis>
   misses: integer
-  misses_time_in_millis: long
+  misses_time_in_millis: DurationValue<UnitMillis>
   evictions: integer
   size_in_bytes: long
 }
