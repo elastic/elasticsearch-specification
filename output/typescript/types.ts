34639bac3e3b799e55b45e3a59577fb54c63ee0b
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2024-09-06 11:43:57 +0200

    doc_count already exists in parent class (#2865)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -4225,7 +4225,6 @@ export interface AggregationsTimeSeriesAggregation extends AggregationsBucketAgg
 
 export interface AggregationsTimeSeriesBucketKeys extends AggregationsMultiBucketBase {
   key: Record<Field, FieldValue>
-  doc_count: long
 }
 export type AggregationsTimeSeriesBucket = AggregationsTimeSeriesBucketKeys
   & { [property: string]: AggregationsAggregate | Record<Field, FieldValue> | long }
