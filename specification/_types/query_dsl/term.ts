44322a45a33639817b972864bbe2164b86d42e02
Quentin Pradet <quentin.pradet@elastic.co>
2024-09-26 11:39:49 +0400

    Deprecate legacy params from range query (#2939)

--- specification/_types/query_dsl/term.ts
+++ specification/_types/query_dsl/term.ts
@@ -129,7 +129,9 @@ export class RangeQueryBase<T> extends QueryBase {
    * Less than or equal to.
    */
   lte?: T
+  /** @deprecated 8.16.0 Use gte or gt instead */
   from?: T | null
+  /** @deprecated 8.16.0 Use lte or lt instead */
   to?: T | null
 }
 
