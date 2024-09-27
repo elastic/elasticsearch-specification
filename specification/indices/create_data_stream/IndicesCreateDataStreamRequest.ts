21bfe67ff3ab1a1ed77032054e9866384c32b00c
lcawl <lcawley@elastic.co>
2024-09-27 14:45:17 -0700

    Add @doc_tag to create datastream endpoint

--- specification/indices/create_data_stream/IndicesCreateDataStreamRequest.ts
+++ specification/indices/create_data_stream/IndicesCreateDataStreamRequest.ts
@@ -29,6 +29,7 @@ import { Duration } from '@_types/Time'
  * @availability stack since=7.9.0 stability=stable
  * @availability serverless stability=stable visibility=public
  * @index_privileges create_index
+ * @doc_tag data stream
  */
 export interface Request extends RequestBase {
   path_parts: {
