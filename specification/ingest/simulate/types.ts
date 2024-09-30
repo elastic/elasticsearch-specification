358da78eb06266f6194396d2b4441f7d57e0e39e
Quentin Pradet <quentin.pradet@elastic.co>
2024-09-30 13:56:47 +0400

    Move availability annotation

--- specification/ingest/simulate/types.ts
+++ specification/ingest/simulate/types.ts
@@ -27,6 +27,10 @@ import { ErrorCause } from '@_types/Errors'
 import { DateTime } from '@_types/Time'
 
 export class Ingest {
+  /**
+   * @availability stack since=8.16.0
+   * @availability serverless
+   */
   _redact?: Redact
   timestamp: DateTime
   pipeline?: Name
@@ -35,8 +39,6 @@ export class Ingest {
 export class Redact {
   /**
    * indicates if document has been redacted
-   * @availability stack since=8.16.0
-   * @availability serverless
    */
   _is_redacted: boolean
 }
