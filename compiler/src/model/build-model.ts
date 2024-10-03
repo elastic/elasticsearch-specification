5f59dc31bbf456d0c1f5f3020ce36091b7da70cd
Lisa Cawley <lcawley@elastic.co>
2024-10-03 08:17:44 -0700

    Add @doc_tag to override default tag behaviour (#2961)
    Co-authored-by: Laurent Saint-Félix <laurent.saintfelix@elastic.co>

--- compiler/src/model/build-model.ts
+++ compiler/src/model/build-model.ts
@@ -67,6 +67,7 @@ export function compileEndpoints (): Record<string, model.Endpoint> {
       name: api,
       description: spec.documentation.description,
       docUrl: spec.documentation.url,
+      docTag: spec.docTag,
       // Setting these values by default should be removed
       // when we no longer use rest-api-spec stubs as the
       // source of truth for stability/visibility.
