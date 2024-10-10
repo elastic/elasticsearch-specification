68ea3d64288750d5b0bc68bba8a6e08bbea33bfa
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2024-10-10 15:51:13 +0200

    http can be null if sec disabled (#2997)

--- specification/nodes/info/types.ts
+++ specification/nodes/info/types.ts
@@ -243,7 +243,7 @@ export class NodeInfoXpack {
 }
 
 export class NodeInfoXpackSecurity {
-  http: NodeInfoXpackSecuritySsl
+  http?: NodeInfoXpackSecuritySsl
   enabled: string
   transport?: NodeInfoXpackSecuritySsl
   authc?: NodeInfoXpackSecurityAuthc
