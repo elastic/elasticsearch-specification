7975a6ec9ad0edadbd26c77117cc47b0842f1efd
lcawl <lcawley@elastic.co>
2024-10-21 13:47:07 -0700

    Generate output

--- specification/security/create_service_token/CreateServiceTokenRequest.ts
+++ specification/security/create_service_token/CreateServiceTokenRequest.ts
@@ -26,7 +26,7 @@ import { Name, Namespace, Refresh, Service } from '@_types/common'
  * @rest_spec_name security.create_service_token
  * @availability stack stability=stable
  * @availability serverless stability=stable visibility=private
- * @ext_doc_id service-account
+ * @ext_doc_id service-accounts
  */
 export interface Request extends RequestBase {
   path_parts: {
