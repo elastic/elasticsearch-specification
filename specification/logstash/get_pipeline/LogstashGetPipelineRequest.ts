91f07bd9dba4cb47ad326aebdbd1e27f98c6722c
lcawl <lcawley@elastic.co>
2024-10-23 14:48:29 -0700

    [OpenAPI] Edits Logstash APIs

--- specification/logstash/get_pipeline/LogstashGetPipelineRequest.ts
+++ specification/logstash/get_pipeline/LogstashGetPipelineRequest.ts
@@ -21,16 +21,19 @@ import { RequestBase } from '@_types/Base'
 import { Ids } from '@_types/common'
 
 /**
- * Retrieves pipelines used for Logstash Central Management.
+ * Get Logstash pipelines.
+ *
+ * Get pipelines that are used for Logstash Central Management.
  * @rest_spec_name logstash.get_pipeline
  * @availability stack since=7.12.0 stability=stable
  * @availability serverless stability=stable visibility=public
  * @cluster_privileges manage_logstash_pipelines
+ * @ext_doc_id logstash-centralized-pipeline-management
  */
 export interface Request extends RequestBase {
   path_parts: {
     /**
-     * Comma-separated list of pipeline identifiers.
+     * A comma-separated list of pipeline identifiers.
      */
     id?: Ids
   }
