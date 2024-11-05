34171a0f53cf27d3c0d1ac2df89d95186daf1180
Laura Trotta <laura.trotta@elastic.co>
2024-11-05 12:17:28 +0100

    deprecating local param from alias req

--- specification/indices/get_alias/IndicesGetAliasRequest.ts
+++ specification/indices/get_alias/IndicesGetAliasRequest.ts
@@ -65,6 +65,7 @@ export interface Request extends RequestBase {
     /**
      * If `true`, the request retrieves information from the local node only.
      * @server_default false
+     * @deprecated 8.12.0
      */
     local?: boolean
   }
