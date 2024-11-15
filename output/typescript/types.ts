b50e63f7e32bbb8099b40209faabfe518493da10
Niels Bauman <33722607+nielsbauman@users.noreply.github.com>
2024-11-15 06:44:27 +0100

    Add missing fields to `IndexTemplate` (#3129)
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    (cherry picked from commit 2c94880087755e1659734dd9d1bac720cd21700c)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -11108,6 +11108,8 @@ export interface IndicesIndexTemplate {
   _meta?: Metadata
   allow_auto_create?: boolean
   data_stream?: IndicesIndexTemplateDataStreamConfiguration
+  deprecated?: boolean
+  ignore_missing_component_templates?: Names
 }
 
 export interface IndicesIndexTemplateDataStreamConfiguration {
