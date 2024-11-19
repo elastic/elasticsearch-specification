6fc99a3c447e0fb77e165cda201610c88b4c1c63
Quentin Pradet <quentin.pradet@elastic.co>
2024-11-19 11:42:02 +0400

    Add restriction to RoleDescriptor (#3142)
    (cherry picked from commit 496e0b85f4e9550789ad8db8d33bc6daa00a7b89)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -17699,6 +17699,12 @@ export interface SecurityReplicationAccess {
   allow_restricted_indices?: boolean
 }
 
+export interface SecurityRestriction {
+  workflows: SecurityRestrictionWorkflow[]
+}
+
+export type SecurityRestrictionWorkflow = 'search_application_query'| string
+
 export interface SecurityRoleDescriptor {
   cluster?: SecurityClusterPrivilege[]
   indices?: SecurityIndicesPrivileges[]
@@ -17710,6 +17716,7 @@ export interface SecurityRoleDescriptor {
   metadata?: Metadata
   run_as?: string[]
   description?: string
+  restriction?: SecurityRestriction
   transient_metadata?: Record<string, any>
 }
 
@@ -17724,6 +17731,7 @@ export interface SecurityRoleDescriptorRead {
   metadata?: Metadata
   run_as?: string[]
   description?: string
+  restriction?: SecurityRestriction
   transient_metadata?: Record<string, any>
 }
 
