a17adda460c41ca3c60bc0b615660f8c9fe0de60
lcawl <lcawley@elastic.co>
2024-12-04 13:58:02 -0800

    [DOCS] Edit Fleet operation summaries

--- specification/fleet/global_checkpoints/GlobalCheckpointsRequest.ts
+++ specification/fleet/global_checkpoints/GlobalCheckpointsRequest.ts
@@ -23,6 +23,9 @@ import { Duration } from '@_types/Time'
 import { Checkpoint } from '../_types/Checkpoints'
 
 /**
+ * Get global checkpoints.
+ * Get the current global checkpoints for an index.
+ * This API is design for internal use by the Fleet server project.
  * @rest_spec_name fleet.global_checkpoints
  * @availability stack since=7.13.0 stability=stable
  * @availability serverless stability=stable visibility=private
