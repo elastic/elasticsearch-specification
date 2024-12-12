ea3e9b6675a4b2ebb5bd309bb9db601d7afba8b7
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2024-12-12 10:48:20 +0100

    Fix most ML APIs (#3153)
    * updated discovery node class
    
    * correct import
    
    * fixes around ml
    
    * more ml fixes, tentative discoverynode fix
    
    * more ml fixes
    
    * even more ml fixes
    
    * other ml fixes
    
    * modeling discovery node
    
    * correct discovery nodes optionals
    
    * correcting types
    
    * removed deprecated params
    
    * Update specification/ml/_types/TrainedModel.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/_types/TrainedModel.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/_types/TrainedModel.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/_types/TrainedModel.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/_types/TrainedModel.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * addressing reviews
    
    * Update specification/ml/_types/inference.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/_types/inference.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/_types/inference.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Update specification/ml/post_data/MlPostJobDataResponse.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * more review addressing
    
    * merge fixes
    
    ---------
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>

--- specification/ml/_types/Analysis.ts
+++ specification/ml/_types/Analysis.ts
@@ -21,7 +21,7 @@ import { OverloadOf } from '@spec_utils/behaviors'
 import { CharFilter } from '@_types/analysis/char_filters'
 import { Tokenizer } from '@_types/analysis/tokenizers'
 import { TokenFilter } from '@_types/analysis/token_filters'
-import { Field } from '@_types/common'
+import { ByteSize, Field } from '@_types/common'
 import { long } from '@_types/Numeric'
 import { Duration } from '@_types/Time'
 import { Detector, DetectorRead } from './Detector'
@@ -168,7 +168,7 @@ export class AnalysisLimits {
    * The approximate maximum amount of memory resources that are required for analytical processing. Once this limit is approached, data pruning becomes more aggressive. Upon exceeding this limit, new entities are not modeled. If the `xpack.ml.max_model_memory_limit` setting has a value greater than 0 and less than 1024mb, that value is used instead of the default. The default value is relatively small to ensure that high resource usage is a conscious decision. If you have jobs that are expected to analyze high cardinality fields, you will likely need to use a higher value. If you specify a number instead of a string, the units are assumed to be MiB. Specifying a string is recommended for clarity. If you specify a byte size unit of `b` or `kb` and the number does not equate to a discrete number of megabytes, it is rounded down to the closest MiB. The minimum valid value is 1 MiB. If you specify a value less than 1 MiB, an error occurs. If you specify a value for the `xpack.ml.max_model_memory_limit` setting, an error occurs when you try to create jobs that have `model_memory_limit` values greater than that setting value.
    * @server_default 1024mb
    */
-  model_memory_limit?: string
+  model_memory_limit?: ByteSize
 }
 
 export class AnalysisMemoryLimit {
