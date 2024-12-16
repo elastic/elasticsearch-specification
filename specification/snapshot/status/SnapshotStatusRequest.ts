d4a02f4bffea6835de26a0b40c83fa510e16279e
lcawl <lcawley@elastic.co>
2024-12-16 12:16:52 -0800

    [DOCS] Edit the snapshot and restore APIs

--- specification/snapshot/status/SnapshotStatusRequest.ts
+++ specification/snapshot/status/SnapshotStatusRequest.ts
@@ -22,9 +22,21 @@ import { Name, Names } from '@_types/common'
 import { Duration } from '@_types/Time'
 
 /**
+ * Get the snapshot status.
+ * Get a detailed description of the current state for each shard participating in the snapshot.
+ * Note that this API should only be used to obtain detailed shard-level information for ongoing snapshots.
+ * If this detail is not needed or you want to obtain information about one or more existing snapshots, use the get snapshot API.
+ *
+ * WARNING: Using the API to return the status of any snapshots other than currently running snapshots can be expensive.
+ * The API requires a read from the repository for each shard in each snapshot.
+ * For example, if you have 100 snapshots with 1,000 shards each, an API request that includes all snapshots will require 100,000 reads (100 snapshots x 1,000 shards).
+ *
+ * Depending on the latency of your storage, such requests can take an extremely long time to return results.
+ * These requests can also tax machine resources and, when using cloud storage, incur high processing costs.
  * @rest_spec_name snapshot.status
  * @availability stack since=7.8.0 stability=stable
  * @availability serverless stability=stable visibility=private
+ * @cluster_privileges monitor_snapshot
  */
 export interface Request extends RequestBase {
   path_parts: {
