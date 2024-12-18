f1b87625469a0fe4e49dac12d74ccec3c47e5809
lcawl <lcawley@elastic.co>
2024-12-18 14:45:26 -0800

    Add specification for post even to an analytics collection

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -17684,6 +17684,8 @@ export interface SearchApplicationEventDataStream {
   name: IndexName
 }
 
+export type SearchApplicationEventType = 'page_view' | 'search' | 'search_click'
+
 export interface SearchApplicationSearchApplication extends SearchApplicationSearchApplicationParameters {
   name: Name
   updated_at_millis: EpochTime<UnitMillis>
@@ -17732,6 +17734,14 @@ export interface SearchApplicationListRequest extends RequestBase {
 export interface SearchApplicationListResponse {
   count: long
   results: SearchApplicationSearchApplication[]
+}
+
+export interface SearchApplicationPostBehavioralAnalyticsEventRequest extends RequestBase {
+  collection_name: Name
+  event_type: SearchApplicationEventType
+}
+
+export interface SearchApplicationPostBehavioralAnalyticsEventResponse {
 }
 
 export interface SearchApplicationPutRequest extends RequestBase {
