f21462dab7902d9b3c4f15987ff4714860b1a30c
lcawl <lcawley@elastic.co>
2024-12-18 17:40:58 -0800

    Add description

--- specification/connector/sync_job_claim/SyncJobClaimRequest.ts
+++ specification/connector/sync_job_claim/SyncJobClaimRequest.ts
@@ -22,6 +22,14 @@ import { Id } from '@_types/common'
 
 /**
  * Claim a connector sync job.
+ * This action updates the job status to `in_progress` and sets the `last_seen` and `started_at` timestamps to the current time.
+ * Additionally, it can set the `sync_cursor` property for the sync job.
+ * 
+ * This API is not intended for direct connector management by users.
+ * It supports the implementation of services that utilize the connector protocol to communicate with Elasticsearch.
+ *
+ * To sync data using self-managed connectors, you need to deploy the Elastic connector service on your own infrastructure.
+ * This service runs automatically on Elastic Cloud for Elastic managed connectors.
  * @rest_spec_name connector.sync_job_claim
  * @availability stack stability=experimental visibility=public
  * @doc_id connector-sync-job-claim
