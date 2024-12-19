e747641bc0e9973f2d5bd79ea86c4bf4258a0bd5
lcawl <lcawley@elastic.co>
2024-12-18 18:00:43 -0800

    Add specification for set connector sync job stats

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10083,6 +10083,21 @@ export interface ConnectorSyncJobPostResponse {
   id: Id
 }
 
+export interface ConnectorSyncJobUpdateStatsRequest extends RequestBase {
+  connector_sync_job_id: Id
+  body?: {
+    deleted_document_count: integer
+    indexed_document_count: integer
+    indexed_document_volume: integer
+    last_seen?: integer
+    metadata?: Record<string, any>
+    total_document_count?: integer
+  }
+}
+
+export interface ConnectorSyncJobUpdateStatsResponse {
+}
+
 export interface ConnectorUpdateActiveFilteringRequest extends RequestBase {
   connector_id: Id
 }
