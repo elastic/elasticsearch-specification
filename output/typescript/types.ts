bdf2f20dcdae80e5baf2cb53c8def9c96a69fba5
Lisa Cawley <lcawley@elastic.co>
2024-12-19 10:02:06 -0800

    Add specification for claim connector sync job (#3366)
    (cherry picked from commit 68dcbfb1d141a21d80ca1ec3cc609fb79f406d84)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10003,6 +10003,17 @@ export interface ConnectorSyncJobCheckInRequest extends RequestBase {
 export interface ConnectorSyncJobCheckInResponse {
 }
 
+export interface ConnectorSyncJobClaimRequest extends RequestBase {
+  connector_sync_job_id: Id
+  body?: {
+    sync_cursor?: any
+    worker_hostname: string
+  }
+}
+
+export interface ConnectorSyncJobClaimResponse {
+}
+
 export interface ConnectorSyncJobDeleteRequest extends RequestBase {
   connector_sync_job_id: Id
 }
