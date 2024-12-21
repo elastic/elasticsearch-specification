96726e912b6dc73f456946a934a1564e92b53caa
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2024-12-21 06:28:05 +0100

    Add shard_failures to EQL response spec (#3372)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10312,6 +10312,7 @@ export interface EqlEqlSearchResponseBase<TEvent = unknown> {
   took?: DurationValue<UnitMillis>
   timed_out?: boolean
   hits: EqlEqlHits<TEvent>
+  shard_failures?: ShardFailure[]
 }
 
 export interface EqlHitsEvent<TEvent = unknown> {
