41ea7416dac6384455a230f67fe63ab4424324d9
Lisa Cawley <lcawley@elastic.co>
2025-01-09 12:03:41 -0800

    Add SLM API examples (#3470)
    Co-authored-by: Marci W <333176+marciw@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marci W <333176+marciw@users.noreply.github.com>

--- specification/slm/put_lifecycle/PutSnapshotLifecycleRequest.ts
+++ specification/slm/put_lifecycle/PutSnapshotLifecycleRequest.ts
@@ -33,6 +33,7 @@ import { Duration } from '@_types/Time'
  * @availability serverless stability=stable visibility=private
  * @cluster_privileges manage_slm
  * @index_privileges manage
+ * @doc_id slm-api-put-policy
  */
 export interface Request extends RequestBase {
   path_parts: {
@@ -43,12 +44,16 @@ export interface Request extends RequestBase {
   }
   query_parameters: {
     /**
-     * Period to wait for a connection to the master node. If no response is received before the timeout expires, the request fails and returns an error.
+     * The period to wait for a connection to the master node.
+     * If no response is received before the timeout expires, the request fails and returns an error.
+     * To indicate that the request should never timeout, set it to `-1`.
      * @server_default 30s
      */
     master_timeout?: Duration
     /**
-     * Period to wait for a response. If no response is received before the timeout expires, the request fails and returns an error.
+     * The period to wait for a response.
+     * If no response is received before the timeout expires, the request fails and returns an error.
+     * To indicate that the request should never timeout, set it to `-1`.
      * @server_default 30s
      */
     timeout?: Duration
