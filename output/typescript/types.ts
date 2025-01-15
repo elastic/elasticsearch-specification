5f186c2877c98f898eb4c6256635c084093fd836
Quentin Pradet <quentin.pradet@elastic.co>
2025-01-15 13:52:37 +0400

    Allow null version in cat component templates API (#3519)
    (cherry picked from commit 19b608a4c947235bb69edbbe6805ca246a9b0012)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -6994,7 +6994,7 @@ export type CatAllocationResponse = CatAllocationAllocationRecord[]
 
 export interface CatComponentTemplatesComponentTemplate {
   name: string
-  version: string
+  version: string | null
   alias_count: string
   mapping_count: string
   settings_count: string
