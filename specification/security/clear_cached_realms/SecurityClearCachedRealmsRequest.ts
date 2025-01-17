86a67b3cb310b4cdf301370dc61629249ad6bfff
lcawl <lcawley@elastic.co>
2025-01-17 00:55:41 -0800

    Add remaining security API examples

--- specification/security/clear_cached_realms/SecurityClearCachedRealmsRequest.ts
+++ specification/security/clear_cached_realms/SecurityClearCachedRealmsRequest.ts
@@ -23,11 +23,17 @@ import { Names } from '@_types/common'
 /**
  * Clear the user cache.
  *
- * Evict users from the user cache. You can completely clear the cache or evict specific users.
+ * Evict users from the user cache.
+ * You can completely clear the cache or evict specific users.
+ *
+ * User credentials are cached in memory on each node to avoid connecting to a remote authentication service or hitting the disk for every incoming request.
+ * There are realm settings that you can use to configure the user cache.
+ * For more information, refer to the documentation about controlling the user cache.
  * @rest_spec_name security.clear_cached_realms
  * @availability stack stability=stable
  * @availability serverless stability=stable visibility=private
  * @doc_id security-api-clear-cache
+ * @ext_doc_id security-user-cache
  */
 export interface Request extends RequestBase {
   urls: [
@@ -37,9 +43,18 @@ export interface Request extends RequestBase {
     }
   ]
   path_parts: {
+    /**
+     * A comma-separated list of realms.
+     * To clear all realms, use an asterisk (`*`).
+     * It does not support other wildcard patterns.
+     */
     realms: Names
   }
   query_parameters: {
+    /**
+     * A comma-separated list of the users to clear from the cache.
+     * If you do not specify this parameter, the API evicts all users from the user cache.
+     */
     usernames?: string[]
   }
 }
