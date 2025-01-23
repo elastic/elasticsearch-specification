7c19ebb45d56a130d702d583f922500677f83920
Parker Timmins <parker.timmins@elastic.co>
2025-01-23 09:34:57 -0600

    _create_from defaults remove_index_blocks to true

--- specification/indices/create_from/MigrateCreateFromRequest.ts
+++ specification/indices/create_from/MigrateCreateFromRequest.ts
@@ -53,7 +53,7 @@ export class CreateFrom {
   settings_override?: IndexSettings
   /**
    * If index blocks should be removed when creating destination index (optional)
-   * @server_default false
+   * @server_default true
    */
   remove_index_blocks?: boolean
 }
