374de639655f272b37199d3e2f0b11363f1d24d8
Lisa Cawley <lcawley@elastic.co>
2025-01-23 16:31:31 -0800

    Augment painless execute API (#3589)

--- specification/_global/scripts_painless_execute/ExecutePainlessScriptRequest.ts
+++ specification/_global/scripts_painless_execute/ExecutePainlessScriptRequest.ts
@@ -19,15 +19,23 @@
 
 import { RequestBase } from '@_types/Base'
 import { Script } from '@_types/Scripting'
-import { PainlessContextSetup } from './types'
+import { PainlessContext, PainlessContextSetup } from './types'
 
 /**
  * Run a script.
+ *
  * Runs a script and returns a result.
+ * Use this API to build and test scripts, such as when defining a script for a runtime field.
+ * This API requires very few dependencies and is especially useful if you don't have permissions to write documents on a cluster.
+ *
+ * The API uses several _contexts_, which control how scripts are run, what variables are available at runtime, and what the return type is.
+ *
+ * Each context requires a script, but additional parameters depend on the context you're using for that script.
  * @rest_spec_name scripts_painless_execute
  * @availability stack since=6.3.0 stability=experimental
  * @availability serverless stability=experimental visibility=public
  * @doc_tag script
+ * @doc_id painless-execute-api
  */
 export interface Request extends RequestBase {
   urls: [
@@ -39,15 +47,17 @@ export interface Request extends RequestBase {
   body: {
     /**
      * The context that the script should run in.
+     * NOTE: Result ordering in the field contexts is not guaranteed.
      * @server_default painless_test
      */
-    context?: string
+    context?: PainlessContext
     /**
      * Additional parameters for the `context`.
+     * NOTE: This parameter is required for all contexts except `painless_test`, which is the default if no value is provided for `context`.
      */
     context_setup?: PainlessContextSetup
     /**
-     * The Painless script to execute.
+     * The Painless script to run.
      */
     script?: Script
   }
