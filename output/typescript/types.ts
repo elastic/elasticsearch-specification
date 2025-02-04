0404c6c3de3bced21d5281c3bf2c54fde75ba662
Ying <ying.mao@elastic.co>
2025-02-04 11:37:36 -0500

    Add text_embedding_bits to inference result output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -12998,6 +12998,7 @@ export interface InferenceInferenceEndpointInfo extends InferenceInferenceEndpoi
 
 export interface InferenceInferenceResult {
   text_embedding_bytes?: InferenceTextEmbeddingByteResult[]
+  text_embedding_bits?: InferenceTextEmbeddingByteResult[]
   text_embedding?: InferenceTextEmbeddingResult[]
   sparse_embedding?: InferenceSparseEmbeddingResult[]
   completion?: InferenceCompletionResult[]
