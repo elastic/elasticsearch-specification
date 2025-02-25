779fb2abe07c7c8fb3f10109326f54c1203b763e
Quentin Pradet <quentin.pradet@elastic.co>
2025-02-25 16:23:36 +0400

    Revert "Uppercase enum source mode" (#3810)
    * Revert "Switch enums from upper case to lower case"

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -11583,7 +11583,7 @@ export interface IndicesSoftDeletes {
   retention_lease?: IndicesRetentionLease
 }
 
-export type IndicesSourceMode = 'DISABLED' | 'STORED' | 'SYNTHETIC'
+export type IndicesSourceMode = 'disabled' | 'stored' | 'synthetic'
 
 export interface IndicesStorage {
   type: IndicesStorageType
