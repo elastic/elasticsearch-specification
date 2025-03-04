9004cda33d7c0f15713d531e25b10e86a608ac7a
lcawl <lcawley@elastic.co>
2025-03-04 13:22:14 -0800

    Generate output

--- specification/inference/put_watsonx/PutWatsonxRequest.ts
+++ specification/inference/put_watsonx/PutWatsonxRequest.ts
@@ -17,15 +17,14 @@
  * under the License.
  */
 
+import { RateLimitSetting } from '@inference/_types/Services'
 import { RequestBase } from '@_types/Base'
 import { Id } from '@_types/common'
-import { integer } from '@_types/Numeric'
 
 /**
  * Create a Watsonx inference endpoint.
  *
  * Creates an inference endpoint to perform an inference task with the `watsonxai` service.
- * The only valid task type for the model to perform is `text_embedding`.
  * You need an IBM Cloud Databases for Elasticsearch deployment to use the `watsonxai` inference service.
  * You can provision one through the IBM catalog, the Cloud Databases CLI plug-in, the Cloud Databases API, or Terraform.
  *
@@ -35,7 +34,7 @@ import { integer } from '@_types/Numeric'
  * Look for `"state": "fully_allocated"` in the response and ensure that the `"allocation_count"` matches the `"target_allocation_count"`.
  * Avoid creating multiple endpoints for the same model unless required, as each endpoint consumes significant resources.
  * @rest_spec_name inference.put_watsonx
- * @availability stack since=8.11.0 stability=stable visibility=public
+ * @availability stack since=8.16.0 stability=stable visibility=public
  * @availability serverless stability=stable visibility=public
  * @cluster_privileges manage_inference
  * @doc_id inference-api-put-watsonx
@@ -43,11 +42,16 @@ import { integer } from '@_types/Numeric'
 export interface Request extends RequestBase {
   urls: [
     {
-      path: '/_inference/text_embedding/{watsonx_inference_id}'
+      path: '/_inference/{task_type}/{watsonx_inference_id}'
       methods: ['PUT']
     }
   ]
   path_parts: {
+    /**
+     * The task type.
+     * The only valid task type for the model to perform is `text_embedding`.
+     */
+    task_type?: WatsonxTaskType
     /**
      * The unique identifier of the inference endpoint.
      */
@@ -65,16 +69,12 @@ export interface Request extends RequestBase {
   }
 }
 
-export enum ServiceType {
-  watsonxai
+export enum WatsonxTaskType {
+  text_embedding
 }
 
-export class RateLimitSetting {
-  /**
-   * By default, the `watsonxai` service sets the number of requests allowed per minute to 120.
-   * @server_default 120
-   */
-  requests_per_minute?: integer
+export enum ServiceType {
+  watsonxai
 }
 
 export class WatsonxServiceSettings {
@@ -107,6 +107,7 @@ export class WatsonxServiceSettings {
   project_id: string
   /**
    * This setting helps to minimize the number of rate limit errors returned from Watsonx.
+   * By default, the `watsonxai` service sets the number of requests allowed per minute to 120.
    */
   rate_limit?: RateLimitSetting
   /**
