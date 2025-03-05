3981a7cd49ac5d9e31f73d51828546bd214c1dfe
Laura Trotta <laura.trotta@elastic.co>
2025-03-05 10:36:58 +0100

    removed deprecated options from ts spec

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15954,7 +15954,6 @@ export interface MlGetTrainedModelsRequest extends RequestBase {
   exclude_generated?: boolean
   from?: integer
   include?: MlInclude
-  include_model_definition?: boolean
   size?: integer
   tags?: string | string[]
 }
