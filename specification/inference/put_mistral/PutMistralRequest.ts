f0662e648ddbc2fca6fd35f7f5b4d2d4ef820b52
lcawl <lcawley@elastic.co>
2025-03-17 21:28:53 -0700

    Add Mistral inference details

--- specification/inference/put_mistral/PutMistralRequest.ts
+++ specification/inference/put_mistral/PutMistralRequest.ts
@@ -17,32 +17,34 @@
  * under the License.
  */
 
-import { RateLimitSetting } from '@inference/_types/Services'
+import {
+  InferenceChunkingSettings,
+  RateLimitSetting
+} from '@inference/_types/Services'
 import { RequestBase } from '@_types/Base'
 import { Id } from '@_types/common'
+import { integer } from '@_types/Numeric'
 
 /**
- * Create a Watsonx inference endpoint.
+ * Create a Mistral inference endpoint.
  *
- * Creates an inference endpoint to perform an inference task with the `watsonxai` service.
- * You need an IBM Cloud Databases for Elasticsearch deployment to use the `watsonxai` inference service.
- * You can provision one through the IBM catalog, the Cloud Databases CLI plug-in, the Cloud Databases API, or Terraform.
+ * Creates an inference endpoint to perform an inference task with the `mistral` service.
  *
  * When you create an inference endpoint, the associated machine learning model is automatically deployed if it is not already running.
  * After creating the endpoint, wait for the model deployment to complete before using it.
  * To verify the deployment status, use the get trained model statistics API.
  * Look for `"state": "fully_allocated"` in the response and ensure that the `"allocation_count"` matches the `"target_allocation_count"`.
  * Avoid creating multiple endpoints for the same model unless required, as each endpoint consumes significant resources.
- * @rest_spec_name inference.put_watsonx
- * @availability stack since=8.16.0 stability=stable visibility=public
+ * @rest_spec_name inference.put_mistral
+ * @availability stack since=8.15.0 stability=stable visibility=public
  * @availability serverless stability=stable visibility=public
  * @cluster_privileges manage_inference
- * @doc_id inference-api-put-watsonx
+ * @doc_id inference-api-put-mistral
  */
 export interface Request extends RequestBase {
   urls: [
     {
-      path: '/_inference/{task_type}/{watsonx_inference_id}'
+      path: '/_inference/{task_type}/{mistral_inference_id}'
       methods: ['PUT']
     }
   ]
@@ -51,67 +53,62 @@ export interface Request extends RequestBase {
      * The task type.
      * The only valid task type for the model to perform is `text_embedding`.
      */
-    task_type: WatsonxTaskType
+    task_type: MistralTaskType
     /**
      * The unique identifier of the inference endpoint.
      */
-    watsonx_inference_id: Id
+    mistral_inference_id: Id
   }
   body: {
     /**
-     * The type of service supported for the specified task type. In this case, `watsonxai`.
+     * The chunking configuration object.
+     * @ext_doc_id inference-chunking
+     */
+    chunking_settings?: InferenceChunkingSettings
+    /**
+     * The type of service supported for the specified task type. In this case, `mistral`.
      */
     service: ServiceType
     /**
-     * Settings used to install the inference model. These settings are specific to the `watsonxai` service.
+     * Settings used to install the inference model. These settings are specific to the `mistral` service.
      */
-    service_settings: WatsonxServiceSettings
+    service_settings: MistralServiceSettings
   }
 }
 
-export enum WatsonxTaskType {
+export enum MistralTaskType {
   text_embedding
 }
 
 export enum ServiceType {
-  watsonxai
+  mistral
 }
 
-export class WatsonxServiceSettings {
+export class MistralServiceSettings {
   /**
-   * A valid API key of your Watsonx account.
-   * You can find your Watsonx API keys or you can create a new one on the API keys page.
+   * A valid API key of your Mistral account.
+   * You can find your Mistral API keys or you can create a new one on the API Keys page.
    *
    * IMPORTANT: You need to provide the API key only once, during the inference model creation.
    * The get inference endpoint API does not retrieve your API key.
    * After creating the inference model, you cannot change the associated API key.
    * If you want to use a different API key, delete the inference model and recreate it with the same name and the updated API key.
-   * @ext_doc_id watsonx-api-keys
+   * @ext_doc_id mistral-api-keys
    */
   api_key: string
   /**
-   * A version parameter that takes a version date in the format of `YYYY-MM-DD`.
-   * For the active version data parameters, refer to the Wastonx documentation.
-   * @ext_doc_id watsonx-api-version
+   * The maximum number of tokens per input before chunking occurs.
    */
-  api_version: string
+  max_input_tokens?: integer
   /**
    * The name of the model to use for the inference task.
-   * Refer to the IBM Embedding Models section in the Watsonx documentation for the list of available text embedding models.
-   * @ext_doc_id watsonx-api-models
+   * Refer to the Mistral models documentation for the list of available text embedding models.
+   * @ext_doc_id mistral-api-models
    */
-  model_id: string
+  model: string
   /**
-   * The identifier of the IBM Cloud project to use for the inference task.
-   */
-  project_id: string
-  /**
-   * This setting helps to minimize the number of rate limit errors returned from Watsonx.
-   * By default, the `watsonxai` service sets the number of requests allowed per minute to 120.
+   * This setting helps to minimize the number of rate limit errors returned from the Mistral API.
+   * By default, the `mistral` service sets the number of requests allowed per minute to 240.
    */
   rate_limit?: RateLimitSetting
-  /**
-   * The URL of the inference endpoint that you created on Watsonx.
-   */
-  url: string
 }
