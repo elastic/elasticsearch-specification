c6c9ad8af1a9cb113bb1f7944a709ceb829f8fb5
lcawl <lcawley@elastic.co>
2025-03-21 20:02:23 -0700

    Add Google AI Studio details

---
+++ specification/inference/put_googleaistudio/PutGoogleAiStudioRequest.ts
@@ -0,0 +1,102 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import {
+  InferenceChunkingSettings,
+  RateLimitSetting
+} from '@inference/_types/Services'
+import { RequestBase } from '@_types/Base'
+import { Id } from '@_types/common'
+
+/**
+ * Create an Google AI Studio inference endpoint.
+ *
+ * Create an inference endpoint to perform an inference task with the `googleaistudio` service.
+ *
+ * When you create an inference endpoint, the associated machine learning model is automatically deployed if it is not already running.
+ * After creating the endpoint, wait for the model deployment to complete before using it.
+ * To verify the deployment status, use the get trained model statistics API.
+ * Look for `"state": "fully_allocated"` in the response and ensure that the `"allocation_count"` matches the `"target_allocation_count"`.
+ * Avoid creating multiple endpoints for the same model unless required, as each endpoint consumes significant resources.
+ * @rest_spec_name inference.put_googleaistudio
+ * @availability stack since=8.15.0 stability=stable visibility=public
+ * @availability serverless stability=stable visibility=public
+ * @cluster_privileges manage_inference
+ * @doc_id inference-api-put-googleaistudio
+ */
+export interface Request extends RequestBase {
+  urls: [
+    {
+      path: '/_inference/{task_type}/{googleaistudio_inference_id}'
+      methods: ['PUT']
+    }
+  ]
+  path_parts: {
+    /**
+     * The type of the inference task that the model will perform.
+     */
+    task_type: GoogleAiStudioTaskType
+    /**
+     * The unique identifier of the inference endpoint.
+     */
+    googleaistudio_inference_id: Id
+  }
+  body: {
+    /**
+     * The chunking configuration object.
+     * @ext_doc_id inference-chunking
+     */
+    chunking_settings?: InferenceChunkingSettings
+    /**
+     * The type of service supported for the specified task type. In this case, `googleaistudio`.
+     */
+    service: ServiceType
+    /**
+     * Settings used to install the inference model. These settings are specific to the `googleaistudio` service.
+     */
+    service_settings: GoogleAiStudioServiceSettings
+  }
+}
+
+export enum GoogleAiStudioTaskType {
+  completion,
+  text_embedding
+}
+
+export enum ServiceType {
+  googleaistudio
+}
+
+export class GoogleAiStudioServiceSettings {
+  /**
+   * A valid API key of your Google Gemini account.
+   */
+  api_key: string
+  /**
+   * The name of the model to use for the inference task.
+   * Refer to the Google documentation for the list of supported models.
+   * @ext_doc_id googleaistudio-models
+   */
+  model_id: string
+  /**
+   * This setting helps to minimize the number of rate limit errors returned from Google AI Studio.
+   * By default, the `googleaistudio` service sets the number of requests allowed per minute to 360.
+   */
+  rate_limit?: RateLimitSetting
+}
