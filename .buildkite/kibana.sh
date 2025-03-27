97182c7b75199a39042da0948b2e5548ce08e6f1
Quentin Pradet <quentin.pradet@elastic.co>
2025-03-27 11:50:42 +0400

    Add debug ls statements

--- .buildkite/kibana.sh
+++ .buildkite/kibana.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+ls
+ls .buildkite
+
 # Since we're into the current repo, move to the top-level
 cd ..
 
@@ -26,6 +29,10 @@ git fetch origin pull/213375/head:es-9.0.0-alpha.4
 git checkout es-9.0.0-alpha.4
 git config --global user.email "clients-team@elastic.co"
 git config --global user.name "Clients CI"
+ls
+ls ..
+ls ../elasticsearch-specification
+ls ../elasticsearch-specification/.buildkite
 git am ../elasticsearch-specification/.buildkite/0001-Fix-require_alias-use.patch
 
 echo "--- Install Node.js and Yarn"
