281fb86c263c9166062670fea59707f667ba1e61
Quentin Pradet <quentin.pradet@elastic.co>
2025-03-27 12:09:57 +0400

    more debug and attempted fix

--- .buildkite/kibana.sh
+++ .buildkite/kibana.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
+set -x
+pwd
 ls
 ls .buildkite
+set +x
 
 # Since we're into the current repo, move to the top-level
 cd ..
@@ -29,11 +32,14 @@ git fetch origin pull/213375/head:es-9.0.0-alpha.4
 git checkout es-9.0.0-alpha.4
 git config --global user.email "clients-team@elastic.co"
 git config --global user.name "Clients CI"
+set -x
+pwd
 ls
 ls ..
-ls ../elasticsearch-specification
-ls ../elasticsearch-specification/.buildkite
-git am ../elasticsearch-specification/.buildkite/0001-Fix-require_alias-use.patch
+ls ../kibana-type-checks
+ls ../kibana-type-checks/.buildkite
+set +x
+git am ../kibana-type-checks/.buildkite/0001-Fix-require_alias-use.patch
 
 echo "--- Install Node.js and Yarn"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
