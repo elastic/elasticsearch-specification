01c2702ab7a4e95808a9b2c4eb0e52198c8cee1f
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-03-28 11:55:35 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -5386,6 +5386,10 @@ export interface MappingBooleanProperty extends MappingDocValuesPropertyBase {
   fielddata?: IndicesNumericFielddata
   index?: boolean
   null_value?: boolean
+  ignore_malformed?: boolean
+  script?: Script | string
+  on_script_error?: MappingOnScriptError
+  time_series_dimension?: boolean
   type: 'boolean'
 }
 
