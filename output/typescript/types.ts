f22adb6578817d606ea57d9de89c789e7979cf1c
Quentin Pradet <quentin.pradet@elastic.co>
2025-04-08 16:42:57 +0400

    Remove timeout from clone snapshot API (#4234)
    We removed it from 8.18 already.

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -20555,7 +20555,6 @@ export interface SnapshotCloneRequest extends RequestBase {
   snapshot: Name
   target_snapshot: Name
   master_timeout?: Duration
-  timeout?: Duration
   body?: {
     indices: string
   }
