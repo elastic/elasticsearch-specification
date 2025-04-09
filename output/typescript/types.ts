2ab7fa805694030dbec5261b9e91da0c6ec25e9c
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-04-09 14:41:08 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10606,7 +10606,14 @@ export interface EsqlAsyncEsqlResult extends EsqlEsqlResult {
   is_running: boolean
 }
 
-export interface EsqlClusterInfo {
+export interface EsqlEsqlClusterDetails {
+  status: EsqlEsqlClusterStatus
+  indices: string
+  took?: DurationValue<UnitMillis>
+  _shards?: EsqlEsqlShardInfo
+}
+
+export interface EsqlEsqlClusterInfo {
   total: integer
   successful: integer
   running: integer
@@ -10616,29 +10623,22 @@ export interface EsqlClusterInfo {
   details: Record<string, EsqlEsqlClusterDetails>
 }
 
-export interface EsqlColumnInfo {
+export type EsqlEsqlClusterStatus = 'running' | 'successful' | 'partial' | 'skipped' | 'failed'
+
+export interface EsqlEsqlColumnInfo {
   name: string
   type: string
 }
-
-export interface EsqlEsqlClusterDetails {
-  status: EsqlEsqlClusterStatus
-  indices: string
-  took?: DurationValue<UnitMillis>
-  _shards?: EsqlEsqlShardInfo
-}
-
-export type EsqlEsqlClusterStatus = 'running' | 'successful' | 'partial' | 'skipped' | 'failed'
 
 export type EsqlEsqlFormat = 'csv' | 'json' | 'tsv' | 'txt' | 'yaml' | 'cbor' | 'smile' | 'arrow'
 
 export interface EsqlEsqlResult {
   took?: DurationValue<UnitMillis>
   is_partial?: boolean
-  all_columns?: EsqlColumnInfo[]
-  columns: EsqlColumnInfo[]
+  all_columns?: EsqlEsqlColumnInfo[]
+  columns: EsqlEsqlColumnInfo[]
   values: FieldValue[][]
-  _clusters?: EsqlClusterInfo
+  _clusters?: EsqlEsqlClusterInfo
   profile?: any
 }
 
