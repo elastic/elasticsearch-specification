3fe65a348574c0bdab2910d74101b78a95f1f40e
Quentin Pradet <quentin.pradet@elastic.co>
2025-04-10 16:09:08 +0400

    Add back inference.inference API
    It’s important for Python and can be a fallback in cases where new providers
    are missing. Statically-typed clients are expected to exclude this endpoint.

---
+++ specification/inference/inference/InferenceRequest.ts
@@ -0,0 +1,89 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { TaskSettings } from '@inference/_types/Services'
+import { TaskType } from '@inference/_types/TaskType'
+import { RequestBase } from '@_types/Base'
+import { Id } from '@_types/common'
+import { Duration } from '@_types/Time'
+
+/**
+ * Perform inference on the service.
+ *
+ * This API enables you to use machine learning models to perform specific tasks on data that you provide as an input.
+ * It returns a response with the results of the tasks.
+ * The inference endpoint you use can perform one specific task that has been defined when the endpoint was created with the create inference API.
+ *
+ * > info
+ * > The inference APIs enable you to use certain services, such as built-in machine learning models (ELSER, E5), models uploaded through Eland, Cohere, OpenAI, Azure, Google AI Studio, Google Vertex AI, Anthropic, Watsonx.ai, or Hugging Face. For built-in models and models uploaded through Eland, the inference APIs offer an alternative way to use and manage trained models. However, if you do not plan to use the inference APIs to use these models or if you want to use non-NLP models, use the machine learning trained model APIs.
+ * @rest_spec_name inference.inference
+ * @availability stack since=8.11.0 stability=stable visibility=public
+ * @availability serverless stability=stable visibility=public
+ * @cluster_privileges monitor_inference
+ * @doc_id inference-api-post
+ */
+export interface Request extends RequestBase {
+  urls: [
+    {
+      path: '/_inference/{inference_id}'
+      methods: ['POST']
+    },
+    {
+      path: '/_inference/{task_type}/{inference_id}'
+      methods: ['POST']
+    }
+  ]
+  path_parts: {
+    /**
+     * The type of inference task that the model performs.
+     */
+    task_type?: TaskType
+    /**
+     * The unique identifier for the inference endpoint.
+     */
+    inference_id: Id
+  }
+  query_parameters: {
+    /**
+     * The amount of time to wait for the inference request to complete.
+     * @server_default 30s
+     */
+    timeout?: Duration
+  }
+  body: {
+    /**
+     * The query input, which is required only for the `rerank` task.
+     * It is not required for other tasks.
+     */
+    query?: string
+    /**
+     * The text on which you want to perform the inference task.
+     * It can be a single string or an array.
+     *
+     * > info
+     * > Inference endpoints for the `completion` task type currently only support a single string as input.
+     */
+    input: string | Array<string>
+    /**
+     * Task settings for the individual inference request.
+     * These settings are specific to the task type you specified and override the task settings specified when initializing the service.
+     */
+    task_settings?: TaskSettings
+  }
+}
