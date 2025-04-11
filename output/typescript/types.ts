19aecf980d55157e10a13e3ea0497ec9ab0171af
Quentin Pradet <quentin.pradet@elastic.co>
2025-04-11 16:59:20 +0400

    Add back inference.inference API (#4256)
    * Add back inference.inference API
    
    It’s important for Python and can be a fallback in cases where new providers
    are missing. Statically-typed clients are expected to exclude this endpoint.
    
    * Mention service-specific documentation
    
    Co-authored-by: Lisa Cawley <lcawley@elastic.co>
    
    ---------
    
    Co-authored-by: Lisa Cawley <lcawley@elastic.co>
    (cherry picked from commit d1072a9314368e66cfb32c04432e52bbfd9a4554)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13323,6 +13323,15 @@ export interface InferenceInferenceEndpointInfo extends InferenceInferenceEndpoi
   task_type: InferenceTaskType
 }
 
+export interface InferenceInferenceResult {
+  text_embedding_bytes?: InferenceTextEmbeddingByteResult[]
+  text_embedding_bits?: InferenceTextEmbeddingByteResult[]
+  text_embedding?: InferenceTextEmbeddingResult[]
+  sparse_embedding?: InferenceSparseEmbeddingResult[]
+  completion?: InferenceCompletionResult[]
+  rerank?: InferenceRankedDocument[]
+}
+
 export interface InferenceJinaAIServiceSettings {
   api_key: string
   model_id?: string
@@ -13514,6 +13523,19 @@ export interface InferenceGetRequest extends RequestBase {
 export interface InferenceGetResponse {
   endpoints: InferenceInferenceEndpointInfo[]
 }
+
+export interface InferenceInferenceRequest extends RequestBase {
+  task_type?: InferenceTaskType
+  inference_id: Id
+  timeout?: Duration
+  body?: {
+    query?: string
+    input: string | string[]
+    task_settings?: InferenceTaskSettings
+  }
+}
+
+export type InferenceInferenceResponse = InferenceInferenceResult
 
 export interface InferencePutRequest extends RequestBase {
   task_type?: InferenceTaskType
