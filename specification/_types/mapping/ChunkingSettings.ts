870962ed0086a92c8584929a12f931eea5e98d04
Kathleen DeRusso <kathleen.derusso@elastic.co>
2025-04-11 14:37:38 -0400

    Linting

--- specification/_types/mapping/ChunkingSettings.ts
+++ specification/_types/mapping/ChunkingSettings.ts
@@ -17,12 +17,11 @@
  * under the License.
  */
 
-import { integer } from "@_types/Numeric"
-import { InferenceChunkingSettings } from "@inference/_types/Services"
-import { OverloadOf } from "@spec_utils/behaviors"
+import { InferenceChunkingSettings } from '@inference/_types/Services'
+import { OverloadOf } from '@spec_utils/behaviors'
+import { integer } from '@_types/Numeric'
 
 export class ChunkingSettings implements OverloadOf<InferenceChunkingSettings> {
-
   strategy: string
 
   max_chunk_size: integer
