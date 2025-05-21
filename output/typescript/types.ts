7d45a330d8fa5d1deda44c4d7e1175ac955dcec4
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-05-21 07:18:09 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -1746,7 +1746,7 @@ export interface SearchShardProfile {
   index: IndexName
   node_id: NodeId
   searches: SearchSearchProfile[]
-  shard_id: long
+  shard_id: integer
 }
 
 export interface SearchSmoothingModelContainer {
@@ -20698,7 +20698,7 @@ export interface SnapshotSnapshotShardFailure {
   index: IndexName
   node_id?: Id
   reason: string
-  shard_id: Id
+  shard_id: integer
   index_uuid: Id
   status: string
 }
