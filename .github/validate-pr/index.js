ecd349da58f72cf195d406307ecdcb5ffcbecf71
Josh Mock <joshua.mock@elastic.co>
2025-05-23 16:04:38 -0500

    Skip markdown files in validate-pr script

--- .github/validate-pr/index.js
+++ .github/validate-pr/index.js
@@ -95,7 +95,7 @@ async function run() {
     if (file.endsWith('eslint.config.js')) continue
     if (file.endsWith('package.json')) continue
     if (file.endsWith('package-lock.json')) continue
-    console.log(file)
+    if (file.endsWith('.md')) continue
     if (getApi(file).endsWith('_types')) {
       const apis = specification.endpoints
         .filter(endpoint => endpoint.name.split('.').filter(s => !privateNames.includes(s))[0] === getApi(file).split('.')[0])
