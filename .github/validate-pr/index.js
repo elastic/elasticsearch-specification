f07e3c5fc8bebff3b1ee18107c159955995b94fe
Josh Mock <joshua.mock@elastic.co>
2025-05-23 14:41:18 -0500

    Ignore new ESLint-related files in validate-pr script

--- .github/validate-pr/index.js
+++ .github/validate-pr/index.js
@@ -90,7 +90,11 @@ async function run() {
     if (file.startsWith('specification/_spec_utils')) continue
     if (file.startsWith('specification/_doc_ids')) continue
     if (file.startsWith('specification/_json_spec')) continue
+    if (file.startsWith('specification/node_modules')) continue
     if (file === 'specification/tsconfig.json') continue
+    if (file === 'specification/eslint.config.js') continue
+    if (file === 'specification/package.json') continue
+    if (file === 'specification/package-lock.json') continue
     if (getApi(file).endsWith('_types')) {
       const apis = specification.endpoints
         .filter(endpoint => endpoint.name.split('.').filter(s => !privateNames.includes(s))[0] === getApi(file).split('.')[0])
