984ce87847b808f1188f8fc9cd9b9c7e49047457
Josh Mock <joshua.mock@elastic.co>
2025-05-23 11:51:06 -0500

    Appease the linter

--- specification/eslint.config.js
+++ specification/eslint.config.js
@@ -1,19 +1,19 @@
-import parser from '@typescript-eslint/parser'
-import validator from 'eslint-plugin-es-spec'
-import { defineConfig } from 'eslint/config'
+import parser from "@typescript-eslint/parser";
+import validator from "eslint-plugin-es-spec";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig({
-  files: ['**/*.ts'],
+  files: ["**/*.ts"],
   languageOptions: {
     parser,
     parserOptions: {
       projectService: true,
-      tsconfigRootDir: import.meta.dirname
-    }
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
-  plugins: { 'es-spec-validator': validator },
+  plugins: { "es-spec-validator": validator },
   rules: {
-    'es-spec-validator/single-key-dictionary-key-is-string': 'error',
-    'es-spec-validator/invalid-node-types': 'warn',
-  }
-})
+    "es-spec-validator/single-key-dictionary-key-is-string": "error",
+    "es-spec-validator/invalid-node-types": "warn",
+  },
+});
