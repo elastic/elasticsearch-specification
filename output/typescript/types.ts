8860d69cc621f486415aaa99b8f8076fa32994c9
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-05-30 14:20:51 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13308,7 +13308,7 @@ export interface InferenceAzureOpenAITaskSettings {
 
 export type InferenceAzureOpenAITaskType = 'completion' | 'text_embedding'
 
-export type InferenceCohereEmbeddingType = 'byte' | 'float' | 'int8'
+export type InferenceCohereEmbeddingType = 'binary' | 'bit' | 'byte' | 'float' | 'int8'
 
 export type InferenceCohereInputType = 'classification' | 'clustering' | 'ingest' | 'search'
 
