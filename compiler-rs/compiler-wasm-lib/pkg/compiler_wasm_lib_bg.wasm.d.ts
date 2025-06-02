becc661b974cc2fad3a6a07f792a12367ef8e071
Sylvain Wallez <sylvain@elastic.co>
2025-06-02 09:25:17 +0200

    [OpenAPI] Add a CLI to configure export parameters (#4426) (#4437)

--- compiler-rs/compiler-wasm-lib/pkg/compiler_wasm_lib_bg.wasm.d.ts
+++ compiler-rs/compiler-wasm-lib/pkg/compiler_wasm_lib_bg.wasm.d.ts
@@ -2,8 +2,8 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function convert_schema_to_openapi(a: number, b: number, c: number, d: number, e: number): void;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_free(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
