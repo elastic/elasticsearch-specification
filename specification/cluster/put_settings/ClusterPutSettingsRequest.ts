2590782d4672eb29801744e9fd137c2ebd475cc4
Lisa Cawley <lcawley@elastic.co>
2025-06-09 09:02:24 -0700

    [DOCS] Add link for cluster settings (#4491)
    (cherry picked from commit 93cbe0b3bcd4996a2939c4ffa1dd7553441e32b5)

--- specification/cluster/put_settings/ClusterPutSettingsRequest.ts
+++ specification/cluster/put_settings/ClusterPutSettingsRequest.ts
@@ -45,6 +45,7 @@ import { UserDefinedValue } from '@spec_utils/UserDefinedValue'
  * @availability stack stability=stable
  * @availability serverless stability=stable visibility=private
  * @doc_id cluster-update-settings
+ * @ext_doc_id stack-settings
  */
 export interface Request extends RequestBase {
   urls: [
@@ -61,7 +62,9 @@ export interface Request extends RequestBase {
     timeout?: Duration
   }
   body: {
+    /** The settings that persist after the cluster restarts. */
     persistent?: Dictionary<string, UserDefinedValue>
+    /** The settings that do not persist after the cluster restarts. */
     transient?: Dictionary<string, UserDefinedValue>
   }
 }
