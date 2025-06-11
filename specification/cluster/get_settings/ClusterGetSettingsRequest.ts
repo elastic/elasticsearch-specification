9b7392bdd0c342f39b49d9cedbc11f2eb1c6e6e1
lcawl <lcawley@elastic.co>
2025-06-11 16:08:03 -0700

    Reflect changes to example generation

--- specification/cluster/get_settings/ClusterGetSettingsRequest.ts
+++ specification/cluster/get_settings/ClusterGetSettingsRequest.ts
@@ -25,7 +25,7 @@ import { Duration } from '@_types/Time'
  *
  * By default, it returns only settings that have been explicitly defined.
  * @rest_spec_name cluster.get_settings
- * @availability stack since=9.1.0 stability=stable
+ * @availability stack stability=stable
  * @availability serverless stability=stable visibility=private
  * @cluster_privileges monitor
  * @doc_id cluster-get-settings
