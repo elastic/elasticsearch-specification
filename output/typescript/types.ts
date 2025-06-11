7d2bc3477aa3cd1999ed3a3be02b93a106897d42
Stanislav Malyshev <smalyshev@users.noreply.github.com>
2025-06-11 01:01:15 -0600

    Fix AsyncQueryResuest - those parameters should be in the body (#4517)
    (cherry picked from commit 0ac4f3b154e93370517d5e6e3551e0a1be11470a)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10781,9 +10781,6 @@ export interface EsqlAsyncQueryRequest extends RequestBase {
   delimiter?: string
   drop_null_columns?: boolean
   format?: EsqlQueryEsqlFormat
-  keep_alive?: Duration
-  keep_on_completion?: boolean
-  wait_for_completion_timeout?: Duration
   body?: {
     columnar?: boolean
     filter?: QueryDslQueryContainer
@@ -10794,6 +10791,8 @@ export interface EsqlAsyncQueryRequest extends RequestBase {
     tables?: Record<string, Record<string, EsqlTableValuesContainer>>
     include_ccs_metadata?: boolean
     wait_for_completion_timeout?: Duration
+    keep_alive?: Duration
+    keep_on_completion?: boolean
   }
 }
 
