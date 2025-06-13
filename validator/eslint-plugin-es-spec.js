809187aa8584cc1c6027e785019a0cd7f9cb1d6a
Laura Trotta <laura.trotta@elastic.co>
2025-06-13 10:32:53 +0200

    address nit

--- validator/eslint-plugin-es-spec.js
+++ validator/eslint-plugin-es-spec.js
@@ -17,13 +17,13 @@
  * under the License.
  */
 import singleKeyDict from './rules/single-key-dictionary-key-is-string.js'
-import Dict from './rules/dictionary-key-is-string.js'
+import dict from './rules/dictionary-key-is-string.js'
 import invalidNodeTypes from './rules/invalid-node-types.js'
 
 export default {
   rules: {
     'single-key-dictionary-key-is-string': singleKeyDict,
-    'dictionary-key-is-string': Dict,
+    'dictionary-key-is-string': dict,
     'invalid-node-types': invalidNodeTypes,
   }
 }
