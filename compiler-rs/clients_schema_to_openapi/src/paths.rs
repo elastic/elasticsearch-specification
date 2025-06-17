688cb3447d94699a50f1ca5b89324caf922e11d5
Miguel Grinberg <miguel.grinberg@gmail.com>
2025-06-17 14:37:55 +0100

    Only add x-codeSamples extensions to docs builds of openapi (#4582)

--- compiler-rs/clients_schema_to_openapi/src/paths.rs
+++ compiler-rs/clients_schema_to_openapi/src/paths.rs
@@ -347,31 +347,34 @@ pub fn add_endpoint(
         // add the x-state extension for availability
         let mut extensions = crate::availability_as_extensions(&endpoint.availability, &tac.config.flavor);
 
-        // add the x-codeSamples extension
-        let mut code_samples = vec![];
-        if let Some(examples) = request.examples.clone() {
-            if let Some((_, example)) = examples.first() {
-                let request_line = example.method_request.clone().unwrap_or(String::from(""));
-                let request_body = example.value.clone().unwrap_or(String::from(""));
-                if !request_line.is_empty() {
-                    code_samples.push(serde_json::json!({
-                        "lang": "Console",
-                        "source": request_line + "\n" + request_body.as_str(),
-                    }));
-                }
-                if let Some(alternatives) = example.alternatives.clone() {
-                    for alternative in alternatives.iter() {
+        if tac.config.include_language_examples {
+            // add the x-codeSamples extension
+            let mut code_samples = vec![];
+            if let Some(examples) = request.examples.clone() {
+                if let Some((_, example)) = examples.first() {
+                    let request_line = example.method_request.clone().unwrap_or(String::from(""));
+                    let request_body = example.value.clone().unwrap_or(String::from(""));
+                    if !request_line.is_empty() {
                         code_samples.push(serde_json::json!({
-                            "lang": alternative.language,
-                            "source": alternative.code.as_str(),
+                            "lang": "Console",
+                            "source": request_line + "\n" + request_body.as_str(),
                         }));
+                    }
+                    if let Some(alternatives) = example.alternatives.clone() {
+                        for alternative in alternatives.iter() {
+                            code_samples.push(serde_json::json!({
+                                "lang": alternative.language,
+                                "source": alternative.code.as_str(),
+                            }));
+                        }
                     }
                 }
             }
+            if !code_samples.is_empty() {
+                extensions.insert("x-codeSamples".to_string(), serde_json::json!(code_samples));
+            }
         }
-        if !code_samples.is_empty() {
-            extensions.insert("x-codeSamples".to_string(), serde_json::json!(code_samples));
-        }
+
         extensions.append(&mut crate::product_meta_as_extensions(namespace, product_meta));
 
         // Create the operation, it will be repeated if we have several methods
