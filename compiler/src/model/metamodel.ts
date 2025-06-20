7d0ded378e901654bbf73f0207733fb98298378d
Miguel Grinberg <miguel.grinberg@gmail.com>
2025-06-20 18:53:40 +0100

    Add previous version documentation URLs to schema and OpenAPI (#4638)

--- compiler/src/model/metamodel.ts
+++ compiler/src/model/metamodel.ts
@@ -443,6 +443,7 @@ export class Endpoint {
   docId?: string
   extDocId?: string
   extDocUrl?: string
+  extPreviousVersionDocUrl?: string
   deprecation?: Deprecation
   availability: Availabilities
   docTag?: string
