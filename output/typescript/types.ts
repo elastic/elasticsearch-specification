d059f8dc85d3fdeaa3474d16a93c6fcdeca75a86
Pat Whelan <pat.whelan@elastic.co>
2025-07-01 12:01:15 -0400

    [ML] Add DeepSeek (#4723)
    * [ML] Add DeepSeek
    
    Create put request types, objects, and examples for DeepSeek.
    Verified with `make validate api=inference.put`.
    
    * Update specification/inference/_types/CommonTypes.ts
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    
    * Remove example alternatives; remove duplicate task type enum
    
    ---------
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>
    (cherry picked from commit f190eecde34b50c608d8a728a3e88c408b3c58ea)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13668,6 +13668,14 @@ export interface InferenceContentObject {
   type: string
 }
 
+export interface InferenceDeepSeekServiceSettings {
+  api_key: string
+  model_id: string
+  url?: string
+}
+
+export type InferenceDeepSeekServiceType = 'deepseek'
+
 export interface InferenceDeleteInferenceEndpointResult extends AcknowledgedResponseBase {
   pipelines: string[]
 }
@@ -13792,6 +13800,11 @@ export interface InferenceInferenceEndpointInfoAzureOpenAI extends InferenceInfe
 export interface InferenceInferenceEndpointInfoCohere extends InferenceInferenceEndpoint {
   inference_id: string
   task_type: InferenceTaskTypeCohere
+}
+
+export interface InferenceInferenceEndpointInfoDeepSeek extends InferenceInferenceEndpoint {
+  inference_id: string
+  task_type: InferenceTaskTypeDeepSeek
 }
 
 export interface InferenceInferenceEndpointInfoELSER extends InferenceInferenceEndpoint {
@@ -13963,6 +13976,8 @@ export type InferenceTaskTypeAzureAIStudio = 'text_embedding' | 'completion'
 export type InferenceTaskTypeAzureOpenAI = 'text_embedding' | 'completion'
 
 export type InferenceTaskTypeCohere = 'text_embedding' | 'rerank' | 'completion'
+
+export type InferenceTaskTypeDeepSeek = 'completion' | 'chat_completion'
 
 export type InferenceTaskTypeELSER = 'sparse_embedding'
 
@@ -14175,6 +14190,18 @@ export interface InferencePutCohereRequest extends RequestBase {
 }
 
 export type InferencePutCohereResponse = InferenceInferenceEndpointInfoCohere
+
+export interface InferencePutDeepseekRequest extends RequestBase {
+  task_type: InferenceTaskTypeDeepSeek
+  deepseek_inference_id: Id
+  body?: {
+    chunking_settings?: InferenceInferenceChunkingSettings
+    service: InferenceDeepSeekServiceType
+    service_settings: InferenceDeepSeekServiceSettings
+  }
+}
+
+export type InferencePutDeepseekResponse = InferenceInferenceEndpointInfoDeepSeek
 
 export interface InferencePutElasticsearchRequest extends RequestBase {
   task_type: InferenceElasticsearchTaskType
