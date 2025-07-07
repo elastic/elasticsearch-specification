cd0a74cf3796e08fad982304a5ab204277b43beb
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-07 17:43:41 +0400

    Remove master_timeout from ingest.get_ip_location_database

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15036,7 +15036,6 @@ export interface IngestGetIpLocationDatabaseDatabaseConfigurationMetadata {
 
 export interface IngestGetIpLocationDatabaseRequest extends RequestBase {
   id?: Ids
-  master_timeout?: Duration
 }
 
 export interface IngestGetIpLocationDatabaseResponse {
