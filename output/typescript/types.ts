1fc197e6f38cf65609f4c9156a6a2fb1138bf889
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-07 18:25:03 +0400

    Add missing cause to cluster.put_component_template (#4795)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -9612,6 +9612,7 @@ export type ClusterPostVotingConfigExclusionsResponse = boolean
 export interface ClusterPutComponentTemplateRequest extends RequestBase {
   name: Name
   create?: boolean
+  cause?: string
   master_timeout?: Duration
   body?: {
     template: IndicesIndexState
