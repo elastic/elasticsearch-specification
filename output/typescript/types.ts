8f7684368e046da8c8134954e9cc78cc71eb8087
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-08 10:51:26 +0400

    Add settings_filter to cluster.get_component_template

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -9478,6 +9478,7 @@ export type ClusterExistsComponentTemplateResponse = boolean
 export interface ClusterGetComponentTemplateRequest extends RequestBase {
   name?: Name
   flat_settings?: boolean
+  settings_filter?: string | string[]
   include_defaults?: boolean
   local?: boolean
   master_timeout?: Duration
