c22f1922f2c96a74a65cc02553252c60ec23fe41
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-07-10 11:22:31 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -14007,6 +14007,7 @@ export interface InferenceInferenceRequest extends RequestBase {
   body?: {
     query?: string
     input: string | string[]
+    input_type?: string
     task_settings?: InferenceTaskSettings
   }
 }
