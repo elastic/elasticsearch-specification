1935b8dfcc60e035e3b798e6cb80636e55164f93
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-11 17:29:40 +0400

    Add refresh to security.grant_api_key (#4872)
    (cherry picked from commit e847a834f2dd019e9d2281a401e8861cc78a153b)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -20204,6 +20204,7 @@ export interface SecurityGrantApiKeyGrantApiKey {
 }
 
 export interface SecurityGrantApiKeyRequest extends RequestBase {
+  refresh?: Refresh
   body?: {
     api_key: SecurityGrantApiKeyGrantApiKey
     grant_type: SecurityGrantApiKeyApiKeyGrantType
