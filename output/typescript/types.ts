4b6aa3f8a011af91db2d13e6f26370c45650f48a
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-11 17:22:43 +0400

    Remove query parameters from security.get_user_privileges (#4869)
    They are not supported as this API only returns privileges from the current
    users and accepts no parameters.
    
    (cherry picked from commit d7b2276cc92771044cf5ef464a332bca35123d3d)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -20167,9 +20167,6 @@ export interface SecurityGetUserRequest extends RequestBase {
 export type SecurityGetUserResponse = Record<string, SecurityUser>
 
 export interface SecurityGetUserPrivilegesRequest extends RequestBase {
-  application?: Name
-  priviledge?: Name
-  username?: Name | null
 }
 
 export interface SecurityGetUserPrivilegesResponse {
