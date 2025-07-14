ed3f286f2079cdda64650ad14b0260c3a12a1771
kosabogi <boglarka.kosa@elastic.co>
2025-07-14 11:02:06 +0200

    Updates Cohere inference API

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13706,7 +13706,7 @@ export type InferenceCohereInputType = 'classification' | 'clustering' | 'ingest
 export interface InferenceCohereServiceSettings {
   api_key: string
   embedding_type?: InferenceCohereEmbeddingType
-  model_id?: string
+  model_id: string
   rate_limit?: InferenceRateLimitSetting
   similarity?: InferenceCohereSimilarityType
 }
@@ -13716,7 +13716,7 @@ export type InferenceCohereServiceType = 'cohere'
 export type InferenceCohereSimilarityType = 'cosine' | 'dot_product' | 'l2_norm'
 
 export interface InferenceCohereTaskSettings {
-  input_type?: InferenceCohereInputType
+  input_type: InferenceCohereInputType
   return_documents?: boolean
   top_n?: integer
   truncate?: InferenceCohereTruncateType
