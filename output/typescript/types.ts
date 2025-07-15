5964b74721c031941c4d30b3dea72e392672125d
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-15 15:42:46 +0400

    Add ccs_minimize_roundtrips

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -10834,6 +10834,7 @@ export interface EqlSearchRequest extends RequestBase {
   allow_partial_search_results?: boolean
   allow_partial_sequence_results?: boolean
   expand_wildcards?: ExpandWildcards
+  ccs_minimize_roundtrips?: boolean
   ignore_unavailable?: boolean
   keep_alive?: Duration
   keep_on_completion?: boolean
