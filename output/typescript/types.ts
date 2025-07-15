962864afc642906e322b2d72bddad956f5a687ba
David Kyle <david.kyle@elastic.co>
2025-07-15 10:57:55 +0100

    Add timeout params for PUT inference and POST inference/_stream (#4895)
    (cherry picked from commit 7fbfa61ea927b7998e5021ba4a4ca721dbc40052)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -14019,6 +14019,7 @@ export type InferenceInferenceResponse = InferenceInferenceResult
 export interface InferencePutRequest extends RequestBase {
   task_type?: InferenceTaskType
   inference_id: Id
+  timeout?: Duration
   body?: InferenceInferenceEndpoint
 }
 
