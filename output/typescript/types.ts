deee578971d9ad38c87676368d63121f12848df3
kosabogi <105062005+kosabogi@users.noreply.github.com>
2025-07-15 11:13:45 +0200

    Updates Cohere inference API (#4896)
    (cherry picked from commit a7876a88620687d84e4cf83e9be1323531463d88)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13663,7 +13663,7 @@ export type InferenceCohereInputType = 'classification' | 'clustering' | 'ingest
 export interface InferenceCohereServiceSettings {
   api_key: string
   embedding_type?: InferenceCohereEmbeddingType
-  model_id?: string
+  model_id: string
   rate_limit?: InferenceRateLimitSetting
   similarity?: InferenceCohereSimilarityType
 }
@@ -13673,7 +13673,7 @@ export type InferenceCohereServiceType = 'cohere'
 export type InferenceCohereSimilarityType = 'cosine' | 'dot_product' | 'l2_norm'
 
 export interface InferenceCohereTaskSettings {
-  input_type?: InferenceCohereInputType
+  input_type: InferenceCohereInputType
   return_documents?: boolean
   top_n?: integer
   truncate?: InferenceCohereTruncateType
