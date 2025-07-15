c370c5d4cf321a4f16e6abc2ce1648f14458ac3b
Joshua Adams <joshua.adams@elastic.co>
2025-07-15 15:34:51 +0100

    Apply suggestions from code review
    Apply suggested improvements from code review
    
    Co-authored-by: Lisa Cawley <lcawley@elastic.co>
    Co-authored-by: Laura Trotta <153528055+l-trotta@users.noreply.github.com>

--- specification/cluster/allocation_explain/ClusterAllocationExplainRequest.ts
+++ specification/cluster/allocation_explain/ClusterAllocationExplainRequest.ts
@@ -25,7 +25,7 @@ import { Duration } from '@_types/Time'
 /**
  * Explain the shard allocations.
  * Get explanations for shard allocations in the cluster.
- * This API accepts the current_node, index, primary and shard parameters via the request body, or via query parameters, but not via both at the same time.
+ * This API accepts the current_node, index, primary and shard parameters in the request body or in query parameters, but not in both at the same time.
  * For unassigned shards, it provides an explanation for why the shard is unassigned.
  * For assigned shards, it provides an explanation for why the shard is remaining on its current node and has not moved or rebalanced to another node.
  * This API can be very useful when attempting to diagnose why a shard is unassigned or why a shard continues to remain on its current node when you might expect otherwise.
