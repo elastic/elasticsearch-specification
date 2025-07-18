49262849630d994f02e5f903638ef795de9152f1
Quentin Pradet <quentin.pradet@elastic.co>
2025-07-18 14:28:21 +0400

    Fix indices.delete_alias response (#2951)
    * Fix indices.delete_api response
    
    * specific ack response for indices delete alias
    
    ---------
    
    Co-authored-by: Laura Trotta <laura.trotta@elastic.co>

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -12375,6 +12375,10 @@ export interface IndicesDeleteRequest extends RequestBase {
 
 export type IndicesDeleteResponse = IndicesResponseBase
 
+export interface IndicesDeleteAliasIndicesAliasesResponseBody extends AcknowledgedResponseBase {
+  errors?: boolean
+}
+
 export interface IndicesDeleteAliasRequest extends RequestBase {
   index: Indices
   name: Names
@@ -12382,7 +12386,7 @@ export interface IndicesDeleteAliasRequest extends RequestBase {
   timeout?: Duration
 }
 
-export type IndicesDeleteAliasResponse = AcknowledgedResponseBase
+export type IndicesDeleteAliasResponse = IndicesDeleteAliasIndicesAliasesResponseBody
 
 export interface IndicesDeleteDataLifecycleRequest extends RequestBase {
   name: DataStreamNames
