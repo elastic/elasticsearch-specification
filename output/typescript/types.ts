f0bf57ee8684acef87c53620a5ed559a357b6be7
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-07-21 15:20:24 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13882,6 +13882,32 @@ export interface InferenceContentObject {
   type: string
 }
 
+export interface InferenceCustomRequestParams {
+  content: string
+}
+
+export interface InferenceCustomResponseParams {
+  json_parser: any
+}
+
+export interface InferenceCustomServiceSettings {
+  headers?: any
+  input_type?: any
+  query_parameters?: any
+  request: InferenceCustomRequestParams
+  response: InferenceCustomResponseParams
+  secret_parameters: any
+  url?: string
+}
+
+export type InferenceCustomServiceType = 'custom'
+
+export interface InferenceCustomTaskSettings {
+  parameters?: any
+}
+
+export type InferenceCustomTaskType = 'text_embedding' | 'sparse_embedding' | 'rerank' | 'completion'
+
 export interface InferenceDeepSeekServiceSettings {
   api_key: string
   model_id: string
@@ -14014,6 +14040,11 @@ export interface InferenceInferenceEndpointInfoAzureOpenAI extends InferenceInfe
 export interface InferenceInferenceEndpointInfoCohere extends InferenceInferenceEndpoint {
   inference_id: string
   task_type: InferenceTaskTypeCohere
+}
+
+export interface InferenceInferenceEndpointInfoCustom extends InferenceInferenceEndpoint {
+  inference_id: string
+  task_type: InferenceTaskTypeCustom
 }
 
 export interface InferenceInferenceEndpointInfoDeepSeek extends InferenceInferenceEndpoint {
@@ -14190,6 +14221,8 @@ export type InferenceTaskTypeAzureAIStudio = 'text_embedding' | 'completion'
 export type InferenceTaskTypeAzureOpenAI = 'text_embedding' | 'completion'
 
 export type InferenceTaskTypeCohere = 'text_embedding' | 'rerank' | 'completion'
+
+export type InferenceTaskTypeCustom = 'text_embedding' | 'sparse_embedding' | 'rerank' | 'completion'
 
 export type InferenceTaskTypeDeepSeek = 'completion' | 'chat_completion'
 
@@ -14412,6 +14445,19 @@ export interface InferencePutCohereRequest extends RequestBase {
 }
 
 export type InferencePutCohereResponse = InferenceInferenceEndpointInfoCohere
+
+export interface InferencePutCustomRequest extends RequestBase {
+  task_type: InferenceCustomTaskType
+  custom_inference_id: Id
+  body?: {
+    chunking_settings?: InferenceInferenceChunkingSettings
+    service: InferenceCustomServiceType
+    service_settings: InferenceCustomServiceSettings
+    task_settings?: InferenceCustomTaskSettings
+  }
+}
+
+export type InferencePutCustomResponse = InferenceInferenceEndpointInfoCustom
 
 export interface InferencePutDeepseekRequest extends RequestBase {
   task_type: InferenceTaskTypeDeepSeek
