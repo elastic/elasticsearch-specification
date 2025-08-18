01c5a7be4e159f6adef8be48b14aacd260e75143
lcawl <lcawley@elastic.co>
2025-08-18 14:12:26 -0700

    Fix inference availability values

--- specification/inference/put_deepseek/PutDeepSeekRequest.ts
+++ specification/inference/put_deepseek/PutDeepSeekRequest.ts
@@ -32,7 +32,7 @@ import { TaskTypeDeepSeek } from '@inference/_types/TaskType'
  *
  * Create an inference endpoint to perform an inference task with the `deepseek` service.
  * @rest_spec_name inference.put_deepseek
- * @availability stack since=9.1.0 stability=stable visibility=public
+ * @availability stack since=8.19.0 stability=stable visibility=public
  * @availability serverless stability=stable visibility=public
  * @cluster_privileges manage_inference
  * @doc_id inference-api-put-deepseek
