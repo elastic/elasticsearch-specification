a9f3c3c7fff440ee696528b3187417d8e579f607
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-09-26 09:36:07 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -3099,6 +3099,7 @@ export interface WriteResponseBase {
   _seq_no?: SequenceNumber
   _shards: ShardStatistics
   _version: VersionNumber
+  failure_store?: BulkFailureStoreStatus
   forced_refresh?: boolean
 }
 
