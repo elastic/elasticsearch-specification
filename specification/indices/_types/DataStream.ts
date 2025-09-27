636ab1c6995726a2aa564ca0eadda0caf697017c
Keith Massey <keith.massey@elastic.co>
2025-09-27 00:35:12 -0500

    Adding specification for the new data stream settings APIs (#4416) (#5342)
    (cherry picked from commit 9e631d8579446c2c24e91995d4a93654773047c4)
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>

--- specification/indices/_types/DataStream.ts
+++ specification/indices/_types/DataStream.ts
@@ -28,6 +28,7 @@ import {
 } from '@_types/common'
 import { integer } from '@_types/Numeric'
 import { DataStreamLifecycleWithRollover } from '@indices/_types/DataStreamLifecycle'
+import { IndexSettings } from '@indices/_types/IndexSettings'
 
 enum ManagedBy {
   ilm = 'Index Lifecycle Management',
@@ -104,6 +105,11 @@ export class DataStream {
    * If `true`, the next write to this data stream will trigger a rollover first and the document will be indexed in the new backing index. If the rollover fails the indexing request will fail too.
    */
   rollover_on_write: boolean
+  /**
+   * The settings specific to this data stream that will take precedence over the settings in the matching index
+   * template.
+   */
+  settings: IndexSettings
   /**
    * Health status of the data stream.
    * This health status is based on the state of the primary and replica shards of the stream’s backing indices.
