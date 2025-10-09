977228719c2e42a7701212f03162a911afb2c9d0
David Kyle <david.kyle@elastic.co>
2025-10-09 10:20:29 +0100

    autogen

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -14877,7 +14877,9 @@ export interface InferenceRerankRequest extends RequestBase {
   timeout?: Duration
   body?: {
     query: string
-    input: string | string[]
+    input: string[]
+    return_documents?: boolean
+    top_n?: integer
     task_settings?: InferenceTaskSettings
   }
 }
