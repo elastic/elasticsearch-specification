aaf870ff8c329a1577a3e98fde7e2a601726b166
Quentin Pradet <quentin.pradet@elastic.co>
2025-10-13 17:00:01 +0400

    Specification fixes (#5472)
    * Remove VersionType.force
    
    It got removed in 2019 in Elasticsearch 7.5.
    
    * Switch to lowercase default_operator
    
    They are the default values used in the Operator enum.
    
    * Fix streams.status master_timeout
    
    * Run make contrib
    
    (cherry picked from commit c59570341f6984f98e056f0a6df367729d57d85d)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -3091,7 +3091,7 @@ export type VersionNumber = long
 
 export type VersionString = string
 
-export type VersionType = 'internal' | 'external' | 'external_gte' | 'force'
+export type VersionType = 'internal' | 'external' | 'external_gte'
 
 export type WaitForActiveShardOptions = 'all' | 'index-setting'
 
@@ -22231,7 +22231,7 @@ export interface StreamsStatusLogsStatus {
 }
 
 export interface StreamsStatusRequest extends RequestBase {
-  master_timeout?: TimeUnit
+  master_timeout?: Duration
 }
 
 export interface StreamsStatusResponse {
