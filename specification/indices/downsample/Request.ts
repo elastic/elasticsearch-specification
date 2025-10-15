346085d5804cbf1d1a8b7037a31a15b82ac2481f
gmarouli <mary.gouseti@elastic.co>
2025-10-15 18:36:20 +0300

    Add new sampling method to the downsample API

--- specification/indices/downsample/Request.ts
+++ specification/indices/downsample/Request.ts
@@ -23,7 +23,11 @@ import { DownsampleConfig } from '@indices/_types/Downsample'
 
 /**
  * Downsample an index.
- * Aggregate a time series (TSDS) index and store pre-computed statistical summaries (`min`, `max`, `sum`, `value_count` and `avg`) for each metric field grouped by a configured time interval.
+ * Downsamples a time series (TSDS) index and reduces its size by keeping the last value or by pre-aggregating metrics:
+ *
+ * - When running in `aggregate` mode, it pre-calculates and stores statistical summaries (`min`, `max`, `sum`, `value_count` and `avg`)
+ * for each metric field grouped by a configured time interval and their dimensions.
+ * - When running in `last_value` mode, it keeps the last value for each metric in the configured interval and their dimensions.
  * For example, a TSDS index that contains metrics sampled every 10 seconds can be downsampled to an hourly index.
  * All documents within an hour interval are summarized and stored as a single document in the downsample index.
  *
