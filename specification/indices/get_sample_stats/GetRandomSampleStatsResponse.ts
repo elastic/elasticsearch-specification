7bcc8c1de5b36c2ba91a9a859d07bdc2016511ef
Keith Massey <keith.massey@elastic.co>
2025-10-20 15:20:48 -0500

    prettier

--- specification/indices/get_sample_stats/GetRandomSampleStatsResponse.ts
+++ specification/indices/get_sample_stats/GetRandomSampleStatsResponse.ts
@@ -25,20 +25,20 @@ import { Duration, DurationValue, UnitMillis } from '@_types/Time'
  * It reuses the standard SearchResponse structure.
  */
 export class Response {
-  body: { 
-          potential_samples: long,
-          samples_rejected_for_max_samples_exceeded: long,
-          samples_rejected_for_condition: long,
-          samples_rejected_for_rate: long,
-          samples_rejected_for_exception: long,
-          samples_rejected_for_size: long,
-          samples_accepted: long,
-          time_sampling?: Duration,
-          time_sampling_millis: DurationValue<UnitMillis>,
-          time_evaluating_condition?: Duration,
-          time_evaluating_condition_millis: DurationValue<UnitMillis>,
-          time_compiling_condition?: Duration,
-          time_compiling_condition_millis: DurationValue<UnitMillis>,
-          last_exception?: string
-        }
+  body: {
+    potential_samples: long
+    samples_rejected_for_max_samples_exceeded: long
+    samples_rejected_for_condition: long
+    samples_rejected_for_rate: long
+    samples_rejected_for_exception: long
+    samples_rejected_for_size: long
+    samples_accepted: long
+    time_sampling?: Duration
+    time_sampling_millis: DurationValue<UnitMillis>
+    time_evaluating_condition?: Duration
+    time_evaluating_condition_millis: DurationValue<UnitMillis>
+    time_compiling_condition?: Duration
+    time_compiling_condition_millis: DurationValue<UnitMillis>
+    last_exception?: string
+  }
 }
