fc462cc7d32a6c1313478d7f53500457284d027b
Luke Whiting <luke.whiting@elastic.co>
2025-10-28 17:34:51 +0000

    Add new params to cat segments API (#5554)
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -8720,6 +8720,11 @@ export interface CatSegmentsRequest extends CatCatRequestBase {
   s?: Names
   local?: boolean
   master_timeout?: Duration
+  expand_wildcards?: ExpandWildcards
+  allow_no_indices?: boolean
+  ignore_throttled?: boolean
+  ignore_unavailable?: boolean
+  allow_closed?: boolean
 }
 
 export type CatSegmentsResponse = CatSegmentsSegmentsRecord[]
