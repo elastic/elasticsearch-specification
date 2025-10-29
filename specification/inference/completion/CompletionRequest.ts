8dd0bcacafa552fb665e5c191bde7b24fd5eecc4
kosabogi <105062005+kosabogi@users.noreply.github.com>
2025-10-29 10:01:27 +0100

    Fixing descriptions in the Inference APIs (#5549)

--- specification/inference/completion/CompletionRequest.ts
+++ specification/inference/completion/CompletionRequest.ts
@@ -62,7 +62,7 @@ export interface Request extends RequestBase {
      */
     input: string | Array<string>
     /**
-     * Optional task settings
+     * Task settings for the individual inference request. These settings are specific to the <task_type> you specified and override the task settings specified when initializing the service.
      */
     task_settings?: TaskSettings
   }
