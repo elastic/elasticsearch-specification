3871c6637cafd41cc3cbb800cfb1d094f22d782d
Quentin Pradet <quentin.pradet@gmail.com>
2025-10-31 13:38:26 +0400

    Revert "Allow single function in FunctionScoreQuery.functions (#5576)"
    This reverts commit 9bd939c42dba0f52f7848849b8a7508c2b6733c3.

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -6412,7 +6412,7 @@ export type QueryDslFunctionScoreMode = 'multiply' | 'sum' | 'avg' | 'first' | '
 
 export interface QueryDslFunctionScoreQuery extends QueryDslQueryBase {
   boost_mode?: QueryDslFunctionBoostMode
-  functions?: QueryDslFunctionScoreContainer | QueryDslFunctionScoreContainer[]
+  functions?: QueryDslFunctionScoreContainer[]
   max_boost?: double
   min_score?: double
   query?: QueryDslQueryContainer
@@ -6771,7 +6771,7 @@ export interface QueryDslQueryContainer {
   dis_max?: QueryDslDisMaxQuery
   distance_feature?: QueryDslDistanceFeatureQuery
   exists?: QueryDslExistsQuery
-  function_score?: QueryDslFunctionScoreQuery | QueryDslFunctionScoreContainer | QueryDslFunctionScoreContainer[]
+  function_score?: QueryDslFunctionScoreQuery | QueryDslFunctionScoreContainer[]
   fuzzy?: Partial<Record<Field, QueryDslFuzzyQuery | string | double | boolean>>
   geo_bounding_box?: QueryDslGeoBoundingBoxQuery
   geo_distance?: QueryDslGeoDistanceQuery
