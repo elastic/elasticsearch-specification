0f60d1ada8c71380ea730e8906c18df83db5f314
Quentin Pradet <quentin.pradet@elastic.co>
2025-10-31 11:18:35 +0400

    Fix indices.put_data_lifecycle API (#5583) (#5588)
    (cherry picked from commit 5c8d75f0979890f64184a42c7f59b4b3c50aa2c2)
    
    # Conflicts:
    #	output/openapi/elasticsearch-serverless-openapi.json

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -12904,7 +12904,7 @@ export interface IndicesPutDataLifecycleRequest extends RequestBase {
   timeout?: Duration
   body?: {
     data_retention?: Duration
-    downsampling?: IndicesDataStreamLifecycleDownsampling
+    downsampling?: IndicesDownsamplingRound[]
     enabled?: boolean
   }
 }
