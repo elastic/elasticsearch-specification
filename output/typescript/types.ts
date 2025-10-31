8febe64e089da86e01413dcc448ec486c1c8d7ec
Quentin Pradet <quentin.pradet@elastic.co>
2025-10-31 10:13:22 +0400

    Fix indices.put_data_lifecycle API (#5583)
    (cherry picked from commit 5c8d75f0979890f64184a42c7f59b4b3c50aa2c2)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -13017,7 +13017,7 @@ export interface IndicesPutDataLifecycleRequest extends RequestBase {
   timeout?: Duration
   body?: {
     data_retention?: Duration
-    downsampling?: IndicesDataStreamLifecycleDownsampling
+    downsampling?: IndicesDownsamplingRound[]
     enabled?: boolean
   }
 }
