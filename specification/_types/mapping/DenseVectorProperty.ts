0a885d90aa51fc7bd12dc2b909d7e01ce04d992f
Simon Cooper <simon.cooper@elastic.co>
2025-11-24 12:03:49 +0000

    Add new options for generic vector formats to dense_vector

--- specification/_types/mapping/DenseVectorProperty.ts
+++ specification/_types/mapping/DenseVectorProperty.ts
@@ -76,7 +76,12 @@ export enum DenseVectorElementType {
   /**
    * Indexes a 4-byte floating-point value per dimension.
    */
-  float
+  float,
+  /**
+   * Indexes a 2-byte floating-point value per dimension.
+   * @availability stack since=9.3.0
+   */
+  bfloat16
 }
 
 export enum DenseVectorSimilarity {
@@ -166,8 +171,8 @@ export class DenseVectorIndexOptions {
   /**
    * `true` if vector rescoring should be done on-disk
    *
-   * Only applicable to `bbq_hnsw`
-   * @server_default false
+   * Only applicable to `bbq_disk`, `bbq_hnsw`, `int4_hnsw`, `int8_hnsw`
+   * @availability stack since=9.3.0 stability=preview
    */
   on_disk_rescore?: boolean
 }
