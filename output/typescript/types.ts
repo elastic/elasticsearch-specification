a51173ea2e36c67e827717b8b9f569c83b67b0df
Mary Gouseti <mary.gouseti@elastic.co>
2025-12-04 16:36:23 +0200

    Fix data stream lifecycle type, to correctly handle downsampling. (#5593)
    (cherry picked from commit 3582774b3846f82e706a1898baa59b421d13a1cd)

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -11879,12 +11879,8 @@ export interface IndicesDataStreamIndex {
 
 export interface IndicesDataStreamLifecycle {
   data_retention?: Duration
-  downsampling?: IndicesDataStreamLifecycleDownsampling
+  downsampling: IndicesDownsamplingRound[]
   enabled?: boolean
-}
-
-export interface IndicesDataStreamLifecycleDownsampling {
-  rounds: IndicesDownsamplingRound[]
 }
 
 export interface IndicesDataStreamLifecycleRolloverConditions {
