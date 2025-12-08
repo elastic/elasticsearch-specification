1cd36d69ee3f8e4b113a93dcc43092859e4e5cee
Quentin Pradet <quentin.pradet@elastic.co>
2025-12-08 17:40:29 +0400

    Fix cluster.put_component_template API (#5370)
    * Fix cluster.put_component_template API
    
    * Revert "Fix cluster.put_component_template API"
    
    This reverts commit 69c9eee0cb67d05a2e7b58348dccd84a572541d4.
    
    * Properly use `DataStreamOptions` and fix `cluster.put_component_template`
    
    * Also use `IndexTemplateMapping` in `IndicesSimulateIndexTemplateRequest`
    
    * Allow `null` for `data_stream_options`
    
    * Revert "Also use `IndexTemplateMapping` in `IndicesSimulateIndexTemplateRequest`"
    
    This reverts commit 85a3defdfb51896799ca24c84f1bce212da3f949.
    
    ---------
    
    Co-authored-by: Niels Bauman <nielsbauman@gmail.com>
    Co-authored-by: Mary Gouseti <mary.gouseti@elastic.co>

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -9708,7 +9708,7 @@ export interface ClusterComponentTemplateSummary {
   mappings?: MappingTypeMapping
   aliases?: Record<string, IndicesAliasDefinition>
   lifecycle?: IndicesDataStreamLifecycleWithRollover
-  data_stream_options?: IndicesDataStreamOptionsTemplate | null
+  data_stream_options?: IndicesDataStreamOptions
 }
 
 export interface ClusterAllocationExplainAllocationDecision {
@@ -9995,7 +9995,7 @@ export interface ClusterPutComponentTemplateRequest extends RequestBase {
   cause?: string
   master_timeout?: Duration
   body?: {
-    template: IndicesIndexState
+    template: IndicesPutIndexTemplateIndexTemplateMapping
     version?: VersionNumber
     _meta?: Metadata
     deprecated?: boolean
@@ -12370,7 +12370,7 @@ export interface IndicesIndexTemplateSummary {
   mappings?: MappingTypeMapping
   settings?: IndicesIndexSettings
   lifecycle?: IndicesDataStreamLifecycleWithRollover
-  data_stream_options?: IndicesDataStreamOptionsTemplate | null
+  data_stream_options?: IndicesDataStreamOptions
 }
 
 export interface IndicesIndexVersioning {
@@ -13515,6 +13515,7 @@ export interface IndicesPutIndexTemplateIndexTemplateMapping {
   mappings?: MappingTypeMapping
   settings?: IndicesIndexSettings
   lifecycle?: IndicesDataStreamLifecycle
+  data_stream_options?: IndicesDataStreamOptionsTemplate | null
 }
 
 export interface IndicesPutIndexTemplateRequest extends RequestBase {
