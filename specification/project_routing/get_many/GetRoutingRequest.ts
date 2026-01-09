43f38b5b396e3c426d9af542f7fde0e93bc67d40
lcawl <lcawley@elastic.co>
2026-01-09 08:41:51 -0800

    Fix typo

--- specification/project_routing/get_many/GetRoutingRequest.ts
+++ specification/project_routing/get_many/GetRoutingRequest.ts
@@ -22,7 +22,7 @@ import { MediaType } from '@_types/common'
 
 /**
  * Get project routing expressions.
- * @doc_id operation-project-routing-get-many
+ * @doc_id project-routing-get-many
  * @rest_spec_name project_routing.get_many
  * @availability serverless stability=experimental visibility=public
  * @cluster_privileges monitor
