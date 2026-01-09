bdbc31354b5d7999d217842adedc6c6edab44114
kosabogi <105062005+kosabogi@users.noreply.github.com>
2026-01-09 15:11:27 +0100

    Update specification/indices/put_data_stream_settings/IndicesPutDataStreamSettingsRequest.ts
    Co-authored-by: István Zoltán Szabó <szabosteve@gmail.com>

--- specification/indices/put_data_stream_settings/IndicesPutDataStreamSettingsRequest.ts
+++ specification/indices/put_data_stream_settings/IndicesPutDataStreamSettingsRequest.ts
@@ -25,7 +25,7 @@ import { IndexSettings } from '@indices/_types/IndexSettings'
 /**
  * Update data stream settings.
  *
- * NOTE: Available in 8.19. Not available in earlier 8.x versions.
+ * NOTE: Available in 8.19. Not available in earlier versions.
  *
  * This API can be used to override settings on specific data streams. These overrides will take precedence over what
  * is specified in the template that the data stream matches. To prevent your data stream from getting into an invalid state,
