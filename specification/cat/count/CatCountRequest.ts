9bf4a953df014813da8e35543bcd7e96eeeadbab
Charlotte Hoblik <sarolta@saroltah.hu>
2026-02-11 17:36:56 +0100

    add note

--- specification/cat/count/CatCountRequest.ts
+++ specification/cat/count/CatCountRequest.ts
@@ -28,6 +28,8 @@ import { CatCountColumns, CatRequestBase } from '@cat/_types/CatBase'
  *
  * IMPORTANT: CAT APIs are only intended for human consumption using the command line or Kibana console.
  * They are not intended for use by applications. For application consumption, use the count API.
+ *
+ * NOTE: Starting in Elasticsearch 9.3.0, this endpoint also supports the `POST` method. This is primarily intended for project routing in serverless environments.
  * @rest_spec_name cat.count
  * @availability stack stability=stable
  * @availability serverless stability=stable visibility=public
