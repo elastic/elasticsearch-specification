a4dfdfebc299c3ce07762d7a88cbc0dba4594683
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-03-02 15:26:12 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -14061,7 +14061,8 @@ export type IndicesUpdateAliasesResponse = AcknowledgedResponseBase
 export interface IndicesValidateQueryIndicesValidationExplanation {
   error?: string
   explanation?: string
-  index: IndexName
+  index?: IndexName
+  shard?: integer
   valid: boolean
 }
 
