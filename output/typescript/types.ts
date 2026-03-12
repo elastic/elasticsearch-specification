31a73e88e934de1174f8782afd035329fa444523
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-03-12 14:51:09 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -1675,7 +1675,6 @@ export interface SearchPhraseSuggester extends SearchSuggesterBase {
   separator?: string
   shard_size?: integer
   smoothing?: SearchSmoothingModelContainer
-  text?: string
   token_limit?: integer
 }
 
@@ -1878,7 +1877,6 @@ export interface SearchTermSuggester extends SearchSuggesterBase {
   sort?: SearchSuggestSort
   string_distance?: SearchStringDistance
   suggest_mode?: SuggestMode
-  text?: string
 }
 
 export interface SearchTotalHits {
