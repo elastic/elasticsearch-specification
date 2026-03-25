052feb8958b2630e2d478e1bae7dddb96ae610c0
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-03-25 08:59:26 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -23428,6 +23428,7 @@ export interface TransformSettings {
   docs_per_second?: float
   max_page_search_size?: integer
   use_point_in_time?: boolean
+  num_failure_retries?: integer
   unattended?: boolean
 }
 
