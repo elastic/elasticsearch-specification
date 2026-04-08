c671e66cf65217eea036d55df0bdea1566bc519d
Craig Taverner <craig@amanzi.com>
2026-04-08 10:56:52 +0200

    Make ESQL Views tech-preview

--- specification/esql/put_view/PutViewRequest.ts
+++ specification/esql/put_view/PutViewRequest.ts
@@ -25,8 +25,8 @@ import { Id, MediaType } from '@_types/common'
  *
  * @rest_spec_name esql.put_view
  * @index_privileges create_view
- * @availability stack since=9.3.0 stability=experimental visibility=feature_flag feature_flag=esql_views
- * @availability serverless stability=experimental visibility=feature_flag feature_flag=esql_views
+ * @availability stack since=9.4.0 stability=experimental visibility=public
+ * @availability serverless stability=experimental visibility=public
  * @doc_id esql-put-view
  */
 export interface Request extends RequestBase {
