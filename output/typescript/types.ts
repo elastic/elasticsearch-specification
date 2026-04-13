01c220370fa01a34efe87d3538972e101ed38e92
Jan-Kazlouski-elastic <jan.kazlouski@elastic.co>
2026-04-13 15:43:23 +0300

    [Backport 9.3] Remove return_documents from ContextualAITaskSettings (#6194)
    Backport c376fd6d01cd8d60c6ce229ca4822624de4563fd from #6190.

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -14531,7 +14531,6 @@ export type InferenceContextualAIServiceType = 'contextualai'
 
 export interface InferenceContextualAITaskSettings {
   instruction?: string
-  return_documents?: boolean
   top_k?: integer
 }
 
