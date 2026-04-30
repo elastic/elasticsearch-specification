3acd1e220ac54554cd6053476438e0f5549a7f2b
Chris Parrinello <chris.parrinello@elastic.co>
2026-04-30 14:16:40 -0500

    Specification changes for https://github.com/elastic/elasticsearch/issues/143783 (#6254)
    - Added a new SearchRequestCoordinatorMetadata object under the
      search response Profile object

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -1746,6 +1746,7 @@ export interface SearchPointInTimeReference {
 
 export interface SearchProfile {
   shards: SearchShardProfile[]
+  request?: SearchSearchRequestCoordinatorMetadata
 }
 
 export interface SearchQueryBreakdown {
@@ -1846,6 +1847,11 @@ export interface SearchSearchRequestBody {
   pit?: SearchPointInTimeReference
   runtime_mappings?: MappingRuntimeFields
   stats?: string[]
+}
+
+export interface SearchSearchRequestCoordinatorMetadata {
+  source?: SearchSearchRequestBody
+  indices?: IndexName[]
 }
 
 export interface SearchShardProfile {
