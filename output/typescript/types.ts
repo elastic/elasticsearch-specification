8c628ef4825f8a1573300ebebc66c5273b5c4713
Donal Evans <donal.evans@elastic.co>
2026-06-01 09:27:40 -0700

    Correct Jina inference service setting name (#6335)
    The name of the service setting used to specify the embedding element
    type for the Jina integration is "embedding_type", not "element_type"

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -15072,7 +15072,7 @@ export interface InferenceJinaAIServiceSettings {
   rate_limit?: InferenceRateLimitSetting
   similarity?: InferenceJinaAISimilarityType
   dimensions?: integer
-  element_type?: InferenceJinaAIElementType
+  embedding_type?: InferenceJinaAIElementType
   multimodal_model?: boolean
 }
 
