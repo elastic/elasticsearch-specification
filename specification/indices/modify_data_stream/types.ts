5df7656a6d1a2ffa055a9f74203248406085dbbf
James Baiera <james.baiera@gmail.com>
2026-06-22 22:00:02 -0400

    Update specification/indices/modify_data_stream/types.ts
    Co-authored-by: Liam Thompson <leemthompo@gmail.com>

--- specification/indices/modify_data_stream/types.ts
+++ specification/indices/modify_data_stream/types.ts
@@ -35,6 +35,7 @@ export class Action {
    */
   remove_backing_index?: IndexAndDataStreamAction
   /**
+   * @availability stack since=9.5.0 stability=stable
    * Deletes a backing index from a data stream.
    * The deletion is atomically executed along with the other actions in this operation.
    * A data stream’s write index cannot be deleted.
