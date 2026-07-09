1b1e33f0c54a08fbca3eec0e21093a8b296ccb38
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-07-09 14:00:02 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -19200,6 +19200,14 @@ export interface NodesAdaptiveSelection {
   rank?: string
 }
 
+export interface NodesAllocations {
+  shards?: integer
+  undesired_shards?: integer
+  forecasted_ingest_load?: double
+  forecasted_disk_usage_in_bytes?: long
+  current_disk_usage_in_bytes?: long
+}
+
 export interface NodesBreaker {
   estimated_size?: string
   estimated_size_in_bytes?: long
@@ -19630,6 +19638,7 @@ export interface NodesSizeHttpHistogram {
 
 export interface NodesStats {
   adaptive_selection?: Record<string, NodesAdaptiveSelection>
+  allocations?: NodesAllocations
   breakers?: Record<string, NodesBreaker>
   fs?: NodesFileSystem
   host?: Host
