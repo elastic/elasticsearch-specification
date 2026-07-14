73fd1c868cc2afff1513be8897e6b1257930a846
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-07-14 10:17:48 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -6091,8 +6091,15 @@ export interface MappingDenseVectorProperty extends MappingPropertyBase {
 
 export type MappingDenseVectorSimilarity = 'cosine' | 'dot_product' | 'l2_norm' | 'max_inner_product'
 
+export type MappingDocValues = boolean | MappingDocValuesConfig
+
+export interface MappingDocValuesConfig {
+  multi_value?: boolean
+  nullability?: boolean
+}
+
 export interface MappingDocValuesPropertyBase extends MappingCorePropertyBase {
-  doc_values?: boolean
+  doc_values?: MappingDocValues
 }
 
 export interface MappingDoubleNumberProperty extends MappingNumberPropertyBase {
