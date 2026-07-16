258ee180eeeafa99d8463a23ba8a5226eedbc441
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-07-16 14:07:29 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -23970,6 +23970,7 @@ export interface TransformGetTransformStatsCheckpointing {
 export interface TransformGetTransformStatsRequest extends RequestBase {
   transform_id: Names
   allow_no_match?: boolean
+  basic?: boolean
   from?: long
   size?: long
   timeout?: Duration
