992cd95297794e914b3a34c0f62b72c61d2fb41e
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-08-06 11:17:08 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -22499,7 +22499,13 @@ export interface TextStructureTestGrokPatternResponse {
 
 export interface TransformDestination {
   index?: IndexName
+  aliases?: TransformDestinationAlias[]
   pipeline?: string
+}
+
+export interface TransformDestinationAlias {
+  alias: IndexAlias
+  move_on_creation?: boolean
 }
 
 export interface TransformLatest {
@@ -22602,7 +22608,7 @@ export interface TransformGetTransformTransformSummary {
   create_time?: EpochTime<UnitMillis>
   create_time_string?: DateTime
   description?: string
-  dest: ReindexDestination
+  dest: TransformDestination
   frequency?: Duration
   id: Id
   latest?: TransformLatest
@@ -22802,7 +22808,7 @@ export interface TransformUpdateTransformResponse {
   authorization?: MlTransformAuthorization
   create_time: long
   description: string
-  dest: ReindexDestination
+  dest: TransformDestination
   frequency?: Duration
   id: Id
   latest?: TransformLatest
