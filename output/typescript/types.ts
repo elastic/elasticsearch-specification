e9697f28b36acd3dafb421fee0f4b363a4f9b144
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-09-25 08:08:32 +0000

    Update specification output

--- output/typescript/types.ts
+++ output/typescript/types.ts
@@ -6731,6 +6731,13 @@ export interface QueryDslIntervalsWildcard {
   use_field?: Field
 }
 
+export interface QueryDslKqlQuery extends QueryDslQueryBase {
+  case_insensitive?: boolean
+  default_field?: Field
+  query: string
+  time_zone?: TimeZone
+}
+
 export type QueryDslLike = string | QueryDslLikeDocument
 
 export interface QueryDslLikeDocument {
@@ -6918,6 +6925,7 @@ export interface QueryDslQueryContainer {
   ids?: QueryDslIdsQuery
   intervals?: Partial<Record<Field, QueryDslIntervalsQuery>>
   knn?: KnnQuery
+  kql?: QueryDslKqlQuery
   match?: Partial<Record<Field, QueryDslMatchQuery | string | float | boolean>>
   match_all?: QueryDslMatchAllQuery
   match_bool_prefix?: Partial<Record<Field, QueryDslMatchBoolPrefixQuery | string>>
